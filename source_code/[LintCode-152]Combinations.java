18155c3de00087096e5746e92a3c205ebb300d1f
Marco Chen <haisheng.chen4u@foxmail.com>
2020-05-14 18:27:20 -0500

    Update [LintCode-152]Combinations.java

--- source_code/[LintCode-152]Combinations.java
+++ source_code/[LintCode-152]Combinations.java
@@ -3,17 +3,17 @@
 Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
 
 Example
-	Given n = 4 and k = 2, a solution is:
-		[
-		  [2,4],
-		  [3,4],
-		  [2,3],
-		  [1,2],
-		  [1,3],
-		  [1,4]
-		]
+    Given n = 4 and k = 2, a solution is:
+        [
+            [2,4],
+            [3,4],
+            [2,3],
+            [1,2],
+            [1,3],
+            [1,4]
+        ]
 Notice
-	You don't need to care the order of combinations, but you should make sure the numbers in a combination are sorted.
+    You don't need to care the order of combinations, but you should make sure the numbers in a combination are sorted.
 ***/
 public class Solution {
 	
