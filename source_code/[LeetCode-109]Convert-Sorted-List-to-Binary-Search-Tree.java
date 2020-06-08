efb60292293f60087f163c0bccc2e0bf1ff7fc92
marco-chen4u <haisheng.chen4u@foxmail.com>
2020-06-07 20:15:09 -0500

    LeetCode 109. Convert Sorted List to Binary Search Tree

---
+++ source_code/[LeetCode-109]Convert-Sorted-List-to-Binary-Search-Tree.java
@@ -0,0 +1,81 @@
+/***
+* LeetCode 109. Convert Sorted List to Binary Search Tree
+Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+Example:
+    Given the sorted linked list: [-10,-3,0,5,9],
+
+    One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+          0
+         / \
+       -3   9
+       /   /
+     -10  5
+***/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+//version-1: two pointers(slow&fast pointers)
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        // check corner cases
+        if (head == null) {
+            return null;
+        }
+        
+        if (head.next == null) {
+            return new TreeNode(head.val);
+        }
+        
+        // regualr case process below
+        // tow pointers
+        ListNode slow = head;
+        ListNode fast = head;
+        if (fast != null && fast.next != null) {
+            fast = fast.next.next;
+        }
+        
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        ListNode pre = slow;
+        ListNode median = slow.next;
+        ListNode next = median.next;
+        
+        pre.next = null;
+        median.next = null;
+        TreeNode root = new TreeNode(median.val);
+        
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(next);
+        
+        return root;
+    }
+}
