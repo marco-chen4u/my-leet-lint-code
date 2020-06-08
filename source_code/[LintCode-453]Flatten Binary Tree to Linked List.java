a5e9351cc10e4ea743295f207bcc660a543a6790
Marco Chen <haisheng.chen4u@foxmail.com>
2020-06-08 00:02:47 -0500

    Update [LintCode-453]Flatten Binary Tree to Linked List.java

--- source_code/[LintCode-453]Flatten Binary Tree to Linked List.java
+++ source_code/[LintCode-453]Flatten Binary Tree to Linked List.java
@@ -4,45 +4,45 @@ Flatten a binary tree to a fake "linked list" in pre-order traversal.
 Here we use the right pointer in TreeNode as the next pointer in ListNode.
 
 Example
-	Example 1:
-		Input:
-			{1,2,5,3,4,#,6}
-				 1
-				/ \
-			   2   5
-			  / \   \
-			 3   4   6
-		Output:
-			{1,#,2,#,3,#,4,#,5,#,6}
-			   1
-				\
-				 2
-				  \
-				   3
-					\
-					 4
-					  \
-					   5
-						\
-						 6
+    Example 1:
+        Input:
+            {1,2,5,3,4,#,6}
+                 1
+                / \
+               2   5
+              / \   \
+             3   4   6
+        Output:
+            {1,#,2,#,3,#,4,#,5,#,6}
+                   1
+                    \
+                     2
+                      \
+                       3
+                        \
+                         4
+                          \
+                           5
+                            \
+                             6
 			 
-	Example 2:
-		Input:
-			{1}
-			1
-		Output:
-			{1}
-			1
+    Example 2:
+        Input:
+            {1}
+            1
+        Output:
+            {1}
+            1
 
 Challenge
-	Do it in-place without any extra memory.
+    Do it in-place without any extra memory.
 
 Notice
-	Don't forget to mark the left child of each node to null. 
-	Or you will get Time Limit Exceeded or Memory Limit Exceeded.
-	
+    Don't forget to mark the left child of each node to null. 
+    Or you will get Time Limit Exceeded or Memory Limit Exceeded.
+
 URL
-	https://www.lintcode.com/problem/flatten-binary-tree-to-linked-list/
+    https://www.lintcode.com/problem/flatten-binary-tree-to-linked-list/
 ***/
 /**
  * Definition of TreeNode:
@@ -64,34 +64,35 @@ public class Solution {
      */
     public void flatten(TreeNode root) {
         // check corner case
-		if (root == null) {
-			return;
-		}
-		
-		flattenHelper(root);
-    }
+        if (root == null) {
+            return;
+        }
 	
-	private TreeNode flattenHelper(TreeNode node) {
-		if (node == null) {
-			return null;
-		}
-		
-		TreeNode leftLast = flattenHelper(node.left);
-		TreeNode rightLast = flattenHelper(node.right);
-		
-		// connect leftlast to nde.right
-		if (leftLast != null) {
-			leftLast.right = node.right;
-			node.right = node.left;
-			node.left = null;
-		}
-		
-		if (rightLast != null) {
-			return rightLast;
-		}
-		
-		return node;			
+        flattenHelper(root);
+    }
+
+    // helper methods
+    private TreeNode flattenTree(TreeNode node) {
+        if (node == null) {
+            return null;
+        }
+	
+	if (node != null && node.left == null && node.right == null) {
+            return node;
 	}
+	
+        TreeNode leftTail = flattenTree(node.left);
+        TreeNode rightTail = flattenTree(node.right);
+
+        // connect leftTail to node.right
+        if (leftTail != null) {
+            leftTail.right = node.right;
+            node.right = node.left;
+            node.left = null;
+        }
+
+        return (rightTail != null) ? rightTail : leftTail;
+    }
 }
 
 
@@ -102,28 +103,28 @@ public class Solution {
      * @return: nothing
      */
     public void flatten(TreeNode root) {
-		// check corner case
-		if (root == null || 
-			root.left == null && root.right == null) {
-			return root;
-		}
-		
-		Stack<TreeNode> stack = new Stack<TreeNode>();
-		stack.push(root);
-		
-		while (!stack.isEmpty()) {
-			TreeNode current = stack.pop();
-			
-			if (node.right != null) {
-				stack.push(node.right);
-			}
-			
-			if (node.left != null) {
-				stack.push(node.left);
-			}
-			
-			node.left = null;
-			node.right = stack.isEmpty() ? stack.peek() : null;
-		}
-	}	
+        // check corner case
+        if (root == null || 
+            root.left == null && root.right == null) {
+            return;
+        }
+	
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+	
+        while (!stack.isEmpty()) {
+            TreeNode current = stack.pop();
+	
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+	
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+	
+            current.left = null;
+            current.right = (!stack.isEmpty()) ? stack.peek() : null;
+        }
+    }	
 }
