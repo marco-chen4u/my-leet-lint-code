3ec31f41345f615e0de7d39e0b1b1f6575c70907
Marco Chen <haisheng.chen4u@foxmail.com>
2020-06-13 09:50:29 -0500

    Update [LintCode-149]Best Time to Buy and Sell Stock.java

--- source_code/[LintCode-149]Best Time to Buy and Sell Stock.java
+++ source_code/[LintCode-149]Best Time to Buy and Sell Stock.java
@@ -5,20 +5,20 @@ Say you have an array for which the ith element is the price of a given stock on
 If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 
 Example
-	Example 1
-		Input: [3, 2, 3, 1, 2]
-		Output: 1
-		Explanation: You can buy at the third day and then sell it at the 4th day. The profit is 2 - 1 = 1
+    Example 1
+        Input: [3, 2, 3, 1, 2]
+        Output: 1
+        Explanation: You can buy at the third day and then sell it at the 4th day. The profit is 2 - 1 = 1
 
-	Example 2
-		Input: [1, 2, 3, 4, 5]
-		Output: 4
-		Explanation: You can buy at the 0th day and then sell it at the 4th day. The profit is 5 - 1 = 4
+    Example 2
+        Input: [1, 2, 3, 4, 5]
+        Output: 4
+        Explanation: You can buy at the 0th day and then sell it at the 4th day. The profit is 5 - 1 = 4
 
-	Example 3
-		Input: [5, 4, 3, 2, 1]
-		Output: 0
-		Explanation: You can do nothing and get nothing.
+    Example 3
+        Input: [5, 4, 3, 2, 1]
+        Output: 0
+        Explanation: You can do nothing and get nothing.
 ***/
 public class Solution {
     /**
@@ -31,23 +31,23 @@ public class Solution {
         if (prices == null || prices.length == 0) {
             return result;
         }
-        
+
         int maxPrice = Integer.MIN_VALUE;
         int minPrice = prices[0];
         for (int i = 1; i < prices.length; i++) {
             int current = prices[i];
             minPrice = Math.min(minPrice, current);
-            
+
             if (minPrice == current) {
                 maxPrice = Integer.MIN_VALUE;
                 continue;
             }
-            
+
             maxPrice = Math.max(maxPrice, current);
-            
+
             result = Math.max(result, maxPrice - minPrice);
         }// for
-        
+
         return result;
     }
 }
@@ -59,20 +59,20 @@ public class Solution {
      * @return: Maximum profit
      */
     public int maxProfit(int[] prices) {
-		int result = 0;
-		if (prices == null || prices.length == 0) {
-			return result;
-		}
-		
-		result = Integer.MIN_VALUE;
-		int min = prices[0];
-		for (int i = 1; i < prices.length; i++) {
-			int current = prices[i];
-			result = Math.max(result, current - min);
-			
-			min = Math.min(min, current);
-		}
-		
-		return result;
-	}
+        int result = 0;
+        if (prices == null || prices.length == 0) {
+            return result;
+        }
+
+        result = Integer.MIN_VALUE;
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            int current = prices[i];
+            result = Math.max(result, current - min);
+
+            min = Math.min(min, current);
+        }
+
+        return result;
+    }
 }
