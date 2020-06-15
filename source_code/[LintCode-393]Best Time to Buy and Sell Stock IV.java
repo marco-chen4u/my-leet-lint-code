f04b66d98bcc2d0ef894fa7fc57b53ff934dccd2
Marco Chen <haisheng.chen4u@foxmail.com>
2020-06-15 14:06:22 -0500

    Update [LintCode-393]Best Time to Buy and Sell Stock IV.java

--- source_code/[LintCode-393]Best Time to Buy and Sell Stock IV.java
+++ source_code/[LintCode-393]Best Time to Buy and Sell Stock IV.java
@@ -5,21 +5,21 @@ Given an array prices and the i-th element of it represents the price of a stock
 You may complete at most k transactions. What's the maximum profit?
 
 Example
-	Example 1:
-		Input: k = 2, prices = [4, 4, 6, 1, 1, 4, 2 ,5]
-		Output: 6
-		Explanation: Buy at 4 and sell at 6. Then buy at 1 and sell at 5. Your profit is 2 + 4 = 6.
+    Example 1:
+        Input: k = 2, prices = [4, 4, 6, 1, 1, 4, 2 ,5]
+        Output: 6
+        Explanation: Buy at 4 and sell at 6. Then buy at 1 and sell at 5. Your profit is 2 + 4 = 6.
 
-	Example 2:
-		Input: k = 1, prices = [3, 2, 1]
-		Output: 0
-		Explanation: No transaction.
+    Example 2:
+        Input: k = 1, prices = [3, 2, 1]
+        Output: 0
+        Explanation: No transaction.
 
 Challenge
-	O(nk) time. n is the size of prices.
+    O(nk) time. n is the size of prices.
 
 Notice
-	You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+    You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
 ***/
 //Verison-1 DP
 public class Solution {
@@ -92,63 +92,63 @@ public class Solution {
      * @return: Maximum profit
      */
     public int maxProfit(int K, int[] prices) {
-		int result = 0;
-		// check corner case
-		if (prices == null || prices.length <= 1) {
-			return result;
-		}
-		
-		int n = prices.length;
-		if (K > n / 2) {
-			int currentProfit = 0;
-			for (int i = 1; i < n; i++) {
-				currentProfit = prices[i] - prices[i - 1];
-				
-				result += (currentProfit > 0) ? currentProfit : 0;
-			}
-			
-			return result;
-		}
-		
-		// normal case
-		// state		
-		int[][] dp = new int[n + 1][(2 * K + 1) + 1];
-		
-		// initialize
-		for (int[] row :dp) {
-			Arrays.fill(row, Integer.MIN_VALUE);// which means impossible
-		}
-		dp[0][1] = 0;
-		
-		// function
-		for (int i = 1; i <= n; i++) {
-			// phase 1, 3, 5, ..., 2 * K + 1 no stock holding state
-			// dp[i][j] = max{dp[i - 1][j], dp[i - 1][j - 1] + (Pi-1 - Pi-2)}
-			for (int j = 1; j <= 2 * K + 1; j += 2) {
-				dp[i][j] = dp[i - 1][j];
-				
-				if (i > 1 && dp[i - 1][j - 1] != Integer.MIN_VALUE) {
-					dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + prices[i - 1] - prices[i - 2]);
-				}
-			}
-			
-			// phase 2, 4, ..., 2 * K stock holding state
-			// dp[i][j] = max{dp[i - 1][j] + (Pi-1 - Pi-2), dp[i - 1][j -1]}
-			for (int j = 2; j <= 2 * K; j += 2) {
-				dp[i][j] = dp[i -1][j - 1];
-				
-				if (i > 1 && dp[i - 1][j] != Integer.MIN_VALUE) {
-					dp[i][j] = Math.max(dp[i][j], dp[i - 1][j] + prices[i - 1] - prices[i - 2]);
-				}
-			}			
-		}
-		
-		// answer
-		// get the max Profit sum in the phase 1, 3 and 5
-		for (int i = 1; i <= 2 * K + 1; i += 2) {
-			result = Math.max(result, dp[n][i]);
-		}
-		
-		return result;
+        int result = 0;
+        // check corner case
+        if (prices == null || prices.length <= 1) {
+            return result;
+        }
+
+        int n = prices.length;
+        if (K > n / 2) {
+            int currentProfit = 0;
+            for (int i = 1; i < n; i++) {
+                currentProfit = prices[i] - prices[i - 1];
+
+                result += (currentProfit > 0) ? currentProfit : 0;
+            }
+
+            return result;
+        }
+
+        // normal case
+        // state		
+        int[][] dp = new int[n + 1][(2 * K + 1) + 1];
+
+        // initialize
+        for (int[] row :dp) {
+            Arrays.fill(row, Integer.MIN_VALUE);// which means impossible
+        }
+        dp[0][1] = 0;
+
+        // function
+        for (int i = 1; i <= n; i++) {
+            // phase 1, 3, 5, ..., 2 * K + 1 no stock holding state
+            // dp[i][j] = max{dp[i - 1][j], dp[i - 1][j - 1] + (Pi-1 - Pi-2)}
+            for (int j = 1; j <= 2 * K + 1; j += 2) {
+                dp[i][j] = dp[i - 1][j];
+
+                if (i > 1 && dp[i - 1][j - 1] != Integer.MIN_VALUE) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + prices[i - 1] - prices[i - 2]);
+                }
+            }
+
+            // phase 2, 4, ..., 2 * K stock holding state
+            // dp[i][j] = max{dp[i - 1][j] + (Pi-1 - Pi-2), dp[i - 1][j -1]}
+            for (int j = 2; j <= 2 * K; j += 2) {
+                dp[i][j] = dp[i -1][j - 1];
+
+                if (i > 1 && dp[i - 1][j] != Integer.MIN_VALUE) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j] + prices[i - 1] - prices[i - 2]);
+                }
+            }			
+        }
+
+        // answer
+        // get the max Profit sum in the phase 1, 3 and 5
+        for (int i = 1; i <= 2 * K + 1; i += 2) {
+            result = Math.max(result, dp[n][i]);
+        }
+
+        return result;
     }
 }
