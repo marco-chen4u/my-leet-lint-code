96764e20a459dccffc19a863906e241be916585f
Marco Chen <haisheng.chen4u@foxmail.com>
2020-06-15 18:36:06 -0500

    Update [LintCode-107]Word Break-1.java

--- source_code/[LintCode-107]Word Break-1.java
+++ source_code/[LintCode-107]Word Break-1.java
@@ -4,14 +4,13 @@ Given a string s and a dictionary of words dict,
 determine if s can be break into a space-separated sequence of one or more dictionary words.
 
 Example
-	Example 1:
-		Input:  "lintcode", ["lint", "code"]
-		Output:  true
+    Example 1:
+        Input:  "lintcode", ["lint", "code"]
+        Output:  true
 
-	Example 2:
-		Input: "a", ["a"]
-		Output:  true
-	
+    Example 2:
+        Input: "a", ["a"]
+        Output:  true
 ***/
 
 public class Solution {
