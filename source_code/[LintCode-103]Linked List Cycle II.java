7758d8c16e9dcdb33912ce70d8de811e8a427068
Marco Chen <haisheng.chen4u@foxmail.com>
2020-06-15 22:22:53 -0500

    Update [LintCode-103]Linked List Cycle II.java

--- source_code/[LintCode-103]Linked List Cycle II.java
+++ source_code/[LintCode-103]Linked List Cycle II.java
@@ -6,21 +6,21 @@ Given a linked list, return the node where the cycle begins.
 If there is no cycle, return null.
 
 Example
-	Example 1:
-		Input：null,no cycle
-		Output：no cycle
-		Explanation：
-			List is null，so no cycle.
+    Example 1:
+        Input：null,no cycle
+        Output：no cycle
+        Explanation：
+            List is null，so no cycle.
 
-	Example 2:
-		Input：-21->10->4->5，tail connects to node index 1
-		Output：10
-		Explanation：
-			The last node 5 points to the node whose index is 1, which is 10, so the entrance of the ring is 10
+    Example 2:
+        Input：-21->10->4->5，tail connects to node index 1
+        Output：10
+        Explanation：
+            The last node 5 points to the node whose index is 1, which is 10, so the entrance of the ring is 10
 
 Challenge
-	Follow up:
-	Can you solve it without using extra space?
+    Follow up:
+    Can you solve it without using extra space?
 ***/
 /**
  * Definition for ListNode
