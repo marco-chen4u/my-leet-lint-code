f3c5072e11f20d750b34808af0d524eb264fb481
Marco Chen <haisheng.chen4u@foxmail.com>
2020-06-15 21:33:27 -0500

    Update [LintCode-121]Word Ladder II.java

--- source_code/[LintCode-121]Word Ladder II.java
+++ source_code/[LintCode-121]Word Ladder II.java
@@ -2,82 +2,82 @@
 * LintCode 121. Word Ladder II
 Given two words (start and end), and a dictionary, 
 find all shortest transformation sequence(s) from start to end, such that:
-	1.Only one letter can be changed at a time
-	2.Each intermediate word must exist in the dictionary
-	
+    1.Only one letter can be changed at a time
+    2.Each intermediate word must exist in the dictionary
+
 Example
-	Given:
-		start = "hit"
-		end = "cog"
-		dict = ["hot","dot","dog","lot","log"]
-	Return:
-		[
-		  ["hit","hot","dot","dog","cog"],
-		  ["hit","hot","lot","log","cog"]
-		]
+    Given:
+        start = "hit"
+        end = "cog"
+        dict = ["hot","dot","dog","lot","log"]
+    Return:
+        [
+          ["hit","hot","dot","dog","cog"],
+          ["hit","hot","lot","log","cog"]
+    ]
 Notice
-	All words have the same length.
-	All words contain only lowercase alphabetic characters.
+    All words have the same length.
+    All words contain only lowercase alphabetic characters.
 ***/
 public class Solution {
-	// helper methods
-	
-	private String replace(String word, int index, char c) {
-		char[] wordCharArray = word.toCharArray();
-		wordCharArray[index] = c;
-		return new String(wordCharArray);
-	}
-	
-	private List<String> getNextWords(String word, Set<String> dict) {
-		List<String> result = new ArrayList<String>();
-		
-		for (char c = 'a'; c <= 'z'; c++) {
-			for (int i = 0; i < word.length(); i++) {
-				if (word.charAt(i) == c) {
-					continue;
-				}
-				
-				String newWord = replace(word, i, c);
-				if (dict.contains(newWord)) {
-					result.add(newWord);
-				}
-			}
-		}
-		
-		return result;
-	}
-	
-	/*
-	* use BFS to traverse every node of the graph, keep records of each node's neighboring nodes and distance
-	*/
-	private void traverse(String start, String end, 
-							Set<String> dict, 
-							Map<String, List<String>> map, 
-							Map<String, Integer> distance) {
-		// initialize
-		Queue<String> queue = new LinkedList<String>();
-		for (String keyWord : dict) {
-			map.put(keyWord, new ArrayList<String>());
-		}		
-		distance.put(start, 0);
-		queue.offer(start);
-		
-		while (!queue.isEmpty()) {
-			String current = queue.poll();
-			for (String nextWord : getNextWords(current, dict)) {
-				map.get(current).add(nextWord);
-				if (!distance.containsKey(nextWord)) {
-					int newDistance = distance.get(current) + 1;
-					distance.put(nextWord, newDistance);
-					queue.offer(nextWord);
-				}
-			}
-		}
-	}
-	
-	/*
-	* use DFS to get all path of ladders from end back-tracking to start
-	*/
+    // helper methods
+
+    private String replace(String word, int index, char c) {
+        char[] wordCharArray = word.toCharArray();
+        wordCharArray[index] = c;
+        return new String(wordCharArray);
+    }
+
+    private List<String> getNextWords(String word, Set<String> dict) {
+        List<String> result = new ArrayList<String>();
+
+        for (char c = 'a'; c <= 'z'; c++) {
+            for (int i = 0; i < word.length(); i++) {
+                if (word.charAt(i) == c) {
+                    continue;
+                }
+
+                String newWord = replace(word, i, c);
+                if (dict.contains(newWord)) {
+                    result.add(newWord);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    /*
+    * use BFS to traverse every node of the graph, keep records of each node's neighboring nodes and distance
+    */
+    private void traverse(String start, String end, 
+                            Set<String> dict, 
+                            Map<String, List<String>> map, 
+                            Map<String, Integer> distance) {
+        // initialize
+        Queue<String> queue = new LinkedList<String>();
+        for (String keyWord : dict) {
+            map.put(keyWord, new ArrayList<String>());
+        }		
+        distance.put(start, 0);
+        queue.offer(start);
+
+        while (!queue.isEmpty()) {
+            String current = queue.poll();
+            for (String nextWord : getNextWords(current, dict)) {
+                map.get(current).add(nextWord);
+                if (!distance.containsKey(nextWord)) {
+                    int newDistance = distance.get(current) + 1;
+                    distance.put(nextWord, newDistance);
+                    queue.offer(nextWord);
+                }
+            }
+        }
+    }
+
+    /*
+    * use DFS to get all path of ladders from end back-tracking to start
+    */
     private void getLadderPaths(String current,
                         String start,
                         Map<String, List<String>>map,
@@ -109,26 +109,26 @@ public class Solution {
      * @return: a list of lists of string
      */
     public List<List<String>> findLadders(String start, String end, Set<String> dict) {
-		// initialize
-		List<List<String>> ladders = new ArrayList<List<String>>();// result
-		Map<String, List<String>> map = new HashMap<String, List<String>>();// keep record of each node's neighboring nodes
-		Map<String, Integer> distance = new HashMap<String, Integer>(); // keep record of each node's distance[start->current]
-		
-		// check corner case
-		if (dict == null || dict.size() == 0) {
-			return ladders;
-		}
-		
-		dict.add(start);
-		dict.add(end);
-		
-		// use BFS to traverse every node of the graph, keep records of each node's neighboring nodes and distance
-		traverse(start, end, dict, map, distance);
-		
-		List<String> path = new ArrayList<String>();
-		// use DFS to get all path of ladders from end back-tracking to start
-		getLadderPaths(end, start, map, distance, path, ladders);
-		
-		return ladders;
+        // initialize
+        List<List<String>> ladders = new ArrayList<List<String>>();// result
+        Map<String, List<String>> map = new HashMap<String, List<String>>();// keep record of each node's neighboring nodes
+        Map<String, Integer> distance = new HashMap<String, Integer>(); // keep record of each node's distance[start->current]
+
+        // check corner case
+        if (dict == null || dict.size() == 0) {
+            return ladders;
+        }
+
+        dict.add(start);
+        dict.add(end);
+
+        // use BFS to traverse every node of the graph, keep records of each node's neighboring nodes and distance
+        traverse(start, end, dict, map, distance);
+
+        List<String> path = new ArrayList<String>();
+        // use DFS to get all path of ladders from end back-tracking to start
+        getLadderPaths(end, start, map, distance, path, ladders);
+
+        return ladders;
     }
 }
