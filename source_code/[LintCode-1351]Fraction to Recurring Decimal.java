712ff59b586b5b7614fdef6d6077ce71a5d6d671
Marco Chen <haisheng.chen4u@foxmail.com>
2020-07-08 18:47:00 -0500

    Update [LintCode-1351]Fraction to Recurring Decimal.java

--- source_code/[LintCode-1351]Fraction to Recurring Decimal.java
+++ source_code/[LintCode-1351]Fraction to Recurring Decimal.java
@@ -5,13 +5,13 @@ Given two integers representing the numerator and denominator of a fraction, ret
 If the fractional part is repeating, enclose the repeating part in parentheses.
 
 Example
-	Example1
-		Input: numerator = 1, denominator = 2
-		Output: "0.5"
+    Example1
+        Input: numerator = 1, denominator = 2
+        Output: "0.5"
 
-	Example2
-		Input: numerator = 2, denominator = 3
-		Output: "0.(6)"
+    Example2
+        Input: numerator = 2, denominator = 3
+        Output: "0.(6)"
 ***/
 /*
 * value = numerator / denominator;
@@ -19,87 +19,87 @@ Example
 * (a)get integral-part of string
 
 * (b)get frational-part of string
-	* if not exist (fractional value) {
-		return integralValue string part;
-	* }
-	* else {
-	* 	(long)value = fractionalValue;
-		Map<Long, Integer> map = keep record of the start index pos of the value
-		StringBuilder sb = new StringBuilder();
-		while (value != 0) {
-			value *= 10;
-			sb.append(value / dividend);
-			
-			value %= dividend;
-			
-			if (map.containsKey(value)) {// that means there's reapted value to be calculated
-				int indexPos = map.get(value);// get the index position where we insert at the very beginning
-				sb.insert(indexPos, "(");
-				sb.append(")");
-				break;
-			}
-			map.put(value, sb.length())// record down the new value's starting position index
-		}
-	* }
+    * if not exist (fractional value) {
+        return integralValue string part;
+    * }
+    * else {
+    * 	(long)value = fractionalValue;
+        Map<Long, Integer> map = keep record of the start index pos of the value
+        StringBuilder sb = new StringBuilder();
+        while (value != 0) {
+            value *= 10;
+            sb.append(value / dividend);
+	
+            value %= dividend;
+
+            if (map.containsKey(value)) {// that means there's reapted value to be calculated
+                int indexPos = map.get(value);// get the index position where we insert at the very beginning
+                sb.insert(indexPos, "(");
+                sb.append(")");
+                break;
+            }
+            map.put(value, sb.length())// record down the new value's starting position index
+        }
+    * }
 * (c) integral-part concates fractional-pert, return result
 */
 //version-1
 public class Solution {
-	/**
+    /**
      * @param numerator: a integer
      * @param denominator: a integer
      * @return: return a string
      */
     public String fractionToDecimal(int numerator, int denominator) {
-		String result = null;
-		// check corner case
-		if (numerator == 0) {
-			return "0";
-		}
-		
-		//(1)Integeral part 
-		String integeralPart = "";
-		int integralVal = numerator / denominator;
-		int remainder = numerator % denominator;
-		integeralPart = String.valueOf(integralVal);
-		
-		// if there's no fractional part
-		if (remainder == 0) {
-			return integeralPart;
-		}		
-		
-		//(2)Fractional part
-		String fractionalPart = ".";
-		StringBuilder sb = new StringBuilder(fractionalPart);
-		long value = remainder;
-		Map<Long, Integer> map = new HashMap<Long, Integer>();// <fraction-part-value, startPos>
-		map.put(value, 0);
-		while (value != 0) {
-			value *= 10;
-			sb.append(value / denominator);
-			
-			value %= denominator;
-			
-			if (map.containsKey(value)) {
-				int indexPos = map.get(value);
-				// add brakets
-				sb.insert(indexPos, "(");
-				sb.append(")");
-				
-				break;
-			}
-			
-			map.put(value, sb.length());
-		}
-		
-		fractionalPart = sb.toString();
-		
-		
-		//(3) combine together of integral and fractional parts to the result
-		result = integeralPart + fractionalPart;
-		
-		return result;
-	}
+        String result = null;
+        // check corner case
+        if (numerator == 0) {
+            return "0";
+        }
+
+        //(1)Integeral part 
+        String integeralPart = "";
+        int integralVal = numerator / denominator;
+        int remainder = numerator % denominator;
+        integeralPart = String.valueOf(integralVal);
+
+        // if there's no fractional part
+        if (remainder == 0) {
+            return integeralPart;
+        }		
+
+        //(2)Fractional part
+        String fractionalPart = ".";
+        StringBuilder sb = new StringBuilder(fractionalPart);
+        long value = remainder;
+        Map<Long, Integer> map = new HashMap<Long, Integer>();// <fraction-part-value, startPos>
+        map.put(value, 0);
+        while (value != 0) {
+            value *= 10;
+            sb.append(value / denominator);
+
+            value %= denominator;
+
+            if (map.containsKey(value)) {
+                int indexPos = map.get(value);
+                // add brakets
+                sb.insert(indexPos, "(");
+                sb.append(")");
+
+                break;
+            }
+
+            map.put(value, sb.length());
+        }
+
+        fractionalPart = sb.toString();
+
+
+        //(3) combine together of integral and fractional parts to the result
+        result = integeralPart + fractionalPart;
+
+        return result;
+    }
 }
 
 //version-2
