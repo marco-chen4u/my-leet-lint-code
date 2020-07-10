89b5225c9b4d83ffa7ce40e3d4ec8d0fb1016b70
Marco Chen <haisheng.chen4u@foxmail.com>
2020-07-09 23:15:19 -0500

    Update [LeetCode-170]Two-Sum-III-Data-Structure-design.java

--- source_code/[LeetCode-170]Two-Sum-III-Data-Structure-design.java
+++ source_code/[LeetCode-170]Two-Sum-III-Data-Structure-design.java
@@ -131,3 +131,44 @@ class TwoSum {
  * obj.add(number);
  * boolean param_2 = obj.find(value);
  */
+
+//version-3: O(n), Map
+class TwoSum {
+    
+    Map<Integer, Integer> map;
+
+    /** Initialize your data structure here. */
+    public TwoSum() {
+        map = new HashMap<>();
+    }
+    
+    /** Add the number to an internal data structure.. */
+    public void add(int number) {
+        map.put(number, map.getOrDefault(number, 0) + 1);
+    }
+    
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    public boolean find(int value) {
+        for (int key : map.keySet()) {
+            int num1 = key;
+            int num2 = value - num1;
+            
+            if (num1 == num2 && map.get(key) > 1) {
+                return true;
+            }
+            
+            if (num1 != num2 && map.containsKey(num2)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum obj = new TwoSum();
+ * obj.add(number);
+ * boolean param_2 = obj.find(value);
+ */
