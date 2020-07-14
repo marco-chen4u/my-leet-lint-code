6d6ee1c6c2091f0399d164fafe82203cb1c234e9
Marco Chen <haisheng.chen4u@foxmail.com>
2020-07-13 22:04:01 -0500

    Update [LintCode-184]Largest Number.java

--- source_code/[LintCode-184]Largest Number.java
+++ source_code/[LintCode-184]Largest Number.java
@@ -3,19 +3,19 @@
 Given a list of non negative integers, arrange them such that they form the largest number.
 
 Example
-	Example 1:
-		Input:[1, 20, 23, 4, 8]
-		Output:"8423201"
+    Example 1:
+        Input:[1, 20, 23, 4, 8]
+        Output:"8423201"
 
-	Example 2:
-		Input:[4, 6, 65]
-		Output:"6654"
+    Example 2:
+        Input:[4, 6, 65]
+        Output:"6654"
 
 Challenge
-	Do it in O(nlogn) time complexity.
+    Do it in O(nlogn) time complexity.
 
 Notice
-	The result may be very large, so you need to return a string instead of an integer.
+    The result may be very large, so you need to return a string instead of an integer.
 ***/
 
 public class Solution {
@@ -39,45 +39,19 @@ public class Solution {
         Comparator<String> comparator = new Comparator<String>() {
             @Override
             public int compare(String a, String b) {
-                int i = 0, j = 0;
-                while (i < a.length() && j < b.length()) {
-                    if (a.charAt(i) != b.charAt(j)) {
-                        return b.charAt(j) - a.charAt(i);
-                    }
-                    else {
-                        i++;
-                        j++;
-                        
-                        if (i == a.length() && j == b.length()) {
-                            break;
-                        }
-                        
-                        if (i == a.length()) {
-                            i--;
-                        }
-                        
-                        if (j == b.length()) {
-                            j--;
-                        }
-                    }
-                }
-                
-                return 0;
+                String ab = a + b;
+		String ba = b + a;
+		return ba.compareTo(ab);
             }
         };
         
         Arrays.sort(values, comparator);
         
-        StringBuilder sb = new StringBuilder();
-        for (String value : values) {
-            if (/*value.equals("0") &&*/ (sb.toString().equals("0"))) {
-                continue;
-            }
-            
-            sb.append(value);
-        }
+        if ("0".equals(values[0])) {
+	    return "0";
+	}
         
-        result = sb.toString();
+        result = String.join("", values);
         
         return result;
     }
