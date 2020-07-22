637a85b15eb509a83cf4712460fc35a2976bc2db
marco-chen4u <haisheng.chen4u@foxmail.com>
2020-07-21 23:28:35 -0500

    Leetcode 199. Binary Tree Right Side View

---
+++ source_code/[LeetCode-199]Binary-Tree-Right-Side-View.java
@@ -0,0 +1,66 @@
+/***
+* LeetCode 199. Binary Tree Right Side View
+Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+Example:
+    Input: [1,2,3,null,5,null,4]
+    Output: [1, 3, 4]
+    Explanation:
+
+       1            <---
+     /   \
+    2     3         <---
+     \     \
+      5     4       <---
+***/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+//solution: BFS, level orientation traversal of the binary tree
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        // check corner case
+        if (root == null) {
+            return result;
+        }
+        
+        // regular case
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int last = size - 1;
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                
+                if (i == last) {
+                    result.add(current.val);
+                }
+                
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+                
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+        }
+        
+        return result;
+    }
+}
