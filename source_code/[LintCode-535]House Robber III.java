d47340e706a2c9d4f475f43be95cad06713b5d75
Marco Chen <haisheng.chen4u@foxmail.com>
2020-07-21 22:02:07 -0500

    Update [LintCode-535]House Robber III.java

--- source_code/[LintCode-535]House Robber III.java
+++ source_code/[LintCode-535]House Robber III.java
@@ -1,33 +1,37 @@
 /***
 * LintCode 535. House Robber III
-The thief has found himself a new palce for his thievery again. There's only one entrance to this area called the "root". Besides the root, every house has one and only one parent house. After a toure, the smart thief realized that "all houses in thise place forms a binary tree". It will automatically contact the plice when two directly-linked houses were broken into on the same night.
+The thief has found himself a new palce for his thievery again. 
+There's only one entrance to this area called the "root". 
+Besides the root, every house has one and only one parent house. 
+After a toure, the smart thief realized that "all houses in thise place forms a binary tree". 
+It will automatically contact the plice when two directly-linked houses were broken into on the same night.
 
 Determine the maximum amount of money the thief can rob tonight without alerting the police.
 
 Example
-	Example1
-		Input:  {3,2,3,#,3,#,1}
-		Output: 7
-		Explanation:
-			Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
-			  3<-
-			 / \
-			2   3
-			 \   \ 
-			->3   1<-
-		  
-	Example2
-		Input:  {3,4,5,1,3,#,1}
-		Output: 9
-		Explanation:
-			Maximum amount of money the thief can rob = 4 + 5 = 9.
-				3
-			   / \
-			->4   5<-
-			 / \   \ 
-			1   3   1
+    Example1
+        Input:  {3,2,3,#,3,#,1}
+        Output: 7
+        Explanation:
+            Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+              3<-
+             / \
+            2   3
+             \   \ 
+            ->3   1<-
+  
+    Example2
+        Input:  {3,4,5,1,3,#,1}
+        Output: 9
+        Explanation:
+            Maximum amount of money the thief can rob = 4 + 5 = 9.
+                3
+               / \
+            ->4   5<-
+             / \   \ 
+            1   3   1
 Note
-	This problem is the extension of House Robber and House Robber II.
+    This problem is the extension of House Robber and House Robber II.
 ***/
 /**
  * Definition of TreeNode:
