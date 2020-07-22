f3822a7378542752d1d8d16ce50695ba8a07f5a6
Marco Chen <haisheng.chen4u@foxmail.com>
2020-07-22 18:15:08 -0500

    Update [LintCode-1325]Bitwise AND of Numbers Range.java

--- source_code/[LintCode-1325]Bitwise AND of Numbers Range.java
+++ source_code/[LintCode-1325]Bitwise AND of Numbers Range.java
@@ -5,13 +5,13 @@ Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of 
 For example, given the range [5, 7], you should return 4.
 
 Example
-	Example1
-		Input: m=5, n=7
-		Output: 4
+    Example1
+        Input: m=5, n=7
+        Output: 4
 
-	Example2
-		Input: m=14, n=15
-		Output: 14
+    Example2
+        Input: m=14, n=15
+        Output: 14
 ***/
 public class Solution {
     /**
@@ -23,10 +23,10 @@ public class Solution {
         int posIndex = 0;// from right to left
         
         while (m != n) {
-            m = m >> 1;//right shit by 1 which means by divided by 2,original number will remove last 1 digit
+            m = m >> 1;// right shit by 1 which means being divided by 2,original number will remove last 1 digit
             n = n >> 1;
             
-            posIndex++;// the lower digit position will move forward(from right to left) by 1, until find the common higher 1s
+            posIndex++;// the lower digit position will move forward(from right to left) by 1, until to find the common higher 1s
         }
         
         return m << posIndex;// n << posIndex
