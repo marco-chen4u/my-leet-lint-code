18cadba07833bebd4da5adbc4ea57fe1c50e3413
Marco Chen <haisheng.chen4u@foxmail.com>
2020-07-26 23:43:19 -0500

    Update [LintCode-406]Minimum Size Subarray Sum.java

--- source_code/[LintCode-406]Minimum Size Subarray Sum.java
+++ source_code/[LintCode-406]Minimum Size Subarray Sum.java
@@ -16,6 +16,7 @@ Example
 Challenge
     If you have figured out the O(nlog n) solution, try coding another solution of which the time complexity is O(n).
 ***/
+//solution-1: O(n), Two pointers, Silding Window
 public class Solution {
     /**
      * @param nums: an array of integers
@@ -51,4 +52,52 @@ public class Solution {
 
         return (result == Integer.MAX_VALUE) ? -1 : result;
     }	
+}
+
+//solution-2: PrefixSum + BinarySearch
+class Solution {
+    public int minimumSize(int[] nums, int target) {
+        // check corner case
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        int result = Integer.MAX_VALUE;
+        int n = nums.length;
+        
+        int[] prefixSum = new int[n + 1];
+        for(int i = 1; i <= n; i++) {
+            int current = nums[i - 1];
+            prefixSum[i] = prefixSum[i - 1] + current;
+        }
+        
+        for (int left = 0; left < n + 1; left++) {
+            int right = searchRightPos(prefixSum, left + 1, n, prefixSum[left] + target);
+            
+            if (right == n + 1) {
+                break;
+            }
+            
+            result = Math.min(result, right - left);
+        }
+        
+        return (result == Integer.MAX_VALUE) ? -1 : result;
+    }
+    
+    // helper method
+    private int searchRightPos(int[] prefixSum, int leftNext, int n, int value) {
+        int start = leftNext;
+        int end = n;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (prefixSum[mid] >= value) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        
+        return start;
+    }
 }
