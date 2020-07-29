52ff5bd58dec3469e569287c4b0d9d6cf72ec2ed
Marco Chen <haisheng.chen4u@foxmail.com>
2020-07-28 23:08:46 -0500

    Update [LintCode-132]Word Search II-2.java

--- source_code/[LintCode-132]Word Search II-2.java
+++ source_code/[LintCode-132]Word Search II-2.java
@@ -5,28 +5,27 @@ Find all words in the dictionary that can be found in the matrix.
 A word can start from any position in the matrix and go left/right/up/down to the adjacent position. 
 One character only be used once in one word. No same word in dictionary
 
-Example
 Example 1:
-	Input：["doaf",
-		    "agai",
-		    "dcan"]，
-		   
-		  ["dog","dad","dgdg","can","again"]
-	Output：["again","can","dad","dog"]
-	Explanation：
-			d o a f
-			a g a i
-			d c a n
-		search in Matrix，so return ["again","can","dad","dog"].
+    Input：["doaf",
+            "agai",
+            "dcan"]，
+
+          ["dog","dad","dgdg","can","again"]
+    Output：["again","can","dad","dog"]
+    Explanation：
+            d o a f
+            a g a i
+            d c a n
+        search in Matrix，so return ["again","can","dad","dog"].
 
 Example 2:
-	Input：["a"]，
-	      
-		  ["b"]
-	Output：[]
-	Explanation：
-			a
-		search in Matrix，return null.
+    Input：["a"]，
+
+           ["b"]
+    Output：[]
+    Explanation：
+            a
+        search in Matrix，return null.
 ***/
 // version-2 : Trie data structure
 public class Solution {
