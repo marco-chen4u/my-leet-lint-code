6bddd4d4be7af52796e886b07b44120c8ee575c8
marco-chen4u <haisheng.chen4u@foxmail.com>
2020-11-22 16:44:06 -0600

    add more elegant code solution for LeetCode 227. Basic Calculator II

--- source_code/[LeetCode-227]Basic-Calculator-II.java
+++ source_code/[LeetCode-227]Basic-Calculator-II.java
@@ -21,7 +21,7 @@ Note:
     Do not use the eval built-in library function.
 ***/
 
-//solution-1: using stack
+//solution-1: using stack, time complexity: O(n)
 class Solution {
     public int calculate(String s) {
         
@@ -91,5 +91,77 @@ class Solution {
                 stack.push(stack.pop() / currentValue);
                 break;
         }
+    }
+}
+
+// solution-2: using stack, time complexity:O(n)
+class Solution {
+    public int calculate(String s) {
+        // initialization
+        int result = 0;
+        int currentValue = 0;
+        char operator = '+';
+        
+        // check corner case
+        if (s == null || s.isEmpty()) {
+            return result;
+        }
+        
+        // regular case
+        int size = s.length();
+        int lastPos = size - 1;
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < size; i++) {
+            char ch = s.charAt(i);
+            
+            if (isDigit(ch)) {
+                currentValue = currentValue * 10 + (ch - '0');
+            }
+            
+           if (!isDigit(ch) && !isBlank(ch) || i == size - 1){
+                switch (operator) {
+                    case '+':
+                        stack.push(currentValue);
+                        break;
+                    case '-':
+                        stack.push(-currentValue);
+                        break;
+                    case '*':
+                        stack.push(stack.pop() * currentValue);
+                        break;
+                    case '/':
+                        stack.push(stack.pop() / currentValue);
+                        break;
+                }
+
+                operator = ch;
+                currentValue = 0;                
+            }
+
+        }
+        
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        
+        return result;
+        
+    }
+    
+    // helper methods
+    private boolean isDigit(char ch) {
+        return Character.isDigit(ch);
+    }
+    
+    private boolean isOperator(char ch) {
+        return ch == '+' ||
+                ch == '-' ||
+                ch == '*' ||
+                ch == '/';
+    }
+    
+    private boolean isBlank(char ch) {
+        return Character.isWhitespace(ch);
     }
 }
