11b33fdac174b50b307a84aa744dbbb00c3c40b2
Marco Chen <haisheng.chen4u@foxmail.com>
2021-01-17 22:46:12 -0600

    Update [LintCode-519]Consistent Hashing.java

--- source_code/[LintCode-519]Consistent Hashing.java
+++ source_code/[LintCode-519]Consistent Hashing.java
@@ -1,10 +1,15 @@
 /***
 * LintCode 519. Consistent Hashing
-A general database method for performing a horizontal shard is to take the id against the total number of database servers n and then to find out which machine it is on. The downside of this approach is that as the data continues to increase, we need to increase the database server. When n is changed to n+1, almost all of the data has to be moved, which is not consistent. In order to reduce the defects caused by this naive's hash method (%n), a new hash algorithm emerges: Consistent Hashing
+A general database method for performing a horizontal shard is to take the id against the total number of database servers n and then to find out which machine it is on. 
+The downside of this approach is that as the data continues to increase, we need to increase the database server. When n is changed to n+1, 
+almost all of the data has to be moved, which is not consistent. In order to reduce the defects caused by this naive's hash method (%n), 
+a new hash algorithm emerges: Consistent Hashing
 There are many ways to implement this algorithm. Here we implement a simple Consistent Hashing.
-	-1.Take id to 360. If there are 3 machines at the beginning, then let 3 machines be responsible for the three parts of 0~119, 120~239, 240~359. Then, how much is the model, check which zone you are in, and which machine to go to.
+	-1.Take id to 360. If there are 3 machines at the beginning, then let 3 machines be responsible for the three parts of 0~119, 120~239, 240~359. 
+	    Then, how much is the model, check which zone you are in, and which machine to go to.
 	-2.When the machine changes from n to n+1, we find the largest one from the n intervals, then divide it into two and give half to the n+1th machine.
-	-3.For example, when changing from 3 to 4, we find the third interval 0~119 is the current largest interval, then we divide 0~119 into 0~59 and 60~119. 0~59 is still given to the first machine, 60~119 to the fourth machine.
+	-3.For example, when changing from 3 to 4, we find the third interval 0~119 is the current largest interval, then we divide 0~119 into 0~59 and 60~119. 
+	    0~59 is still given to the first machine, 60~119 to the fourth machine.
 	-4.Then change from 4 to 5, we find the largest interval is the third interval 120~239, after splitting into two, it becomes 120~179, 180~239.
 	Suppose all the data is on one machine at the beginning. When adding to the nth machine, what is the distribution of the interval and the corresponding machine number?
 
@@ -53,8 +58,10 @@ For example, the size of 0~119, 120~239 is 120, but the number of the previous m
 and the number of the next machine is 2, so we split the range of 0~119.
 ***/
 /*
-*查找所有的sharding分段，每次需要分配新的token-range给新加入的机器时，在整个现有的token-range sharding segements中，找到最大的token-range segment,然后对其重新分配-即进行二分，前半段留给原来的机器，把后半段分配给新的机器。
-* virtual nodes is an way to assign token-range to machines, whitch virtual nodes, instead of a machine being repsonsible for just one token range, it is instead responsible for many for token ranges.
+*查找所有的sharding分段，每次需要分配新的token-range给新加入的机器时，在整个现有的token-range sharding segements中，找到最大的token-range segment,然后对其重新分配-即进行二分，
+*前半段留给原来的机器，把后半段分配给新的机器。
+*virtual nodes is an way to assign token-range to machines, whitch virtual nodes, instead of a machine being repsonsible for just one token range, 
+*it is instead responsible for many for token ranges.
 */
 public class Solution {
     /*
