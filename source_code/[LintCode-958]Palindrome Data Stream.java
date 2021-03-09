f389e99f4972d453318a02c7227e8bb34a66143f
Marco Chen <haisheng.chen4u@foxmail.com>
2021-03-08 23:00:06 -0600

    Update [LintCode-958]Palindrome Data Stream.java

--- source_code/[LintCode-958]Palindrome Data Stream.java
+++ source_code/[LintCode-958]Palindrome Data Stream.java
@@ -3,25 +3,25 @@
 There is a data stream, one letter at a time, and determine whether the current stream's arrangement can form a palindrome.
 
 Example
-	Example 1:
-		input:s = ['a','a','a','a','a']
-		outut:[1,1,1,1,1]
-		Explanation:
-			“a” can form a palindrome
-			“aa” can form a palindrome
-			“aaa” can form a palindrome
-			“aaaa” can form a palindrome
-			“aaaaa” can form a palindrome
+    Example 1:
+        input:s = ['a','a','a','a','a']
+        outut:[1,1,1,1,1]
+        Explanation:
+            “a” can form a palindrome
+            “aa” can form a palindrome
+            “aaa” can form a palindrome
+            “aaaa” can form a palindrome
+            “aaaaa” can form a palindrome
 
-	Example 2:
-		input:s = ['a','b','a']
-		outut:[1,0,1]
-		Explanation:
-			“a” can form a palindrome
-			“ab” can't form a palindrome
-			“aba” can form a palindrome
-			Notice
-			1 <= |s| <= 10^5
+    Example 2:
+        input:s = ['a','b','a']
+        outut:[1,0,1]
+        Explanation:
+            “a” can form a palindrome
+            “ab” can't form a palindrome
+            “aba” can form a palindrome
+            Notice
+            1 <= |s| <= 10^5
 
 link: https://www.lintcode.com/problem/palindrome-data-stream/
 ***/
