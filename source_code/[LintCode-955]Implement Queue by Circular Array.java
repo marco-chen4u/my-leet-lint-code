1b2b3e0800a5af94c54e5f178269601e38b62cb0
Marco Chen <haisheng.chen4u@foxmail.com>
2021-03-08 23:03:20 -0600

    Update [LintCode-955]Implement Queue by Circular Array.java

--- source_code/[LintCode-955]Implement Queue by Circular Array.java
+++ source_code/[LintCode-955]Implement Queue by Circular Array.java
@@ -1,47 +1,47 @@
 /***
 * LintCode 955. Implement Queue by Circular Array
 Implement queue by circulant array. You need to support the following methods:
-	1.CircularQueue(n): initialize a circular array with size n to store elements
-	2.boolean isFull(): return true if the array is full
-	3.boolean isEmpty(): return true if there is no element in the array
-	4.void enqueue(element): add an element to the queue
-	5.int dequeue(): pop an element from the queue
+    1.CircularQueue(n): initialize a circular array with size n to store elements
+    2.boolean isFull(): return true if the array is full
+    3.boolean isEmpty(): return true if there is no element in the array
+    4.void enqueue(element): add an element to the queue
+    5.int dequeue(): pop an element from the queue
 Example
 Example 1:
-	Input:
-		CircularQueue(5)
-		isFull()
-		isEmpty()
-		enqueue(1)
-		enqueue(2)
-		dequeue()
-	Output:
-		["false","true","1"]
+    Input:
+        CircularQueue(5)
+        isFull()
+        isEmpty()
+        enqueue(1)
+        enqueue(2)
+        dequeue()
+    Output:
+        ["false","true","1"]
 Example 2:
-	Input:
-		CircularQueue(5)
-		isFull()
-		isEmpty()
-		enqueue(1)
-		enqueue(2)
-		dequeue()
-		dequeue()
-		enqueue(1)
-		enqueue(2)
-		enqueue(3)
-		enqueue(4)
-		enqueue(5)
-		isFull()
-		dequeue()
-		dequeue()
-		dequeue()
-		dequeue()
-		dequeue()
-	Output:
-		["false","true","1","2","true","1","2","3","4","5"]
+    Input:
+        CircularQueue(5)
+        isFull()
+        isEmpty()
+        enqueue(1)
+        enqueue(2)
+        dequeue()
+        dequeue()
+        enqueue(1)
+        enqueue(2)
+        enqueue(3)
+        enqueue(4)
+        enqueue(5)
+        isFull()
+        dequeue()
+        dequeue()
+        dequeue()
+        dequeue()
+        dequeue()
+    Output:
+        ["false","true","1","2","true","1","2","3","4","5"]
 Notice
-	it's guaranteed in the test cases we won't call enqueue if it's full and we also won't call dequeue if it's empty. 
-	So it's ok to raise exception in scenarios described above.
+    it's guaranteed in the test cases we won't call enqueue if it's full and we also won't call dequeue if it's empty. 
+    So it's ok to raise exception in scenarios described above.
 ***/
 public class CircularQueue {
 	// fields
@@ -53,9 +53,9 @@ public class CircularQueue {
 	// constructor
     public CircularQueue(int n) {
         this.array = new int[n];
-		this.front = 0;
-		this.rear = 0;
-		this.size = 0;
+        this.front = 0;
+        this.rear = 0;
+        this.size = 0;
     }
     /**
      * @return:  return true if the array is full
@@ -77,12 +77,12 @@ public class CircularQueue {
      */
     public void enqueue(int element) {
         if (isFull()) {
-			throw new Exception("It is full!");
-		}
-		
-		rear = (front + size) % array.length;
-		array[rear] = element;
-		size++;
+            throw new Exception("It is full!");
+        }
+
+        rear = (front + size) % array.length;
+        array[rear] = element;
+        size++;
     }
 
     /**
@@ -90,13 +90,13 @@ public class CircularQueue {
      */
     public int dequeue() {
         if (isEmpty()) {
-			throw new RuntimeException("It is Empty!");			
-		}
-		
-		int element = array[front];
-		front = (front + 1) % array.length;
-		size--;
-		
-		return element;
+            throw new RuntimeException("It is Empty!");			
+        }
+
+        int element = array[front];
+        front = (front + 1) % array.length;
+        size--;
+
+        return element;
     }
 }
