9e3f7447cc3ac9ef763bd0256bfdf3a93f688cb7
Marco Chen <haisheng.chen4u@foxmail.com>
2021-03-08 22:27:10 -0600

    Update [LintCode-1786]Pub Sub Pattern.java

--- source_code/[LintCode-1786]Pub Sub Pattern.java
+++ source_code/[LintCode-1786]Pub Sub Pattern.java
@@ -3,25 +3,26 @@
 ub/Sub pattern is a wide used pattern in system design. In this problem, you need to implement a pub/sub pattern to support user subscribes on a specific channel and get notification messages from subscribed channels.
 
 There are 3 methods you need to implement:
-	-subscribe(channel, user_id): Subscribe the given user to the given channel.
-	-unsubscribe(channel, user_id): Unsubscribe the given user from the given channel.
-	-publish(channel, message): You need to publish the message to the channel so that everyone subscribed on the channel will receive this message. Call PushNotification.notify(user_id, message) to push the message to the user.
+    -subscribe(channel, user_id): Subscribe the given user to the given channel.
+    -unsubscribe(channel, user_id): Unsubscribe the given user from the given channel.
+    -publish(channel, message): You need to publish the message to the channel so that everyone subscribed on the channel will receive this message. Call PushNotification.notify(user_id, message) to push the message to the user.
 Example
-	subscribe("group1",  1)
-	publish("group1", "hello")
-		>> user 1 received "Hello"
-	subscribe("group1", 2)
-	publish("group1", "thank you")
-		>> user 1 received "thank you"
-		>> user 2 received "thank you"
-	unsubscribe("group2", 3)
-		>> user 3 is not in group2, do nothing
-	unsubscribe("group1", 1)
-	publish("group1", "thank you very much")
-		>> user 2 received "thank you very much"
-	publish("group2", "are you ok?")
-		>> # you don't need to push this message to anyone
-	If there are more than 1 user subscribed on the same channel, it doesn't matter the order of time users receiving the message. It's ok if you push the message to user 2 before user 1.
+    subscribe("group1",  1)
+    publish("group1", "hello")
+        >> user 1 received "Hello"
+    subscribe("group1", 2)
+    publish("group1", "thank you")
+        >> user 1 received "thank you"
+        >> user 2 received "thank you"
+    unsubscribe("group2", 3)
+        >> user 3 is not in group2, do nothing
+    unsubscribe("group1", 1)
+    publish("group1", "thank you very much")
+        >> user 2 received "thank you very much"
+    publish("group2", "are you ok?")
+        >> # you don't need to push this message to anyone
+    If there are more than 1 user subscribed on the same channel, it doesn't matter the order of time users receiving the message. 
+    It's ok if you push the message to user 2 before user 1.
 
 ***/
 /* Definition of PushNotification
@@ -30,8 +31,10 @@ Example
  *  };
  */
 public class PubSubPattern {
-	//fields
-	private Map<String, Set<Integer>> channelMap; // <Channel_Name, Set[subscriber_id]> key pair
+    // fields
+    private Map<String, Set<Integer>> channelMap; // <Channel_Name, Set[subscriber_id]> key pair
+    
+    // constructor
     public PubSubPattern(){
     	this.channelMap = new HashMap<String, Set<Integer>>();
     }
@@ -43,9 +46,9 @@ public class PubSubPattern {
      */
     public void subscribe(String channel, int user_id) {
         channelMap.putIfAbsent(channel, new HashSet<Integer>());
-		
-		Set<Integer> subscribers = channelMap.get(channel);
-		subscribers.add(user_id);
+
+        Set<Integer> subscribers = channelMap.get(channel);
+        subscribers.add(user_id);
     }
 
     /**
@@ -55,11 +58,11 @@ public class PubSubPattern {
      */
     public void unsubscribe(String channel, int user_id) {
         if (!channelMap.containsKey(channel)) {
-			return;
-		}
-		
-		Set<Integer> subscribers = channelMap.get(channel);
-		subscribers.remove(user_id);
+            return;
+        }
+
+        Set<Integer> subscribers = channelMap.get(channel);
+        subscribers.remove(user_id);
     }
 
     /**
@@ -69,12 +72,12 @@ public class PubSubPattern {
      */
     public void publish(String channel, String message) {
         if (!channelMap.containsKey(channel)) {
-			return;
-		}
-		
-		Set<Integer> subscribers = channelMap.get(channel);
-		for (int subscriber : subscribers) {
-			PushNotification.notify(subscriber, message);
-		}
+            return;
+        }
+
+        Set<Integer> subscribers = channelMap.get(channel);
+        for (int subscriber : subscribers) {
+            PushNotification.notify(subscriber, message);
+	}
     }
 }
