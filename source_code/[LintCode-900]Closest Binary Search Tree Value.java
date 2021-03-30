5f8e139ca4e8531f8755f589a4068ab98de07a67
Marco Chen <haisheng.chen4u@foxmail.com>
2021-03-30 12:18:16 -0500

    Update [LintCode-900]Closest Binary Search Tree Value.java

--- source_code/[LintCode-900]Closest Binary Search Tree Value.java
+++ source_code/[LintCode-900]Closest Binary Search Tree Value.java
@@ -3,17 +3,17 @@
 Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
 
 Example
-	Example1
-		Input: root = {5,4,9,2,#,8,10} and target = 6.124780
-		Output: 5
+    Example1
+        Input: root = {5,4,9,2,#,8,10} and target = 6.124780
+        Output: 5
 
-	Example2
-		Input: root = {3,2,4,1} and target = 4.142857
-		Output: 4
+    Example2
+        Input: root = {3,2,4,1} and target = 4.142857
+        Output: 4
 
 Notice
-	Given target value is a floating point.
-	You are guaranteed to have only one unique value in the BST that is closest to the target.
+    Given target value is a floating point.
+    You are guaranteed to have only one unique value in the BST that is closest to the target.
 ***/
 /**
  * Definition of TreeNode:
@@ -27,47 +27,47 @@ Notice
  * }
  */
 public class Solution {
+
+    // helper methods
+    private TreeNode findLowerClosest(TreeNode node, double target) {
+        if (node == null) {
+            return null;
+        }
+
+        TreeNode result = null;
+        if (node.val < target) {
+            result =  findLowerClosest(node.right, target);
+        }
+        else {
+            return findLowerClosest(node.left, target);
+        }
+
+        if (result != null) {
+            return result;
+        }
+	    
+        return node;
+    }
 	
-	// helper methods
-	private TreeNode findLowerClosest(TreeNode node, double target) {
-	    if (node == null) {
-	        return null;
-	    }
-	    
-	    TreeNode result = null;
-	    if (node.val < target) {
-	        result =  findLowerClosest(node.right, target);
-	    }
-	    else {
-	        return findLowerClosest(node.left, target);
-	    }
-	    
-	    if (result != null) {
-	        return result;
-	    }
-	    
-	    return node;
-	}
-	
-	private TreeNode findUpperClosest(TreeNode node, double target) {
-	    if (node == null) {
-	        return null;
-	    }
-	    
-	    TreeNode result = null; 
-	    if (node.val >= target) {
-	        result = findUpperClosest(node.left, target);
-	    }
-	    else {
-	        return findUpperClosest(node.right, target);
-	    }
-	    
-	    if (result != null) {
-	        return result;
-	    }
-	    
-	    return node;
-	}
+    private TreeNode findUpperClosest(TreeNode node, double target) {
+        if (node == null) {
+            return null;
+        }
+
+        TreeNode result = null; 
+        if (node.val >= target) {
+            result = findUpperClosest(node.left, target);
+        }
+        else {
+            return findUpperClosest(node.right, target);
+        }
+
+        if (result != null) {
+            return result;
+        }
+
+        return node;
+    }
 	
     /**
      * @param root: the given BST
@@ -76,26 +76,26 @@ public class Solution {
      */
     public int closestValue(TreeNode root, double target) {
         // check corner case
-		if (root == null) {
-			return 0;
-		}
-		
-		TreeNode lowerClosest = findLowerClosest(root, target);
-		TreeNode upperClosest = findUpperClosest(root, target);
-		
-		if (lowerClosest == null) {
-		    return upperClosest.val;
-		}
-		
-		if (upperClosest == null) {
-		    return lowerClosest.val;
-		}
-		
-		if (Math.abs(target - lowerClosest.val) < Math.abs(upperClosest.val - target)) {
-			return lowerClosest.val;
-		}
-		else {
-			return upperClosest.val;
-		}		
+        if (root == null) {
+            return 0;
+        }
+
+        TreeNode lowerClosest = findLowerClosest(root, target);
+        TreeNode upperClosest = findUpperClosest(root, target);
+
+        if (lowerClosest == null) {
+            return upperClosest.val;
+        }
+
+        if (upperClosest == null) {
+            return lowerClosest.val;
+        }
+
+        if (Math.abs(target - lowerClosest.val) < Math.abs(upperClosest.val - target)) {
+            return lowerClosest.val;
+        }
+        else {
+            return upperClosest.val;
+        }		
     }
 }
