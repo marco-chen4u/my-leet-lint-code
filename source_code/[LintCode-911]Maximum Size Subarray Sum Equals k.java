f2e05482eb38f8c4a5e7123396dbfffb37e7a915
Marco Chen <haisheng.chen4u@foxmail.com>
2021-03-29 22:46:43 -0500

    Update [LintCode-911]Maximum Size Subarray Sum Equals k.java

--- source_code/[LintCode-911]Maximum Size Subarray Sum Equals k.java
+++ source_code/[LintCode-911]Maximum Size Subarray Sum Equals k.java
@@ -3,23 +3,23 @@
 Given an array nums and a target value k, find the maximum length of a subarray that sums to k. If there isn't one, return 0 instead.
 
 Example
-	Example1
-		Input:  nums = [1, -1, 5, -2, 3], k = 3
-		Output: 4
-		Explanation:
-			because the subarray [1, -1, 5, -2] sums to 3 and is the longest.
+    Example1
+        Input:  nums = [1, -1, 5, -2, 3], k = 3
+        Output: 4
+        Explanation:
+            because the subarray [1, -1, 5, -2] sums to 3 and is the longest.
 
-	Example2
-		Input: nums = [-2, -1, 2, 1], k = 1
-		Output: 2
-		Explanation:
-			because the subarray [-1, 2] sums to 1 and is the longest.
+    Example2
+        Input: nums = [-2, -1, 2, 1], k = 1
+        Output: 2
+        Explanation:
+            because the subarray [-1, 2] sums to 1 and is the longest.
 
 Challenge
-	Can you do it in O(n) time?
+    Can you do it in O(n) time?
 
 Notice
-	The sum of the entire nums array is guaranteed to fit within the 32-bit signed integer range.
+    The sum of the entire nums array is guaranteed to fit within the 32-bit signed integer range.
 ***/
 //version-1
 public class Solution {
@@ -30,27 +30,27 @@ public class Solution {
      */
     public int maxSubArrayLen(int[] nums, int k) {
         int result = 0;
+	
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();// <sum, index> key-value pair
+        int sum = 0;
+        map.put(sum, 0);
 		
-		Map<Integer, Integer> map = new HashMap<Integer, Integer>();// <sum, index> key-value pair
-		int sum = 0;
-		map.put(sum, 0);
+        int size = nums.length;
 		
-		int size = nums.length;
-		
-		for (int i = 1; i <= size; i++) {
-			sum += nums[i - 1];
+        for (int i = 1; i <= size; i++) {
+            sum += nums[i - 1];
 			
-			if (map.containsKey(sum - k)) {
-				result = Math.max(result, i - map.get(sum - k));
-			}
+            if (map.containsKey(sum - k)) {
+                result = Math.max(result, i - map.get(sum - k));
+            }
 			
-			if (!map.containsKey(sum)) {// to get the longest length, so keep the oldest index
-				map.put(sum, i);
-			}
-		}
+            if (!map.containsKey(sum)) {// to get the longest length, so keep the oldest index
+                map.put(sum, i);
+            }
+        }
 		
-		return result;
-	}
+        return result;
+    }
 }
 
 //version-2
