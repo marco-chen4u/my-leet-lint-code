5e9b15397bbb355ac326fdcff64020bf7308ea73
Marco Chen <haisheng.chen4u@foxmail.com>
2021-03-29 23:05:41 -0500

    Update [LintCode-915]Inorder Predecessor in BST.java

--- source_code/[LintCode-915]Inorder Predecessor in BST.java
+++ source_code/[LintCode-915]Inorder Predecessor in BST.java
@@ -3,16 +3,16 @@
 Given a binary search tree and a node in it, find the in-order predecessor of that node in the BST.
 
 Example
-	Example1
-		Input: root = {2,1,3}, p = 1
-		Output: null
+    Example1
+        Input: root = {2,1,3}, p = 1
+        Output: null
 
-	Example2
-		Input: root = {2,1}, p = 2
-		Output: 1
+    Example2
+        Input: root = {2,1}, p = 2
+        Output: 1
 
 Notice
-	If the given node has no in-order predecessor in the tree, return null
+    If the given node has no in-order predecessor in the tree, return null
 ***/
 /**
  * Definition of TreeNode:
@@ -34,26 +34,26 @@ public class Solution {
      */
     public TreeNode inorderPredecessor(TreeNode root, TreeNode p) {
         // check corner case
-		if (root == null || p == null) {
-			return null;
-		}
+        if (root == null || p == null) {
+            return null;
+        }
 		
-		TreeNode pre = null;
-		TreeNode current = root;
+        TreeNode pre = null;
+        TreeNode current = root;
 		
-		while (current != null) {
-			if (current.val >= p.val) {
-				current = current.left;
-			}
-			else {
-				if (pre == null || current.val > pre.val) {
-					pre = current;
-				}
+        while (current != null) {
+            if (current.val >= p.val) {
+                current = current.left;
+            }
+            else {
+                if (pre == null || current.val > pre.val) {
+                    pre = current;
+                }
 				
-				current = current.right;
-			}
-		}
+                current = current.right;
+            }
+        }
 		
-		return pre;
+        return pre;
     }
 }
