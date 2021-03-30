6b814b229e673cf3d2dced4ef351e812b02abe3f
Marco Chen <haisheng.chen4u@foxmail.com>
2021-03-30 11:50:21 -0500

    Update [LintCode-894]Pancake Sorting.java

--- source_code/[LintCode-894]Pancake Sorting.java
+++ source_code/[LintCode-894]Pancake Sorting.java
@@ -7,19 +7,19 @@ Unlike a traditional sorting algorithm, which attempts to sort with the fewest c
 the goal is to sort the sequence in as few reversals as possible.
 
 Example
-Example 1:
-	Input: array = [6,11,10,12,7,23,20]
+    Example 1:
+        Input: array = [6,11,10,12,7,23,20]
 
-Example 2:
-	Input: array = [4, 2, 3]
+    Example 2:
+        Input: array = [4, 2, 3]
 
-	Explanation:
-		flip(array, 2)
-		flip(array, 1)
-	
+        Explanation:
+            flip(array, 2)
+            flip(array, 1)
+
 Notice
-	You only call flip function.
-	Don't allow to use any sort function or other sort methods.
+    You only call flip function.
+    Don't allow to use any sort function or other sort methods.
 
 Java：you can call FlipTool.flip(arr, i)
 C++： you can call FlipTool::flip(arr, i)
@@ -31,33 +31,33 @@ public class Solution {
      * @return: nothing
      */
     public void pancakeSort(int[] array) {
-		// check corner case
-		if (array == null || array.length <= 1) {
-			return;
-		}
-		
-		int left = 0; 
-		int right = array.length - 1;
-		while (left <= right) {
-			int max = Integer.MIN_VALUE;
-			int index = left;
-			
-			for (int = left; i <= right; i++) {
-				if (max < array[i]) {
-					max = array[i];
-					index = i;
-				}
-			}
-			
-			if (index == left) {
-				FlipTool.flip(array, right);
-			}
-			else if (index != right){
-				FlipTool.flip(array, index);
-				FlipTool.flip(array, right);
-			}
-			
-			right--;
-		}
-	}	
+        // check corner case
+        if (array == null || array.length <= 1) {
+            return;
+        }
+
+        int left = 0; 
+        int right = array.length - 1;
+        while (left <= right) {
+            int max = Integer.MIN_VALUE;
+            int index = left;
+
+            for (int = left; i <= right; i++) {
+                if (max < array[i]) {
+                    max = array[i];
+                    index = i;
+                }
+            }
+
+            if (index == left) {
+                FlipTool.flip(array, right);
+            }
+            else if (index != right){
+                FlipTool.flip(array, index);
+                FlipTool.flip(array, right);
+            }
+	
+            right--;
+        }
+    }	
 }
