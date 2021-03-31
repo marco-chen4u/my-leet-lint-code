c17c69368fe3c9d53482be88500bf42433c769f9
Marco Chen <haisheng.chen4u@foxmail.com>
2021-03-31 12:43:22 -0500

    Update [LintCode-892]Alien Dictionary.java

--- source_code/[LintCode-892]Alien Dictionary.java
+++ source_code/[LintCode-892]Alien Dictionary.java
@@ -8,26 +8,26 @@ Derive the order of letters in this language.
 
 Example
 Example 1:
-	Input：["wrt","wrf","er","ett","rftt"]
-	Output："wertf"
-	Explanation：
-		from "wrt"and"wrf" ,we can get 't'<'f'
-		from "wrt"and"er" ,we can get 'w'<'e'
-		from "er"and"ett" ,we can get 'r'<'t'
-		from "ett"and"rtff" ,we can get 'e'<'r'
-		So return "wertf"
+    Input：["wrt","wrf","er","ett","rftt"]
+    Output："wertf"
+    Explanation：
+        from "wrt"and"wrf" ,we can get 't'<'f'
+        from "wrt"and"er" ,we can get 'w'<'e'
+        from "er"and"ett" ,we can get 'r'<'t'
+        from "ett"and"rtff" ,we can get 'e'<'r'
+        So return "wertf"
 
 Example 2:
-	Input：["z","x"]
-	Output："zx"
-	Explanation：
-		from "z" and "x"，we can get 'z' < 'x'
-		So return "zx"
+    Input：["z","x"]
+    Output："zx"
+    Explanation：
+        from "z" and "x"，we can get 'z' < 'x'
+        So return "zx"
 Notice
-	You may assume all letters are in lowercase.
-	You may assume that if a is a prefix of b, then a must appear before b in the given dictionary.
-	If the order is invalid, return an empty string.
-	There may be multiple valid order of letters, return the smallest in normal lexicographical order
+    You may assume all letters are in lowercase.
+    You may assume that if a is a prefix of b, then a must appear before b in the given dictionary.
+    If the order is invalid, return an empty string.
+    There may be multiple valid order of letters, return the smallest in normal lexicographical order
 ***/
 public class Solution {
     
