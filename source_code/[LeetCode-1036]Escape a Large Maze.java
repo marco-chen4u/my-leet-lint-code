f895790e08fc541a679f43613d13aa7601d37873
Marco Chen <haisheng.chen4u@foxmail.com>
2021-03-31 12:12:36 -0500

    Update [LeetCode-1036]Escape a Large Maze.java

--- source_code/[LeetCode-1036]Escape a Large Maze.java
+++ source_code/[LeetCode-1036]Escape a Large Maze.java
@@ -2,42 +2,43 @@
 * LeetCode 1036. Escape a Large Maze
 In a 1 million by 1 million grid, the coordinates of each grid square are (x, y) with 0 <= x, y < 10^6.
 
-We start at the source square and want to reach the target square.  Each move, we can walk to a 4-directionally adjacent square in the grid that isn't in the given list of blocked squares.
+We start at the source square and want to reach the target square.  
+Each move, we can walk to a 4-directionally adjacent square in the grid that isn't in the given list of blocked squares.
 
 Return true if and only if it is possible to reach the target square through a sequence of moves.
 
  
 
 Example 1:
-	Input: blocked = [[0,1],[1,0]], source = [0,0], target = [0,2]
-	Output: false
-	Explanation: 
-		The target square is inaccessible starting from the source square,is because we can't walk outside the grid.
+    Input: blocked = [[0,1],[1,0]], source = [0,0], target = [0,2]
+    Output: false
+    Explanation: 
+        The target square is inaccessible starting from the source square,is because we can't walk outside the grid.
 
 
 Example 2:
-	Input: blocked = [], source = [0,0], target = [999999,999999]
-	Output: true
-	Explanation: 
-	Because there are no blocked cells, it's possible to reach the target square.
+    Input: blocked = [], source = [0,0], target = [999999,999999]
+    Output: true
+    Explanation: 
+    Because there are no blocked cells, it's possible to reach the target square.
 
 Note:
-	1.0 <= blocked.length <= 200
-	2.blocked[i].length == 2
-	3.0 <= blocked[i][j] < 10^6
-	3.source.length == target.length == 2
-	5.0 <= source[i][j], target[i][j] < 10^6
-	6.source != target
+    1.0 <= blocked.length <= 200
+    2.blocked[i].length == 2
+    3.0 <= blocked[i][j] < 10^6
+    3.source.length == target.length == 2
+    5.0 <= source[i][j], target[i][j] < 10^6
+    6.source != target
 ***/
 //version-1: BFS
 class Solution {
-	// fields
-	private final int[] directionX = new int[]{0, 0, 1, -1};
+    // fields
+    private final int[] directionX = new int[]{0, 0, 1, -1};
     private final int[] directionY = new int[]{1, -1, 0, 0};
     private final int LIMIT = 1000000;// 1 million
     private final int MAX_SIZE = 19900;// the maximum size of area that 200-blocks could hold the wall
 	
-	private final String SEPERATOR = "#";
+    private final String SEPERATOR = "#";
 
     public boolean isEscapePossible(int[][] blocked, int[] source, int[] target) {
         // check corner cases
@@ -83,40 +84,40 @@ class Solution {
                 int nextY = y + directionY[i];
 
                 if (!isBounded(nextX, nextY)) {
-					continue;
-				}
-				
-				String nextPos = getValue(nextX, nextY);
-				if (blockSet.contains(nextPos) ||
-					visited.contains(nextPos)) {
-					continue;
-				}
-				
-				count++;
-				visited.add(nextPos);
-				queue.offer(nextPos);
+                    continue;
+                }
+
+                String nextPos = getValue(nextX, nextY);
+                if (blockSet.contains(nextPos) ||
+                    visited.contains(nextPos)) {
+                    continue;
+                }
+
+                count++;
+                visited.add(nextPos);
+                queue.offer(nextPos);
             }
         }
 
         return true;
     }
 	
-	private boolean isBounded(int x, int y) {
-		return x >= 0 && x < LIMIT &&
-				y >= 0 && y < LIMIT;
-	}
+    private boolean isBounded(int x, int y) {
+        return x >= 0 && x < LIMIT &&
+                y >= 0 && y < LIMIT;
+    }
 	
-	private String getValue(int x, int y) {
-		return x + SEPERATOR + y;
-	}
+    private String getValue(int x, int y) {
+        return x + SEPERATOR + y;
+    }
 
     private Set<String> getBlockSet(int[][] blocked) {
-		Set<String> blockSet = new HashSet<String>();
-		for (int[] blockPos : blocked) {
-			String value = getValue(blockPos[0], blockPos[1]);
-			blockSet.add(value);// keep in mind the area that the blocks get built
-		}
+        Set<String> blockSet = new HashSet<String>();
+        for (int[] blockPos : blocked) {
+            String value = getValue(blockPos[0], blockPos[1]);
+            blockSet.add(value);// keep in mind the area that the blocks get built
+        }
 
-		return blockSet;
+        return blockSet;
     }
 }
