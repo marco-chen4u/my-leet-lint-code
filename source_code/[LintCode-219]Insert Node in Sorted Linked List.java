695ff7206c018d03bff1f533af8b27234e355b9b
Marco Chen <haisheng.chen4u@foxmail.com>
2021-04-05 14:49:32 -0500

    Update [LintCode-219]Insert Node in Sorted Linked List.java

--- source_code/[LintCode-219]Insert Node in Sorted Linked List.java
+++ source_code/[LintCode-219]Insert Node in Sorted Linked List.java
@@ -50,18 +50,6 @@ public class Solution {
 }
 
 // solution-2
-/**
- * Definition for ListNode
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-
 public class Solution {
     /**
      * @param head: The head of linked list.
