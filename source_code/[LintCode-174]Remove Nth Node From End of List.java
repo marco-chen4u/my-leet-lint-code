bebe786f07820c4b46672d8bc1178c421a949467
Marco Chen <haisheng.chen4u@foxmail.com>
2021-04-08 12:47:45 -0500

    Update [LintCode-174]Remove Nth Node From End of List.java

--- source_code/[LintCode-174]Remove Nth Node From End of List.java
+++ source_code/[LintCode-174]Remove Nth Node From End of List.java
@@ -3,19 +3,19 @@
 Given a linked list, remove the nth node from the end of list and return its head.
 
 Example
-	Example 1:
-		Input: list = 1->2->3->4->5->null， n = 2
-		Output: 1->2->3->5->null
+    Example 1:
+        Input: list = 1->2->3->4->5->null， n = 2
+        Output: 1->2->3->5->null
 
-	Example 2:
-		Input:  list = 5->4->3->2->1->null, n = 2
-		Output: 5->4->3->1->null
+    Example 2:
+        Input:  list = 5->4->3->2->1->null, n = 2
+        Output: 5->4->3->1->null
 
 Challenge
-	Can you do it without getting the length of the linked list?
+    Can you do it without getting the length of the linked list?
 
 Notice
-	The minimum number of nodes in list is n.
+    The minimum number of nodes in list is n.
 ***/
 /**
  * Definition for ListNode
