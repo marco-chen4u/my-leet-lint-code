ffd21e29af1f9b9d980de2e9df161149c1f0bc4b
Marco Chen <haisheng.chen4u@foxmail.com>
2021-04-27 23:34:43 -0500

    Update [LeetCode-837]New 21 Game.java

--- source_code/[LeetCode-837]New 21 Game.java
+++ source_code/[LeetCode-837]New 21 Game.java
@@ -2,52 +2,54 @@
 * LeetCode 837. New 21 Game
 Alice plays the following game, loosely based on the card game "21".
 
-Alice starts with 0 points, and draws numbers while she has less than K points.  During each draw, she gains an integer number of points randomly from the range [1, W], where W is an integer.  Each draw is independent and the outcomes have equal probabilities.
+Alice starts with 0 points, and draws numbers while she has less than K points.  
+During each draw, she gains an integer number of points randomly from the range [1, W], where W is an integer.  
+Each draw is independent and the outcomes have equal probabilities.
 
 Alice stops drawing numbers when she gets K or more points.  What is the probability that she has N or less points?
 
 Example 1:
-	Input: N = 10, K = 1, W = 10
-	Output: 1.00000
-	Explanation:  Alice gets a single card, then stops.
+    Input: N = 10, K = 1, W = 10
+    Output: 1.00000
+    Explanation:  Alice gets a single card, then stops.
 
 Example 2:
-	Input: N = 6, K = 1, W = 10
-	Output: 0.60000
-	Explanation:  Alice gets a single card, then stops.
-	In 6 out of W = 10 possibilities, she is at or below N = 6 points.
+    Input: N = 6, K = 1, W = 10
+    Output: 0.60000
+    Explanation:  Alice gets a single card, then stops.
+    In 6 out of W = 10 possibilities, she is at or below N = 6 points.
 
 Example 3:
-	Input: N = 21, K = 17, W = 10
-	Output: 0.73278
-	
+    Input: N = 21, K = 17, W = 10
+    Output: 0.73278
+
 Note:
-	1. 0 <= K <= N <= 10000
-	2. 1 <= W <= 10000
-	3. Answers will be accepted as correct if they are within 10^-5 of the correct answer.
-	4. The judging time limit has been reduced for this question.
+    1. 0 <= K <= N <= 10000
+    2. 1 <= W <= 10000
+    3. Answers will be accepted as correct if they are within 10^-5 of the correct answer.
+    4. The judging time limit has been reduced for this question.
 ***/
 //version-1: recursion， Runtime Error(Time Limit Exceeded)
 class Solution {
     public double new21Game(int N, int K, int W) {
         return helper(0, N, K, W);
     }
-	
-	// helper method
-	private double helper(int startingPoints, int N, int K, int W) {
-		
-		// check corner case
-		if (startingPoints >= K) {// time to terminate
-			return (startingPoints <= N) ? 1 : 0;// probability (100% or 0%)
-		}
-		
-		double sum = 0;
-		for (int i = 1; i <= W; i++) {
-			sum += 1.0/W * helper(startingPoints + i, N, K, W);
-		}
-		
-		return sum;
-	}
+
+    // helper method
+    private double helper(int startingPoints, int N, int K, int W) {
+
+        // check corner case
+        if (startingPoints >= K) {// time to terminate
+            return (startingPoints <= N) ? 1 : 0;// probability (100% or 0%)
+        }
+
+        double sum = 0;
+        for (int i = 1; i <= W; i++) {
+            sum += 1.0/W * helper(startingPoints + i, N, K, W);
+        }
+
+        return sum;
+    }
 }
 
 //version-2: DP
@@ -59,41 +61,41 @@ class Solution {
 */
 class Solution {
     public double new21Game(int N, int K, int W) {
-		// state
-		double[] dp = new double[N + 1];
-		
-		// initialize
-		Arrays.fill(dp, 0);
-		dp[0] = 1;
-		
-		// function
-		double sum = 0;
-		for (int i = 1; i <= N; i++) {
-			
-			/*for (int j = i - W; j <= i - 1; j++) {
-				if (j < 0) {// check corner case, j < 0, that means it's within a W-size window scope
-					continue;
-				}
-				
-				if (j >= K) {
-					continue;
-				}
-				
-				dp[i] += dp[j] * 1.0/W;
-			}*/
-			
-			//sliding window process
-			sum += (i - 1 < K) ? dp[i - 1] : 0;
-			sum -= (i - W - 1 >= 0) ? dp[i - W - 1] : 0;
-			
-			dp[i] = sum * 1.0/W;
-		}
-		
-		double result = 0;
-		for (int i = K; i <= N; i++) {
-			result += dp[i];
-		}
-		
-		return result;
-	}
+        // state
+        double[] dp = new double[N + 1];
+
+        // initialize
+        Arrays.fill(dp, 0);
+        dp[0] = 1;
+
+        // function
+        double sum = 0;
+        for (int i = 1; i <= N; i++) {
+
+            /*for (int j = i - W; j <= i - 1; j++) {
+                if (j < 0) {// check corner case, j < 0, that means it's within a W-size window scope
+                    continue;
+                }
+
+                if (j >= K) {
+                    continue;
+                }
+
+                dp[i] += dp[j] * 1.0/W;
+            }*/
+
+            //sliding window process
+            sum += (i - 1 < K) ? dp[i - 1] : 0;
+            sum -= (i - W - 1 >= 0) ? dp[i - W - 1] : 0;
+
+            dp[i] = sum * 1.0/W;
+        }
+
+        double result = 0;
+        for (int i = K; i <= N; i++) {
+            result += dp[i];
+        }
+
+        return result;
+    }
 }
