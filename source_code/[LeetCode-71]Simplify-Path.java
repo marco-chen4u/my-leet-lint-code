1ba6e5d66dcfb4bf60a68bbe7ba5c0470442f0c0
Marco Chen <haisheng.chen4u@foxmail.com>
2021-04-27 23:23:54 -0500

    Update [LeetCode-71]Simplify-Path.java

--- source_code/[LeetCode-71]Simplify-Path.java
+++ source_code/[LeetCode-71]Simplify-Path.java
@@ -7,31 +7,31 @@ In a UNIX-style file system, a period . refers to the current directory. Further
 Note that the returned canonical path must always begin with a slash /, and there must be only a single slash / between two directory names. The last directory name (if it exists) must not end with a trailing /. Also, the canonical path must be the shortest string representing the absolute path.
 
 Example 1:
-	Input: "/home/"
-	Output: "/home"
-	Explanation: Note that there is no trailing slash after the last directory name.
+    Input: "/home/"
+    Output: "/home"
+    Explanation: Note that there is no trailing slash after the last directory name.
 
 Example 2:
-	Input: "/../"
-	Output: "/"
-	Explanation: Going one level up from the root directory is a no-op, as the root level is the highest level you can go.
+    Input: "/../"
+    Output: "/"
+    Explanation: Going one level up from the root directory is a no-op, as the root level is the highest level you can go.
 
 Example 3:
-	Input: "/home//foo/"
-	Output: "/home/foo"
-	Explanation: In the canonical path, multiple consecutive slashes are replaced by a single one.
+    Input: "/home//foo/"
+    Output: "/home/foo"
+    Explanation: In the canonical path, multiple consecutive slashes are replaced by a single one.
 
 Example 4:
-	Input: "/a/./b/../../c/"
-	Output: "/c"
+    Input: "/a/./b/../../c/"
+    Output: "/c"
 
 Example 5:
-	Input: "/a/../../b/../c//.//"
-	Output: "/c"
+    Input: "/a/../../b/../c//.//"
+    Output: "/c"
 
 Example 6:
-	Input: "/a//b////c/d//././/.."
-	Output: "/a/b/c"
+    Input: "/a//b////c/d//././/.."
+    Output: "/a/b/c"
 ***/
 public class Solution {
     private final String SEPARATOR = "/";
