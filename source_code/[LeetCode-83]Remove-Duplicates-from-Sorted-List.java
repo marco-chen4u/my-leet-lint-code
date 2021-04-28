913a68f35f72f7e98e721b2efd07c1027f2d0ea2
Marco Chen <haisheng.chen4u@foxmail.com>
2021-04-27 23:35:48 -0500

    Update [LeetCode-83]Remove-Duplicates-from-Sorted-List.java

--- source_code/[LeetCode-83]Remove-Duplicates-from-Sorted-List.java
+++ source_code/[LeetCode-83]Remove-Duplicates-from-Sorted-List.java
@@ -3,12 +3,12 @@
 Given a sorted linked list, delete all duplicates such that each element appear only once.
 
 Example1
-	Input: 1->1->2
-	Output: 1->2
+    Input: 1->1->2
+    Output: 1->2
 
 Example2
-	Input: 1->1->2->3->3
-	Output: 1->2->3
+    Input: 1->1->2->3->3
+    Output: 1->2->3
 ***/
 /**
  * Definition for singly-linked list.
