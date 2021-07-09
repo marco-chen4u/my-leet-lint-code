6c58dda71e5f80c432bf3fcd53789366857d9eb4
Marco Chen <haisheng.chen4u@foxmail.com>
2021-07-08 21:37:41 -0500

    Update [LintCode-1534]Convert-Binary-Search-Tree-to-Sorted-Doubly-Linked-List.java

--- source_code/[LintCode-1534]Convert-Binary-Search-Tree-to-Sorted-Doubly-Linked-List.java
+++ source_code/[LintCode-1534]Convert-Binary-Search-Tree-to-Sorted-Doubly-Linked-List.java
@@ -1,7 +1,8 @@
 /***
 * LintCode 1534. Convert Binary Search Tree to Sorted Doubly Linked List
 Description
-Convert a BST to a sorted circular doubly-linked list in-place. Think of the left and right pointers as synonymous to the previous and next pointers in a doubly-linked list.
+Convert a BST to a sorted circular doubly-linked list in-place. 
+Think of the left and right pointers as synonymous to the previous and next pointers in a doubly-linked list.
 
 Let's take the following BST as an example, it may help you understand the problem better:
 
@@ -11,9 +12,14 @@ Let's take the following BST as an example, it may help you understand the probl
      / \
     1   3
 
-We want to transform this BST into a circular doubly linked list. Each node in a doubly linked list has a predecessor and successor. For a circular doubly linked list, the predecessor of the first element is the last element, and the successor of the last element is the first element.
+We want to transform this BST into a circular doubly linked list. 
+Each node in a doubly linked list has a predecessor and successor. 
+For a circular doubly linked list, 
+the predecessor of the first element is the last element, 
+and the successor of the last element is the first element.
 
-The figure below shows the circular doubly linked list for the BST above. The "head" symbol means the node it points to is the smallest element of the linked list.
+The figure below shows the circular doubly linked list for the BST above. 
+The "head" symbol means the node it points to is the smallest element of the linked list.
 
     head
      |
@@ -23,9 +29,13 @@ The figure below shows the circular doubly linked list for the BST above. The "h
    | |_____________________| |
    |_________________________|
    
-Specifically, we want to do the transformation in place. After the transformation, the left pointer of the tree node should point to its predecessor, and the right pointer should point to its successor. We should return the pointer to the first element of the linked list.
+Specifically, we want to do the transformation in place. 
+After the transformation, the left pointer of the tree node should point to its predecessor, 
+and the right pointer should point to its successor. 
+We should return the pointer to the first element of the linked list.
 
-The figure below shows the transformed BST. The solid line indicates the successor relationship, while the dashed line means the predecessor relationship.
+The figure below shows the transformed BST. 
+The solid line indicates the successor relationship, while the dashed line means the predecessor relationship.
 
 Example 1:
     Input: {4,2,5,1,3}
