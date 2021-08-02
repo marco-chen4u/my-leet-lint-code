acb88beebc80c85cc31530bd7255c6306dfca285
Marco Chen <haisheng.chen4u@foxmail.com>
2021-08-01 23:47:40 -0500

    Update [LintCode-610]Two Sum - Difference equals to target.java

--- source_code/[LintCode-610]Two Sum - Difference equals to target.java
+++ source_code/[LintCode-610]Two Sum - Difference equals to target.java
@@ -19,45 +19,7 @@ Example
 Notice
     It's guaranteed there is only one available solution
 ***/
-//version-1:two pointers(slow + fast pointers), sliding window
-public class Solution {
-    /**
-     * @param nums: an array of Integer
-     * @param target: an integer
-     * @return: [num1, num2] (num1 < num2)
-     */
-    public int[] twoSum7(int[] nums, int target) {
-        int DEFAULT_VALUE = -1;
-        int[] DEFAULT_RESULT = new int[] {DEFAULT_VALUE, DEFAULT_VALUE};
-        // check corner cases
-        if (nums == null || nums.length <=1) {
-            return DEFAULT_RESULT;
-        }
-
-        // normal case
-        int size = nums.length;
-        target = Math.abs(target);
-        
-        int i;// slow pointer
-        int j;// fast pointer
-
-        for (i = 0, j = i + 1; i < size; i++) {
-            j = (j <= i) ? i + 1 : j;// make sure fast pointer is right ahead of the slow on
-
-            while (j < size && nums[j] - nums[i] < target) {
-                j++;
-            }
-
-            if (j < size && nums[j] - nums[i] == target) {
-                return new int[]{nums[i], nums[j]};
-            }
-        }
-
-        return DEFAULT_RESULT;
-    }
-}
-
-//version-2:
+//version-1: 2 pointers, sliding window, time complexity: O(nlogn)[/w sorting], process /w O(n)
 public class Solution {
     // inner class
     class Pair {
@@ -114,7 +76,7 @@ public class Solution {
     }
 }
 
-// version-3:
+// version-2: map, space complexity O(n)
 public class Solution {
     /**
      * @param nums: an array of Integer
@@ -141,5 +103,65 @@ public class Solution {
         }
         
         return result;
+    }
+}
+
+updated:
+/***
+* LintCode 610. Two Sum - Difference equals to target
+Given an sorted array of integers, find two numbers that their difference equals to a target value.
+Return a list with two number like [num1, num2] that the difference of num1 and num2 equals to target value, and num1 is less than num2.
+
+Example 1:
+    Input: nums = [2, 7, 15, 24], target = 5 
+     Output: [2, 7] 
+    Explanation:
+        (7 - 2 = 5)
+Example 2:
+    Input: nums = [1, 1], target = 0
+     Output: 1, 1] 
+    Explanation:
+        (1 - 1 = 0)
+        
+Notice
+    It's guaranteed there is only one available solution
+    Note: Requires O(1) space complexity to comple
+***/
+
+//version-3:two pointers(slow + fast pointers), sliding window
+public class Solution {
+    /**
+     * @param nums: an array of Integer
+     * @param target: an integer
+     * @return: [num1, num2] (num1 < num2)
+     */
+    public int[] twoSum7(int[] nums, int target) {
+        int DEFAULT_VALUE = -1;
+        int[] DEFAULT_RESULT = new int[] {DEFAULT_VALUE, DEFAULT_VALUE};
+        // check corner cases
+        if (nums == null || nums.length <=1) {
+            return DEFAULT_RESULT;
+        }
+
+        // normal case
+        int size = nums.length;
+        target = Math.abs(target);
+        
+        int i;// slow pointer
+        int j;// fast pointer
+
+        for (i = 0, j = i + 1; i < size; i++) {
+            j = (j <= i) ? i + 1 : j;// make sure fast pointer is right ahead of the slow on
+
+            while (j < size && nums[j] - nums[i] < target) {
+                j++;
+            }
+
+            if (j < size && nums[j] - nums[i] == target) {
+                return new int[]{nums[i], nums[j]};
+            }
+        }
+
+        return DEFAULT_RESULT;
     }
 }
