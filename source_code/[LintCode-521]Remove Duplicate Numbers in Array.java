0fbf86f9f9652df795966c2416375defb8391db1
Marco Chen <haisheng.chen4u@foxmail.com>
2021-08-02 22:09:57 -0500

    Update [LintCode-521]Remove Duplicate Numbers in Array.java

--- source_code/[LintCode-521]Remove Duplicate Numbers in Array.java
+++ source_code/[LintCode-521]Remove Duplicate Numbers in Array.java
@@ -3,36 +3,37 @@
 Given an array of integers, remove the duplicate numbers in it.
 
 You should:
-	Do it in place in the array.
-	Move the unique numbers to the front of the array.
-	Return the total number of the unique numbers.
+    Do it in place in the array.
+    Move the unique numbers to the front of the array.
+    Return the total number of the unique numbers.
 
 Example
-	Example 1:
-		Input:
-		nums = [1,3,1,4,4,2]
-		Output:
-			[1,3,4,2,?,?]
-			4
-		Explanation:
-			Move duplicate integers to the tail of nums => nums = [1,3,4,2,?,?].
-			Return the number of unique integers in nums => 4.
-			Actually we don't care about what you place in ?, 
-			 we only care about the part which has no duplicate integers.
+    Example 1:
+        Input:
+        nums = [1,3,1,4,4,2]
+        Output:
+            [1,3,4,2,?,?]
+            4
+        Explanation:
+            Move duplicate integers to the tail of nums => nums = [1,3,4,2,?,?].
+            Return the number of unique integers in nums => 4.
+            Actually we don't care about what you place in ?, 
+            we only care about the part which has no duplicate integers.
 
-	Example 2:
-		Input:
-		nums = [1,2,3]
-		Output:
-			[1,2,3]
-			3
+    Example 2:
+        Input:
+        nums = [1,2,3]
+        Output:
+            [1,2,3]
+            3
 
 Challenge
-	Do it in O(n) time complexity.
-	Do it in O(nlogn) time without extra space.
+    Do it in O(n) time complexity.
+    Do it in O(nlogn) time without extra space.
 Notice
-	You don't need to keep the original order of the integers.
+    You don't need to keep the original order of the integers.
 ***/
+//version-1: slow-fast pointers, without set/map, fair solution
 public class Solution {
     /**
      * @param nums: an array of integers
@@ -59,3 +60,6 @@ public class Solution {
         return i + 1;
     }
 }
+
+// version-2: two pointers, without set/map, better solution
+...
