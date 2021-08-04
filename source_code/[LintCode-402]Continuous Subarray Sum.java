f6de3eaa5e0968338eb1aaa2c87b7f5363233371
Marco Chen <haisheng.chen4u@foxmail.com>
2021-08-04 17:56:14 -0500

    Update [LintCode-402]Continuous Subarray Sum.java

--- source_code/[LintCode-402]Continuous Subarray Sum.java
+++ source_code/[LintCode-402]Continuous Subarray Sum.java
@@ -5,13 +5,13 @@ Your code should return the index of the first number and the index of the last 
 (If their are duplicate answer, return the minimum one in lexicographical order)
 
 Example
-	Example 1:
-		Input: [-3,1,3,-3,4]
-		Output: [1, 4]
-	Example 2:
-		Input: [0,1,0,1]
-		Output: [0, 3]
-		Explanation: The minimum one in lexicographical order.
+    Example 1:
+        Input: [-3,1,3,-3,4]
+        Output: [1, 4]
+    Example 2:
+        Input: [0,1,0,1]
+        Output: [0, 3]
+        Explanation: The minimum one in lexicographical order.
 ***/
 public class Solution {
     /*
@@ -19,48 +19,48 @@ public class Solution {
      * @return: A list of integers includes the index of the first number and the index of the last number
      */
     public List<Integer> continuousSubarraySum(int[] A) {
-		List<Integer> result = new ArrayList<Integer>();
-		
-		if (isEmpty(A)) {
-			return result;
-		}
-		
-		int size = A.length;
-		int sum = 0;
-		int maxSum = Integer.MIN_VALUE;
-		
-		int left, right;
-		left = right = 0;
-		
-		// initialize
-		int start, end;
-		start = end = 0;
-		
-		for (int i = 0; i < size; i++) {
-			if (sum < 0) {
-				sum = 0;
-				left = i;
-			}
-			
-			sum += A[i];
-			
-			if (sum > maxSum) {
-				maxSum = sum;
-				right = i;
-				
-				start = left;
-				end = right;
-			}
-		}
+        List<Integer> result = new ArrayList<Integer>();
 
-		result.add(start);
-		result.add(end);
-		
-		return result;
-	}
-	
-	// helper method
-	private boolean isEmpty(int[] nums) {
-	    return (nums == null || nums.length == 0);
-	}
+        if (isEmpty(A)) {
+            return result;
+        }
+
+        int size = A.length;
+        int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        int left, right;
+        left = right = 0;
+
+        // initialize
+        int start, end;
+        start = end = 0;
+
+        for (int i = 0; i < size; i++) {
+            if (sum < 0) {
+                sum = 0;
+                left = i;
+            }
+
+            sum += A[i];
+
+            if (sum > maxSum) {
+                maxSum = sum;
+                right = i;
+
+                start = left;
+                end = right;
+            }
+        }
+
+        result.add(start);
+        result.add(end);
+
+        return result;
+    }
+
+    // helper method
+    private boolean isEmpty(int[] nums) {
+        return (nums == null || nums.length == 0);
+    }
 }	
