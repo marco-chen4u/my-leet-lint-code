1e75ab40564070dd82aabaadb1a0f79a107092bb
Marco Chen <haisheng.chen4u@foxmail.com>
2021-08-21 12:41:01 -0500

    Update [LintCode-120]Word-Ladder.java

--- source_code/[LintCode-120]Word-Ladder.java
+++ source_code/[LintCode-120]Word-Ladder.java
@@ -32,7 +32,8 @@ Example 2:
   Explanation:
     "hit"->"hot"->"dot"->"dog"->"cog"
 ***/
-//version-1: BFS
+//version-1: BFS,time-complexity: O(|V| + |E|), space-complexity: O(|V|)
+//note: |V| : number of vertics, |E| : number of edge 
 public class Solution {
     /*
      * @param start: a string
@@ -112,5 +113,89 @@ public class Solution {
         }
 
         return diffCount == 1;
+    }
+}
+
+//version-2: BFS, more faster, time-complexity: O(|V| + |E|), space-complexity: O(|V|)
+//note: |V| : number of vertics, |E| : number of edge 
+public class Solution {
+    /*
+     * @param start: a string
+     * @param end: a string
+     * @param dict: a set of string
+     * @return: An integer
+     */
+    public int ladderLength(String start, String end, Set<String> dict) {
+        int step = 0;
+        // check corner cases
+        if (start == null || start.isEmpty() || end == null || end.isEmpty() || 
+            dict == null || dict.isEmpty()) {
+            return step;
+        }
+
+        if (start.equals(end)) {
+            return step;
+        }
+
+        Queue<String> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+
+        dict.add(end);
+
+        queue.offer(start);
+        visited.add(start);
+
+        step = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            step +=1;
+            for (int i = 0; i < size; i++) {
+                String current = queue.poll();
+                for (String next : getNeighbours(current, dict)) {
+                    if (visited.contains(next)) {
+                        continue;
+                    }
+
+                    if (next.equals(end)) {
+                        return step;
+                    }
+
+                    queue.offer(next);
+                    visited.add(next);
+                }
+            }
+        }
+
+        return step;
+    }
+
+    // helper method
+    private List<String> getNeighbours(String word, Set<String> dict) {
+        List<String> result = new ArrayList<>();
+        
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            for (int i = 0; i < word.length(); i++) {
+                char currentChar = word.charAt(i);
+
+                if (ch == currentChar) {
+                    continue;
+                }
+
+                String nextWord = getReplaceWord(word, i, ch);
+
+                if (dict.contains(nextWord)) {
+                    result.add(nextWord);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private String getReplaceWord(String word, int index, char ch) {
+        char[] chars = word.toCharArray();
+        chars[index] = ch;
+        return String.valueOf(chars);
     }
 }
