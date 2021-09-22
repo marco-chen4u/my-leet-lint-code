28108290468bfc3bd07c0a3fce133618994bd973
Marco Chen <haisheng.chen4u@foxmail.com>
2021-09-21 22:40:25 -0500

    Update [LintCode-574]Build Post Office.java

--- source_code/[LintCode-574]Build Post Office.java
+++ source_code/[LintCode-574]Build Post Office.java
@@ -5,22 +5,22 @@ find the place to build a post office, the distance that post office to all the 
 
 Return the smallest distance. Return -1 if it is not possible.
 
-Example
-	Example 1:
-		Input：[[0,1,0,0],[1,0,1,1],[0,1,0,0]]
-		Output： 6
-		Explanation：
-			Placing a post office at (1,1), the distance that post office to all the house sum is smallest.
-	Example 2:
-		Input：[[0,1,0],[1,0,1],[0,1,0]]
-		Output： 4
-		Explanation：
-			Placing a post office at (1,1), the distance that post office to all the house sum is smallest.
+
+Example 1:
+    Input：[[0,1,0,0],[1,0,1,1],[0,1,0,0]]
+    Output： 6
+    Explanation：
+        Placing a post office at (1,1), the distance that post office to all the house sum is smallest.
+Example 2:
+    Input：[[0,1,0],[1,0,1],[0,1,0]]
+    Output： 4
+    Explanation：
+        Placing a post office at (1,1), the distance that post office to all the house sum is smallest.
 	
 Notice
-	You can pass through house and empty.
-	You only build post office on an empty.
-	The distance between house and the post office is Manhattan distance
+    You can pass through house and empty.
+    You only build post office on an empty.
+    The distance between house and the post office is Manhattan distance
 ***/
 public class Solution {
     // fields
