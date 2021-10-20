c6619c417b8baef682e7be2839b0e374a7e592f8
Marco Chen <haisheng.chen4u@foxmail.com>
2021-10-19 21:44:46 -0500

    Update [LintCode-804]Number of Distinct Islands II-1.java

--- source_code/[LintCode-804]Number of Distinct Islands II-1.java
+++ source_code/[LintCode-804]Number of Distinct Islands II-1.java
@@ -179,3 +179,147 @@ public class Solution {
         return sb.toString();
     }
 }
+
+//version-2: BFS
+public class Solution {
+    // inner class
+    class Point implements Comparable<Point>{
+        // fields
+        int x;
+        int y;
+
+        // constructor
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int compareTo(Point other) {
+            if (this.x != other.x) {
+                return this.x - other.x;
+            }
+            else {
+                return this.y - other.y;
+            }
+        }
+    }
+
+    // fields
+    private final int SEA = 0;
+    private final int ISLAND = 1;
+
+    private final int[] DIRECTION_X = new int[]{0, 1, -1, 0};
+    private final int[] DIRECTION_Y = new int[]{1, 0, 0, -1};
+    private final int[][] ROTATION = new int[][]{{1, 1}, {-1, 1}, {-1, -1}, {1, -1}};
+    private final String SEPERATOR = "-";
+
+    private int n; // row size
+    private int m; // column size
+
+    /**
+     * @param grid: the 2D grid
+     * @return: the number of distinct islands
+     */
+    public int numDistinctIslands2(int[][] grid) {
+        // check corner cases
+        if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) {
+            return 0;
+        }
+
+        // initialize
+        n = grid.length;
+        m = grid[0].length;
+
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == SEA) {
+                    continue;
+                }
+
+                List<Point> islands = new ArrayList<>();
+                findIslands(grid, i, j, islands);
+                set.add(getUnique(islands));
+            }
+        }
+
+        return set.size();
+    }
+
+    // helper methods
+    private void findIslands(int[][] grid, int x, int y, List<Point> islands) {
+
+        Queue<Point> queue = new LinkedList<Point>();
+        queue.offer(new Point(x, y));
+        grid[x][y] = SEA;
+
+        while (!queue.isEmpty()) {
+            Point current = queue.poll();
+            islands.add(current);
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = current.x + DIRECTION_X[i];
+                int nextY = current.y + DIRECTION_Y[i];
+
+                if (nextX < 0 || nextX >= n ||
+                    nextY < 0 || nextY >= m ||
+                    grid[nextX][nextY] == SEA) {
+                    continue;
+                }
+
+                queue.offer(new Point(nextX, nextY));
+                grid[nextX][nextY] = SEA;
+            }
+        }
+    }	
+
+
+    private String getUnique(List<Point> islands) {
+        List<String> mappingIslands = new ArrayList<String>();
+
+        for (int i = 0; i < 4; i++) {
+            List<Point> l1 = new ArrayList<Point>();
+            List<Point> l2 = new ArrayList<Point>();
+
+            for (Point island : islands) {
+
+                int x = island.x;
+                int y = island.y;
+
+                // rotation by 180 degree
+                Point island1 = new Point(ROTATION[i][0] * x , ROTATION[i][1] * y);
+                // rotation by 90 or 270 degree
+                Point island2 = new Point(ROTATION[i][0] * y , ROTATION[i][1] * x);
+
+                l1.add(island1);
+                l2.add(island2);
+            }
+
+            mappingIslands.add(getString(l1));
+            mappingIslands.add(getString(l2));
+        }
+
+        Collections.sort(mappingIslands);
+
+        return mappingIslands.get(0);
+    }
+
+    private String getString(List<Point> islands) {
+        Collections.sort(islands);
+
+        StringBuilder sb = new StringBuilder();
+
+        int x = islands.get(0).x;
+        int y = islands.get(0).y;		
+
+        for (Point island : islands) {
+            // move(pan) this island to the coordinate point
+            int posX = island.x - x;
+            int posY = island.y - y;
+            sb.append(posX + SEPERATOR + posY + SEPERATOR);
+        }
+
+        return sb.toString();
+    }
+}
