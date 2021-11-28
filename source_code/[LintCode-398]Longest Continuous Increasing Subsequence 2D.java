494d336b7e0ea59184e852a801fc0c79bd0a748e
Marco Chen <haisheng.chen4u@foxmail.com>
2021-11-28 14:42:10 -0600

    Update [LintCode-398]Longest Continuous Increasing Subsequence 2D.java

--- source_code/[LintCode-398]Longest Continuous Increasing Subsequence 2D.java
+++ source_code/[LintCode-398]Longest Continuous Increasing Subsequence 2D.java
@@ -6,33 +6,33 @@ Find the longest increasing continuous subsequence in this matrix and return the
 The longest increasing continuous subsequence here can start at any position and go up/down/left/right.
 
 Example
-	Example 1:
-		Input: 
-			[
-			  [1, 2, 3, 4, 5],
-			  [16,17,24,23,6],
-			  [15,18,25,22,7],
-			  [14,19,20,21,8],
-			  [13,12,11,10,9]
-			]
-		Output: 25
-		Explanation: 1 -> 2 -> 3 -> 4 -> 5 -> ... -> 25 (Spiral from outside to inside.)
+    Example 1:
+        Input: 
+            [
+              [1, 2, 3, 4, 5],
+              [16,17,24,23,6],
+              [15,18,25,22,7],
+              [14,19,20,21,8],
+              [13,12,11,10,9]
+            ]
+        Output: 25
+        Explanation: 1 -> 2 -> 3 -> 4 -> 5 -> ... -> 25 (Spiral from outside to inside.)
 
-	Example 2:
-		Input: 
-			[
-			  [1, 2],
-			  [5, 3]
-			]
-		Output: 4
-		Explanation: 1 -> 2 -> 3 -> 5
+    Example 2:
+        Input: 
+            [
+              [1, 2],
+              [5, 3]
+            ]
+        Output: 4
+        Explanation: 1 -> 2 -> 3 -> 5
 
 Challenge
-	Assume that it is a N x M matrix. Solve this problem in O(NM) time and memory.
+    Assume that it is a N x M matrix. Solve this problem in O(NM) time and memory.
 ***/
 // version-1: Memorized Search
 public class Solution {
-	// fields
+    // fields
     int n;// row size
     int m;// column size
     boolean [][] visited;
