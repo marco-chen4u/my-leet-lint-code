014711cdced2b0843f6117d00cfc0d6a20d6187b
Marco Chen <haisheng.chen4u@foxmail.com>
2021-11-29 11:41:36 -0600

    Update [LintCode-17]Subsets.java

--- source_code/[LintCode-17]Subsets.java
+++ source_code/[LintCode-17]Subsets.java
@@ -32,35 +32,51 @@ Notice
     Elements in a subset must be in non-descending order.
     The solution set must not contain duplicate subsets.
 ***/
+//version-1: DFS
 public class Solution {
-    // hleper method
-    private void findSubsets(List<List<Integer>> result, List<Integer> subset, int[] nums, int startIndex) {
-        result.add(new ArrayList<Integer>(subset));
-        
-        for (int i = startIndex; i < nums.length; i++) {
-            subset.add(nums[i]);
-            findSubsets(result, subset, nums, i + 1);
-            subset.remove(subset.size() - 1);
-        }
-    }
-    
     /**
      * @param nums: A set of numbers
      * @return: A list of lists
      */
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        List<Integer> subset = new ArrayList<Integer>();
+        List<List<Integer>> result = new LinkedList<>();
         // check corner case
-        if (nums == null || nums.length == 0) {
-            result.add(subset);
+        if (nums == null) {
             return result;
         }
-        
+
+        if (nums.length == 0) {
+            result.add(new ArrayList<>());
+            return result;
+        }
+
+        // regular case
+        List<Integer> subset = new LinkedList<>();
+
         Arrays.sort(nums);
-        
-        findSubsets(result, subset, nums, 0);
-        
+
+        search(nums, 0, subset, result);
+
         return result;
     }
+
+    // helper method
+    private void search(int[] nums, int startIndex, List<Integer> subset, List<List<Integer>> result) {
+        result.add(new ArrayList<>(subset));
+        
+        // corner case
+        if (startIndex == nums.length) {
+            return;
+        }
+
+        // regularcase
+        for (int i = startIndex; i < nums.length; i++) {
+            subset.add(nums[i]);
+            search(nums, i + 1, subset, result);
+            subset.remove(subset.size() - 1);
+        }
+    }
 }
+
+//version-2: enumeration
+// to do
