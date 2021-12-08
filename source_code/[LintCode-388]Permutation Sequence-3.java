081e8c8ab227be1a75708638c147ed3ee914ab18
Marco Chen <haisheng.chen4u@foxmail.com>
2021-12-07 19:53:05 -0600

    Update [LintCode-388]Permutation Sequence-3.java

--- source_code/[LintCode-388]Permutation Sequence-3.java
+++ source_code/[LintCode-388]Permutation Sequence-3.java
@@ -3,20 +3,21 @@
 Given n and k, find the kth permutation of the dictionary order in the full permutation of n.
 1 ≤ n ≤ 9
 
-Example
-	Example 1: 
-		Input: n = 3, k = 4
-		Output: "231"
-		Explanation:
-			For n = 3, all permutations are listed as follows:
-			"123", "132", "213", "231", "312", "321"
+Example 1: 
+    Input: n = 3, k = 4
+    Output: "231"
+    Explanation:
+        For n = 3, all permutations are listed as follows:
+            "123", "132", "213", "231", "312", "321"
 
-	Example 2:
-		Input: n = 1, k = 1
-		Output: "1"
+Example 2:
+    Input: n = 1, k = 1
+    Output: "1"
+
 Challenge
-	O(n*k) in time complexity is easy, can you do it in O(n^2) or less?
+    O(n*k) in time complexity is easy, can you do it in O(n^2) or less?
 ***/
+//version-1: digit enumation-mathimatic(n!-factorial) calculation
 public class Solution {
     /**
      * @param n: n
@@ -60,6 +61,130 @@ public class Solution {
             result *= i;
         }
         
+        return result;
+    }
+}
+
+//version-2: DFS
+public class Solution {
+    /**
+     * @param n: n
+     * @param k: the k th permutation
+     * @return: return the k-th permutation
+     */
+    public String getPermutation(int n, int k) {
+        String result = null;
+        // check corner case
+        if (n <= 0 || k < 1) {
+            return result;
+        }
+
+        // regular case
+        // initialize
+        List<String> nums = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            nums.add(String.valueOf(i));
+        }
+
+        StringBuilder permutation = new StringBuilder();
+
+        k = k -1;// 0 based index
+        dfs(nums, n, k, permutation);        
+
+        result = permutation.toString();
+
+        return result;
+    }
+
+    // helper methods
+    private void dfs(List<String> nums, int n, int k, StringBuilder permutation){
+        if (permutation.length() == n) {
+            return;
+        }
+
+        int size = nums.size();
+        int groupCount = getFactorial(size - 1);
+
+        int indexPos = k / groupCount;
+        String digit = nums.remove(indexPos);
+        permutation.append(digit);
+
+        k = k % groupCount;
+
+        dfs(nums, n, k, permutation);
+    }
+
+    private int getFactorial(int x) {
+        int result = 1;
+
+        for (int i = 1; i <= x; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+}
+
+//version-3: DFS
+public class Solution {
+    /**
+     * @param n: n
+     * @param k: the k th permutation
+     * @return: return the k-th permutation
+     */
+    public String getPermutation(int n, int k) {
+        String result = null;
+
+        // check corner case
+        if (n < 1 || k < 1) {
+            return result;
+        }
+
+        // regular case
+        // initialize
+        List<Character> nums = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            nums.add(Character.forDigit(i, 10));
+        }
+
+        k = k -1; // 0 base index
+
+        StringBuilder permutation = new StringBuilder();
+        
+        dfs(nums, n, k, permutation);
+
+        result = permutation.toString();
+
+        return result;
+    }
+
+    // helper methods
+    private void dfs(List<Character> nums, int n, int k, StringBuilder permutation) {
+        // check corner case
+        if (permutation.length() == n) {
+            return;
+        }
+
+        // normal case
+        int size = nums.size();
+        int groupCount = getFactorial(size - 1);
+        int indexPos = k / groupCount;
+        char digit = nums.remove(indexPos);
+
+        permutation.append(digit);
+
+        k = k % groupCount;
+
+        dfs(nums, n, k, permutation);
+    }
+
+    private int getFactorial(int x) {
+        int result = 1;
+
+        for (int i = 1; i <= x; i++) {
+            result *= i;
+        }
+
         return result;
     }
 }
