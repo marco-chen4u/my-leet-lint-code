7f4af3e5a76273c2636bd4bac22e0d85e27d36d6
Marco Chen <haisheng.chen4u@foxmail.com>
2021-12-11 20:16:17 -0600

    Update [LintCode-425]Letter Combinations of a Phone Number.java

--- source_code/[LintCode-425]Letter Combinations of a Phone Number.java
+++ source_code/[LintCode-425]Letter Combinations of a Phone Number.java
@@ -1,4 +1,4 @@
-/*** LintCode 425.Letter Combinations of a Phone Number
+/*** LintCode 425. Letter Combinations of a Phone Number
 Given a digit string excluded '0' and '1', 
 return all possible letter combinations that the number could represent.
 A mapping of digit to letters (just like on the telephone buttons) is given below.
@@ -12,19 +12,20 @@ A mapping of digit to letters (just like on the telephone buttons) is given belo
     7     8    9
    PQRS  TUV  WXYZ
 ----------------------------------   
-Example
-	Example 1:
-	Input: "23"
-	Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
-	Explanation: 
-		'2' could be 'a', 'b' or 'c'
-		'3' could be 'd', 'e' or 'f'
+
+Example 1:
+    Input: "23"
+    Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+    Explanation: 
+        '2' could be 'a', 'b' or 'c'
+        '3' could be 'd', 'e' or 'f'
 
 Example 2:
-	Input: "5"
-	Output: ["j", "k", "l"]
+    Input: "5"
+    Output: ["j", "k", "l"]
+
 Notice
-	Although the answer above is in lexicographical order, your answer could be in any order you want.
+    Although the answer above is in lexicographical order, your answer could be in any order you want.
 ***/
 //version-1: DFS
 public class Solution {
