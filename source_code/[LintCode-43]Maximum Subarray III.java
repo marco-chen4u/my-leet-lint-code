37eba8daa85c6489ce59ff25025f938d3637bfc0
Marco Chen <haisheng.chen4u@foxmail.com>
2021-12-26 22:10:29 -0600

    Update [LintCode-43]Maximum Subarray III.java

--- source_code/[LintCode-43]Maximum Subarray III.java
+++ source_code/[LintCode-43]Maximum Subarray III.java
@@ -4,24 +4,24 @@ Given an array of integers and a number k, find k non-overlapping subarrays whic
 The number in each subarray should be contiguous.
 Return the largest sum.
 
-Example
-    Example 1
-        Input: 
-            List = [1,2,3]
-            k = 1
-        Output: 6
-        Explanation: 1 + 2 + 3 = 6
+Example 1
+    Input: 
+        List = [1,2,3]
+        k = 1
+    Output: 6
+    Explanation: 1 + 2 + 3 = 6
 
-    Example 2
-        Input:
-            List = [-1,4,-2,3,-2,3]
-            k = 2
-        Output: 8
-        Explanation: 4 + (3 + -2 + 3) = 8
+Example 2
+    Input:
+        List = [-1,4,-2,3,-2,3]
+        k = 2
+    Output: 8
+    Explanation: 4 + (3 + -2 + 3) = 8
 
 Notice
     The subarray should contain at least one number
 ***/
+//version-1: DP
 public class Solution {
     // field 
     private int DEFAULT_MIN =  Integer.MIN_VALUE;
