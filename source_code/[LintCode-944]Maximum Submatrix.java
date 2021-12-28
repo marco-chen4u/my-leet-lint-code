1b9d45869daf849c48e6faaeb162728dd272b9c3
Marco Chen <haisheng.chen4u@foxmail.com>
2021-12-28 12:35:13 -0600

    Update [LintCode-944]Maximum Submatrix.java

--- source_code/[LintCode-944]Maximum Submatrix.java
+++ source_code/[LintCode-944]Maximum Submatrix.java
@@ -33,6 +33,7 @@ Example 2
             [1,1,1]
         ]
 ***/
+//version-1: 2D=>1D array, then do prefixSum calculation
 public class Solution {
 	
     // fields
@@ -107,5 +108,84 @@ public class Solution {
         }
 
         return max;
+    }
+}
+
+//version-2: 2D=>1D array transformation, then use prefixSum calculation to get max subarray sum from the 1D array 
+public class Solution {
+    // fields
+    private int n;// row size
+    private int m;// column size
+    /**
+     * @param matrix: the given matrix
+     * @return: the largest possible sum
+     */
+    public int maxSubmatrix(int[][] matrix) {
+        // check corner cases
+        if (matrix == null || matrix.length == 0 || 
+            matrix[0] == null || matrix[0].length == 0) {
+            return 0;
+        }
+
+        n = matrix.length; // row size
+        m = matrix[0].length; // column size
+
+        int[][] prefixSumMaxtrix = getPrefixSumMaxtrix(matrix);
+
+        int result = Integer.MIN_VALUE;
+
+        for (int up = 0; up < n; up++) {
+            for (int down = up + 1; down <= n; down++) {
+                int[] nums = getCalculation(prefixSumMaxtrix, up, down);
+
+                int value = getMaxSubArraySum(nums);
+
+                result = Math.max(value, result);
+            }
+        }
+
+        return result;
+    }
+
+    // helper methods
+    private int[][] getPrefixSumMaxtrix(int[][] matrix) {
+        int[][] result = new int[n + 1][m];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < m; j++) {
+                result[i][j] = result[i - 1][j] + matrix[i - 1][j];
+            }
+        }
+
+        return result;
+    }
+
+    private int[] getCalculation(int[][] prefixSumMaxtrix, int up, int down) {
+        int[] nums = new int[m];
+        Arrays.fill(nums, 0);
+
+        int[] upperNums = prefixSumMaxtrix[up];
+        int[] lowerNums = prefixSumMaxtrix[down];
+
+        for (int i = 0; i < m; i++) {
+            nums[i] = lowerNums[i] - upperNums[i];
+        }
+
+        return nums;
+    }
+
+    private int getMaxSubArraySum(int[] nums) {
+        int prefixSum = 0;
+        int minSum = 0;
+        int result = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            prefixSum += num;
+
+            result = Math.max(result, prefixSum - minSum);
+            minSum = Math.min(minSum, prefixSum);
+        }
+
+        return result;
     }
 }
