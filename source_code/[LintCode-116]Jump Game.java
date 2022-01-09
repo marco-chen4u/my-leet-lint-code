3f7aa79606cd4350f9b6430401323a39d9e666e0
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-09 14:16:14 -0600

    Update [LintCode-116]Jump Game.java

--- source_code/[LintCode-116]Jump Game.java
+++ source_code/[LintCode-116]Jump Game.java
@@ -5,16 +5,16 @@ Each element in the array represents your maximum jump length at that position.
 Determine if you are able to reach the last index.
 
 Example
-A = [2,3,1,1,4], return true.
-A = [3,2,1,0,4], return false.
+    A = [2,3,1,1,4], return true.
+    A = [3,2,1,0,4], return false.
 
 Notice
-	This problem have two method which is Greedy and Dynamic Programming.
-	The time complexity of Greedy method is O(n).
-	The time complexity of Dynamic Programming method is O(n^2).
-	We manually set the small data set to allow you pass the test in both ways.
-	This is just to let you learn how to use this problem in dynamic programming ways. 
-	If you finish it in dynamic programming ways, you can try greedy method to make it accept again.
+    This problem have two method which is Greedy and Dynamic Programming.
+    The time complexity of Greedy method is O(n).
+    The time complexity of Dynamic Programming method is O(n^2).
+    We manually set the small data set to allow you pass the test in both ways.
+    This is just to let you learn how to use this problem in dynamic programming ways. 
+    If you finish it in dynamic programming ways, you can try greedy method to make it accept again.
 ***/
 
 public class Solution {
@@ -27,12 +27,12 @@ public class Solution {
         if (A == null || A.length == 0) {
             return false;
         }
-        
+
         int n = A.length;
         boolean[] dp = new boolean[n];
-        
+
         dp[0] = true;
-        
+
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 if (dp[j] && A[j] + j >= i) {
@@ -41,7 +41,7 @@ public class Solution {
                 }
             }
         }
-        
+
         return dp[n - 1];
      }
 }
