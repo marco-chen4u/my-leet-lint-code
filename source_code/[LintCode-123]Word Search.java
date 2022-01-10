d3d145bee262d4a625c885c86d908d56d31a6aa6
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-09 20:35:59 -0600

    Update and rename [LintCode-123]Word Search-2.java to [LintCode-123]Word Search.java

--- source_code/[LintCode-123]Word Search.java
+++ source_code/[LintCode-123]Word Search.java
@@ -102,7 +102,85 @@ public class Solution {
     } 
 }
 
-// version-2: prefix Map
+/ verison-2 : DFS-2
+public class Solution {
+    private final int[] DIRECTION_X = new int[] {0, 1, -1, 0};
+    private final int[] DIRECTION_Y = new int[] {1, 0, 0, -1};
+    private final char MARKED = '#';    
+    private int n; // row size
+    private int m; // column size
+    
+    /**
+     * @param board: A list of lists of character
+     * @param word: A string
+     * @return: A boolean
+     */
+    public boolean exist(char[][] board, String word) {
+        // check corner case
+        if (board == null || board.length == 0) {
+            return false;
+        }        
+        if (board[0] == null || board[0].length == 0) {
+            return false;
+        }        
+        if (word == null || word.length() == 0) {
+            return false;
+        }
+        
+        // initialize
+        n = board.length;
+        m = board[0].length;
+        boolean[] result = new boolean[1];
+        
+        // function
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                
+                search(result, board, i, j, 
+                        word,
+                        0);
+                
+                if (result[0]) {
+                    break;
+                }
+            }
+        }
+        
+        return result[0];
+    }
+    
+    // helper methods
+    private void search(boolean[] result, char[][] board, int i, int j, 
+                            String word, int startIndex) {
+        // check corner case
+        if (startIndex == word.length()) {
+            result[0] = true;
+            return;
+        }
+        
+        if (i < 0 || i >= n || 
+            j < 0 || j >= m || board[i][j] != word.charAt(startIndex)) {
+            return;
+        }
+        
+        char tmp = board[i][j];
+        board[i][j] = MARKED;// maked it as visited
+        
+        for (int k = 0; k < 4; k++) {
+            int x = i + DIRECTION_X[k];
+            int y = j + DIRECTION_Y[k];
+            
+            search(result, board, x, y, word, startIndex + 1);
+            if (result[0]) {
+                break;
+            }
+        }
+        
+        board[i][j] = tmp; // reverse it back for backtracking
+    } 
+}
+
+// version-3: prefix Map
 public class Solution {
     private final int[] DIRECTION_X = new int[] {0, 1, -1, 0};
     private final int[] DIRECTION_Y = new int[] {1, 0, 0, -1};
