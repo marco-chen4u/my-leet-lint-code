a035aea527df4feb257eb8a5e01b9897cde696f2
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-09 20:19:01 -0600

    Update [LintCode-123]Word Search-3.java

--- source_code/[LintCode-123]Word Search-3.java
+++ source_code/[LintCode-123]Word Search-3.java
@@ -3,24 +3,24 @@
 Given a 2D board and a word, find if the word exists in the grid.
 The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. 
 The same letter cell may not be used more than once.
-Example
-	Example 1:
-		Input：["ABCE","SFCS","ADEE"]，"ABCCED"
-		Output：true
-		Explanation：
-			[    
-				 A B C E
-				 S F C S 
-				 A D E E
-			]
-			(0,0)A->(0,1)B->(0,2)C->(1,2)C->(2,2)E->(2,1)D
 
-	Example 2:
-		Input：["z"]，"z"
-		Output：true
-		Explanation：
-			[ z ]
-			(0,0)z
+Example 1:
+    Input：["ABCE","SFCS","ADEE"]，"ABCCED"
+    Output：true
+    Explanation：
+        [    
+             A B C E
+             S F C S 
+             A D E E
+        ]
+        (0,0)A->(0,1)B->(0,2)C->(1,2)C->(2,2)E->(2,1)D
+
+Example 2:
+    Input：["z"]，"z"
+    Output：true
+    Explanation：
+        [ z ]
+        (0,0)z
 ***/
 // verison-1 : DFS-2
 public class Solution {
@@ -52,6 +52,7 @@ public class Solution {
         m = board[0].length;
         boolean[] result = new boolean[1];
         
+        // function
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 
