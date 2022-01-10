b0043f37fc0b7288afe426fef94674596b0931de
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-09 22:24:52 -0600

    Update [LintCode-597]Subtree with Maximum Average.java

--- source_code/[LintCode-597]Subtree with Maximum Average.java
+++ source_code/[LintCode-597]Subtree with Maximum Average.java
@@ -2,26 +2,26 @@
 * LintCode 597. Subtree with Maximum Average
 Given a binary tree, find the subtree with maximum average. Return the root of the subtree.
 
-Example
-	Example 1
-		Input：
-			 1
-		   /   \
-		 -5     11
-		 / \   /  \
-		1   2 4    -2 
-		Output：11(it's a TreeNode)
 
-	Example 2
-		Input：
-			 1
-		   /   \
-		 -5     11
-		Output：11(it's a TreeNode)
+Example 1
+    Input：
+             1
+           /   \
+         -5     11
+         / \   /  \
+        1   2 4    -2 
+    Output：11(it's a TreeNode)
+
+Example 2
+    Input：
+             1
+           /   \
+         -5     11
+    Output：11(it's a TreeNode)
 
 Notice
-	LintCode will print the subtree which root is your return node.
-	It's guaranteed that there is only one subtree with maximum average.
+    LintCode will print the subtree which root is your return node.
+    It's guaranteed that there is only one subtree with maximum average.
 ***/
 /**
  * Definition of TreeNode:
