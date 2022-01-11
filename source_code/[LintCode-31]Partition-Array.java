1e55d0386907a0efd3b838bf3d2e9d70bfc5ea0e
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-11 09:03:03 -0600

    Update [LintCode-31]Partition-Array.java

--- source_code/[LintCode-31]Partition-Array.java
+++ source_code/[LintCode-31]Partition-Array.java
@@ -1,6 +1,33 @@
 /***
 * LintCode 31. Partition Array
+Given an array nums of integers and an int k, partition the array (i.e move the elements in "nums") such that:
+    -All elements < k are moved to the left
+    -All elements >= k are moved to the right
+Return the partitioning index, i.e the first index i nums[i] >= k.
 
+note:
+    You should do really partition in array nums instead of just counting the numbers of integers smaller than k.
+    If all elements in nums are smaller than k, then return nums.length
+    0<=nums.length<=2000
+
+Example 1:
+    Input:
+        nums = []
+        k = 9
+    Output:
+        0
+
+Example 2:
+    Input:
+        nums = [3,2,2,1]
+        k = 2
+    Output:
+        1
+    Explaination:
+        the real array is [1,2,2,3]. So return 1
+
+Challenge
+    Can you partition the array in-place and in O(n)O(n)?
 ***/
 //version-1: two pointers, partition the array in-place, time complexity: O(n)
 public class Solution {
