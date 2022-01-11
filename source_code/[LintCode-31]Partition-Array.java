e7c968157e676e633a047c39edeb88de76cf0ac2
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-11 09:22:49 -0600

    Update [LintCode-31]Partition-Array.java

--- source_code/[LintCode-31]Partition-Array.java
+++ source_code/[LintCode-31]Partition-Array.java
@@ -75,3 +75,32 @@ public class Solution {
         return left;
     }
 }
+
+
+// class Solution:
+//     """
+//     @param nums: The integer array you should partition
+//     @param k: An integer
+//     @return: The index after partition
+//     """
+//     def partitionArray(self, nums, k):
+//         print("input array = " , nums)
+
+//         left, right = 0, len(nums) - 1
+//         while left <= right:
+//             while left <= right and nums[left] < k:
+//                 left += 1
+//             while left <= right and nums[right] >= k:
+//                 right -= 1
+//             if left <= right:
+//                 nums[left], nums[right] = nums[right], nums[left]
+//                 left += 1
+//                 right -= 1
+
+//         print("after partition, left pointer has move to :" , left)
+//         print("after partition, right pointer has move to :" , right)
+//         print("after partition, the array output = " , nums)
+
+//         return left
+        
+
