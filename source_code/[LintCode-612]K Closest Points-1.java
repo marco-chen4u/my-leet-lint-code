f94900da621ab07b002e1c6b81cf0ccaa9f60698
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-12 18:45:46 -0600

    Update [LintCode-612]K Closest Points-1.java

--- source_code/[LintCode-612]K Closest Points-1.java
+++ source_code/[LintCode-612]K Closest Points-1.java
@@ -2,9 +2,9 @@
 * LintCode 612. K Closest Points
 Given some points and an origin point in two-dimensional space, 
 find k points which are nearest to the origin.
+
 Return these points sorted by distance, if they are same in distance, 
 sorted by the x-axis, and if they are same in the x-axis, sorted by y-axis.
-
 
 Example 1:
     Input: points = [[4,6],[4,7],[4,4],[2,5],[1,1]], origin = [0, 0], k = 3 
@@ -14,6 +14,7 @@ Example 2:
     Input: points = [[0,0],[0,9]], origin = [3, 1], k = 1
     Output: [[0,0]]
 ***/
+
 /**
  * Definition for a point.
  * class Point {
@@ -49,6 +50,7 @@ public class Solution {
         }
         
         ORIGINAL_POINT = origin;
+
         Comparator<Point> comparator = new Comparator<Point>() {
             
             @Override
