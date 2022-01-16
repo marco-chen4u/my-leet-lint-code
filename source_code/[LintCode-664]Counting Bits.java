6b75bc20fee0c7766c79303867e4b2e6ee97e74d
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-16 13:32:04 -0600

    Update [LintCode-664]Counting Bits.java

--- source_code/[LintCode-664]Counting Bits.java
+++ source_code/[LintCode-664]Counting Bits.java
@@ -1,29 +1,30 @@
 /***
 * LintCode 664. Counting Bits
-Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
+Given a non negative integer number num. 
+For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
 
-Example
-	Example1
-		Input: 5
-		Output: [0,1,1,2,1,2]
-		Explanation:
-			The binary representation of 0~5 is:
-			000
-			001
-			010
-			011
-			100
-			101
-			the count of "1" in each number is: 0,1,1,2,1,2
+Example1
+    Input: 5
+    Output: [0,1,1,2,1,2]
+    Explanation:
+        The binary representation of 0~5 is:
+        000
+        001
+        010
+        011
+        100
+        101
+        the count of "1" in each number is: 0,1,1,2,1,2
 
-	Example2
-		Input: 3
-		Output: [0,1,1,2]
+Example2
+    Input: 3
+    Output: [0,1,1,2]
 
 Challenge
-	1.It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a single pass?
-	2.Space complexity should be O(n).
-	3.Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
+    1.It is very easy to come up with a solution with run time O(n*sizeof(integer)). 
+        But can you do it in linear time O(n) /possibly in a single pass?
+    2.Space complexity should be O(n).
+    3.Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
 ***/
 //version-1: traditional way
 public class Solution {
@@ -88,13 +89,13 @@ public class Solution {
         
         for (int i = 2; i <= num; i++) {
             dp[i] = dp[i >> 1] + i % 2;
-			/*
-					dp[i >> 1],i >> 1表示 i 向右移1位，即相等于整除2， 所以dp[i >> 1]表示i的二进制数中去掉最后一位剩下1的个数,这个子问题。
-					
-					i % 2 ,表示i的二进制数中最后一位数是为1，如果是1则为1， 否则为0
-			*/
+            /*
+                    dp[i >> 1],i >> 1表示 i 向右移1位，即相等于整除2， 所以dp[i >> 1]表示i的二进制数中去掉最后一位剩下1的个数,这个子问题。
+		
+                    i % 2 ,表示i的二进制数中最后一位数是为1，如果是1则为1， 否则为0
+            */
         }
-        
+ 
         return dp;
     }
 }
