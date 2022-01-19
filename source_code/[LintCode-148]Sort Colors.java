4720cbafa2d00901a5328a295d316e2d6b3d7291
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-19 12:08:46 -0600

    Update [LintCode-148]Sort Colors.java

--- source_code/[LintCode-148]Sort Colors.java
+++ source_code/[LintCode-148]Sort Colors.java
@@ -6,19 +6,20 @@ sort them so that objects of the same color are adjacent, with the colors in the
 Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
 
 Example
-	Given [1, 0, 1, 2], sort it in-place to [0, 1, 1, 2].
+    Given [1, 0, 1, 2], sort it in-place to [0, 1, 1, 2].
 
 Challenge
-	A rather straight forward solution is a two-pass algorithm using counting sort.
-	First, iterate the array counting number of 0's, 1's, and 2's, 
-		then overwrite array with total number of 0's, then 1's and followed by 2's.
+    A rather straight forward solution is a two-pass algorithm using counting sort.
+    First, iterate the array counting number of 0's, 1's, and 2's, 
+        then overwrite array with total number of 0's, then 1's and followed by 2's.
 
-	Could you come up with an one-pass algorithm using only constant space?
+    Could you come up with an one-pass algorithm using only constant space?
 
 Notice
-	You are not suppose to use the library's sort function for this problem.
-	You should do it in-place (sort numbers in the original array).
+    You are not suppose to use the library's sort function for this problem.
+    You should do it in-place (sort numbers in the original array).
 ***/
+//version-1: 3 pointers + partition
 public class Solution {
     /**
      * @param nums: A list of integer which is 0, 1 or 2 
