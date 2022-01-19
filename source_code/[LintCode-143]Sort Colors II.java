2bc67877aa9bb0c832509ba36f83c3b7494ed4d8
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-19 11:58:51 -0600

    Update [LintCode-143]Sort Colors II.java

--- source_code/[LintCode-143]Sort Colors II.java
+++ source_code/[LintCode-143]Sort Colors II.java
@@ -4,15 +4,15 @@ Given an array of n objects with k different colors (numbered from 1 to k),
 sort them so that objects of the same color are adjacent, with the colors in the order 1, 2, ... k.
 
 Example
-	Given colors=[3, 2, 2, 1, 4], k=4, your code should sort colors in-place to [1, 2, 2, 3, 4].
+    Given colors=[3, 2, 2, 1, 4], k=4, your code should sort colors in-place to [1, 2, 2, 3, 4].
 
 Challenge
-	A rather straight forward solution is a two-pass algorithm using counting sort. 
-	That will cost O(k) extra memory. Can you do it without using extra memory?
+    A rather straight forward solution is a two-pass algorithm using counting sort. 
+    That will cost O(k) extra memory. Can you do it without using extra memory?
 
 Notice
-	You are not suppose to use the library's sort function for this problem.
-	k <= n
+    You are not suppose to use the library's sort function for this problem.
+    k <= n
 ***/
 //version-1: O(nk)
 public class Solution {
