d9662ee06496b2fca74b99380374a671b01b2838
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-19 22:50:17 -0600

    Update [LintCode-539]Move Zeroes.java

--- source_code/[LintCode-539]Move Zeroes.java
+++ source_code/[LintCode-539]Move Zeroes.java
@@ -2,18 +2,18 @@
 * LintCode 539. Move Zeroes
 Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
-Example
-	Example 1:
-		Input: nums = [0, 1, 0, 3, 12],
-		Output: [1, 3, 12, 0, 0].
 
-	Example 2:
-		Input: nums = [0, 0, 0, 3, 1],
-		Output: [3, 1, 0, 0, 0].
+Example 1:
+    Input: nums = [0, 1, 0, 3, 12],
+    Output: [1, 3, 12, 0, 0].
+
+Example 2:
+    Input: nums = [0, 0, 0, 3, 1],
+    Output: [3, 1, 0, 0, 0].
 
 Notice
-	You must do this in-place without making a copy of the array.
-	Minimize the total number of operations.
+    You must do this in-place without making a copy of the array.
+    Minimize the total number of operations.
 ***/
 //version-1
 public class Solution {
