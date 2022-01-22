a7d702f24d050719d7e3298cf264d85581b409ba
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-22 16:55:49 -0600

    Update [LintCode-516]Paint House II].java

--- source_code/[LintCode-516]Paint House II].java
+++ source_code/[LintCode-516]Paint House II].java
@@ -4,7 +4,7 @@ There are a row of n houses, each house can be painted with one of the k colors.
 The cost of painting each house with a certain color is different. 
 You have to paint all the houses such that no two adjacent houses have the same color.
 
-The cost of painting each house with a certain color is represented by a n x k cost matrix. 
+The cost of painting each house with a certain color is represented by a n*k cost matrix. 
 
 For example, 
     costs[0][0] is the cost of painting house 0 with color 0; 
