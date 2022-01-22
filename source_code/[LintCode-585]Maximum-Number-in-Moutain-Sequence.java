d0515f70a14ff8d98a625f5dddb5d0753541c768
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-22 10:34:36 -0600

    Update [LintCode-585]Maximum-Number-in-Moutain-Sequence.java

--- source_code/[LintCode-585]Maximum-Number-in-Moutain-Sequence.java
+++ source_code/[LintCode-585]Maximum-Number-in-Moutain-Sequence.java
@@ -51,3 +51,27 @@ public class Solution {
         }
     }
 }
+
+//version-2: binary search
+public class Solution {
+    /**
+     * @param nums: a mountain sequence which increase firstly and then decrease
+     * @return: then mountain top
+     */
+    public int mountainSequence(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                end = mid;
+            }
+            else {
+                start = mid;
+            }
+        }
+
+        return Math.max(nums[start], nums[end]);
+    }
+}
