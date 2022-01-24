0525668bee3fb5fed468dc36c719f889d03175de
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-24 13:03:43 -0600

    Update [LintCode-85]Insert-Node-in-a-Binary-Search-Tree.java

--- source_code/[LintCode-85]Insert-Node-in-a-Binary-Search-Tree.java
+++ source_code/[LintCode-85]Insert-Node-in-a-Binary-Search-Tree.java
@@ -1,6 +1,7 @@
 /***
 * LintCode 85. Insert Node in a Binary Search Tree
-Given a binary search tree and a new tree node, insert the node into the tree. You should keep the tree still be a valid binary search tree.
+Given a binary search tree and a new tree node, insert the node into the tree. 
+You should keep the tree still be a valid binary search tree.
 
 Example 1:
     Input:
