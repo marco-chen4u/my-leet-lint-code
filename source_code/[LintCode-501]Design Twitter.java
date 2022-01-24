93dc0e7658e06d962dd9374fb5c967d0d0bbe23d
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-23 19:46:01 -0600

    Update [LintCode-501]Design Twitter.java

--- source_code/[LintCode-501]Design Twitter.java
+++ source_code/[LintCode-501]Design Twitter.java
@@ -1,50 +1,50 @@
 /***
 * LintCode 501. Design Twitter
 Implement a simple twitter. Support the following method:
-	1.postTweet(user_id, tweet_text). Post a tweet.
-	2.getTimeline(user_id). 
-		Get the given user's most recently 10 tweets posted by himself, 
-		order by timestamp from most recent to least recent.
-	3.getNewsFeed(user_id). 
-		Get the given user's most recently 10 tweets in his news feed (posted by his friends and himself). 
-		Order by timestamp from most recent to least recent.
-	4.follow(from_user_id, to_user_id). 
-		from_user_id followed to_user_id.
-	5.unfollow(from_user_id, to_user_id). 
-		from_user_id unfollowed to to_user_id.
+    1.postTweet(user_id, tweet_text). Post a tweet.
+    2.getTimeline(user_id). 
+        Get the given user's most recently 10 tweets posted by himself, 
+        order by timestamp from most recent to least recent.
+    3.getNewsFeed(user_id). 
+        Get the given user's most recently 10 tweets in his news feed (posted by his friends and himself). 
+        Order by timestamp from most recent to least recent.
+    4.follow(from_user_id, to_user_id). 
+        from_user_id followed to_user_id.
+    5.unfollow(from_user_id, to_user_id). 
+        from_user_id unfollowed to to_user_id.
 Example
-	Example 1
-		Input:
-			postTweet(1, "LintCode is Good!!!")
-			getNewsFeed(1)
-			getTimeline(1)
-			follow(2, 1)
-			getNewsFeed(2)
-			unfollow(2, 1)
-			getNewsFeed(2)
-		Output:
-			1
-			[1]
-			[1]
-			[1]
-			[]
-	Example 2
-		Input:
-			postTweet(1, "LintCode is Good!!!")
-			getNewsFeed(1)
-			getTimeline(1)
-			follow(2, 1)
-			postTweet(1, "LintCode is best!!!")
-			getNewsFeed(2)
-			unfollow(2, 1)
-			getNewsFeed(2)
-		Output:
-			1
-			[1]
-			[1]
-			2
-			[2,1]
-			[]
+    Example 1
+        Input:
+            postTweet(1, "LintCode is Good!!!")
+            getNewsFeed(1)
+            getTimeline(1)
+            follow(2, 1)
+            getNewsFeed(2)
+            unfollow(2, 1)
+            getNewsFeed(2)
+        Output:
+            1
+            [1]
+            [1]
+            [1]
+            []
+    Example 2
+        Input:
+            postTweet(1, "LintCode is Good!!!")
+            getNewsFeed(1)
+            getTimeline(1)
+            follow(2, 1)
+            postTweet(1, "LintCode is best!!!")
+            getNewsFeed(2)
+            unfollow(2, 1)
+            getNewsFeed(2)
+        Output:
+            1
+            [1]
+            [1]
+            2
+            [2,1]
+            []
 ***/
 
 /**
@@ -61,21 +61,21 @@ Example
  */
  // helper class
 class Node implements Comparable<Node> {
-	// fields
-	int order;
-	Tweet tweet;
-	
-	// constructor
-	public Node(int order, Tweet tweet) {
-		this.order = order;
-		this.tweet = tweet;
-	}
-	
-	// method
-	@Override
-	public int compareTo(Node other) {
-		return other.order - this.order;//reverse order, the most recently
-	}
+    // fields
+    int order;
+    Tweet tweet;
+
+    // constructor
+    public Node(int order, Tweet tweet) {
+        this.order = order;
+        this.tweet = tweet;
+    }
+
+    // method
+    @Override
+    public int compareTo(Node other) {
+        return other.order - this.order;//reverse order, the most recently
+    }
 }
 
 public class MiniTwitter {    
@@ -142,7 +142,7 @@ public class MiniTwitter {
         if (userTweets.containsKey(user_id)) {
             list.addAll(getLastTen(userTweets.get(user_id)));
         }
-        
+
         if (friendship.containsKey(user_id)) {
             for (Integer currentFriend : friendship.get(user_id)) {
                 List<Node> values = getLastTen(userTweets.get(currentFriend));
@@ -154,14 +154,13 @@ public class MiniTwitter {
             }
         }
 
-        
         Collections.sort(list);
         
         list = getFirstTen(list);
         for (Node node : list) {
             result.add(node.tweet);
         }
-        
+
         return result;
     }
 
