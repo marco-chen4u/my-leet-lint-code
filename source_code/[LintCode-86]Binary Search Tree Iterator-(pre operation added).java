b4131400de479b91210d273924d4ab5089ba9124
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-24 22:06:09 -0600

    Update [LintCode-86]Binary Search Tree Iterator-(pre operation added).java

--- source_code/[LintCode-86]Binary Search Tree Iterator-(pre operation added).java
+++ source_code/[LintCode-86]Binary Search Tree Iterator-(pre operation added).java
@@ -437,3 +437,71 @@ public class BSTIterator {
         return result;
     }
 }
+
+//version-7
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ * Example of iterate a tree:
+ * BSTIterator iterator = new BSTIterator(root);
+ * while (iterator.hasNext()) {
+ *    TreeNode node = iterator.next();
+ *    do something for node
+ * } 
+ */
+
+
+public class BSTIterator {
+
+    // field
+    private Stack<TreeNode> stack;
+    private TreeNode current;
+
+    /**
+    * @param root: The root of binary tree.
+    */
+    public BSTIterator(TreeNode root) {
+        // do intialization if necessary
+        stack = new Stack<>();
+        current = root;
+
+        findMostLeft(current);
+    }
+
+    /**
+     * @return: True if there has next node, or false
+     */
+    public boolean hasNext() {
+        return !stack.isEmpty() || current != null;
+    }
+
+    /**
+     * @return: return next node
+     */
+    public TreeNode next() {
+        TreeNode result = null;
+
+        current = stack.pop();
+        result = current;
+
+        current = current.right;
+        findMostLeft(current);
+
+        return result;
+    }
+
+    // helper method
+    private void findMostLeft(TreeNode node) {
+        while (node != null) {
+            stack.push(node);
+            node = node.left;
+        }
+    }
+}
