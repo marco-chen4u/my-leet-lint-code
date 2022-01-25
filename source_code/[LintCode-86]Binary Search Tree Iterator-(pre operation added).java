b36aadc98ea87c5bf658b8bccc27c21830099f94
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-24 21:19:30 -0600

    Update [LintCode-86]Binary Search Tree Iterator-(pre operation added).java

--- source_code/[LintCode-86]Binary Search Tree Iterator-(pre operation added).java
+++ source_code/[LintCode-86]Binary Search Tree Iterator-(pre operation added).java
@@ -235,3 +235,78 @@ public class BSTIterator {
         return (current == null) ? null : backUpStack.peek();
     }
 }
+
+//version-4
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ * Example of iterate a tree:
+ * BSTIterator iterator = new BSTIterator(root);
+ * while (iterator.hasNext()) {
+ *    TreeNode node = iterator.next();
+ *    do something for node
+ * } 
+ */
+
+
+public class BSTIterator {
+
+    // field
+    private Stack<TreeNode> stack;
+    private Stack<TreeNode> preStack;
+    private TreeNode current;
+
+    /**
+    * @param root: The root of binary tree.
+    */
+    public BSTIterator(TreeNode root) {
+        // do intialization if necessary
+        stack = new Stack<>();
+        preStack = new Stack<>();
+        current = root;
+
+        while (current != null) {
+            stack.push(current);
+            current = current.left;
+        }
+    }
+
+    /**
+     * @return: True if there has next node, or false
+     */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /**
+     * @return: return next node
+     */
+    public TreeNode next() {
+        current = stack.peek();
+
+        TreeNode node = current;
+
+        if (node.right == null) {
+            node = stack.pop();
+            while (!stack.isEmpty() && stack.peek().right == node) {
+                node = stack.pop();
+            }
+        }
+        else {
+            node = node.right;
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+        }
+
+        return current;
+    }
+}
