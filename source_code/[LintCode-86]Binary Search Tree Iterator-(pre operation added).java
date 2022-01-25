2f8caa9ecf13f89197f6c7e9ae58f0ee9af1bdd0
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-24 21:32:54 -0600

    Update [LintCode-86]Binary Search Tree Iterator-(pre operation added).java

--- source_code/[LintCode-86]Binary Search Tree Iterator-(pre operation added).java
+++ source_code/[LintCode-86]Binary Search Tree Iterator-(pre operation added).java
@@ -310,3 +310,68 @@ public class BSTIterator {
         return current;
     }
 }
+
+//version-5
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ * Example of iterate a tree:
+ * BSTIterator iterator = new BSTIterator(root);
+ * while (iterator.hasNext()) {
+ *    TreeNode node = iterator.next();
+ *    do something for node
+ * } 
+ */
+
+
+public class BSTIterator {
+
+    // field
+    private Stack<TreeNode> stack;
+    private TreeNode current;
+
+    /**
+    * @param root: The root of binary tree.
+    */
+    public BSTIterator(TreeNode root) {
+        // do intialization if necessary
+        stack = new Stack<>();
+        current = root;
+
+        while (current != null) {
+            stack.push(current);
+            current = current.left;
+        }
+    }
+
+    /**
+     * @return: True if there has next node, or false
+     */
+    public boolean hasNext() {
+        return !stack.isEmpty() || current != null;
+    }
+
+    /**
+     * @return: return next node
+     */
+    public TreeNode next() {
+        current = stack.pop();
+        
+        TreeNode result  = current;
+
+        current = current.right;
+        while (current != null) {
+            stack.push(current);
+            current = current.left;
+        }
+
+        return result;
+    }
+}
