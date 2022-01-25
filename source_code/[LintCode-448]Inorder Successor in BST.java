1e566e554715fdb24c288e13ce64eec3400bf05c
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-24 20:47:52 -0600

    Update [LintCode-448]Inorder Successor in BST.java

--- source_code/[LintCode-448]Inorder Successor in BST.java
+++ source_code/[LintCode-448]Inorder Successor in BST.java
@@ -1,33 +1,34 @@
 /***
 * LintCode 448. Inorder Successor in BST
-Given a binary search tree (See Definition) and a node in it, find the in-order successor of that node in the BST.
+Given a binary search tree (See Definition) and a node in it, 
+find the in-order successor of that node in the BST.
 
 If the given node has no in-order successor in the tree, return null.
 
-Example
-	Example 1:
-		Input: tree = {1,#,2}, node value = 1
-		Output: 2
-		Explanation:
-		  1
-		   \
-			2
+
+Example 1:
+    Input: tree = {1,#,2}, node value = 1
+    Output: 2
+    Explanation:
+          1
+           \
+            2
 		
-	Example 2:
-		Input: tree = {2,1,3}, node value = 1
-		Output: 2
-		Explanation: 
-				2
-			   / \
-			  1   3
-			Binary Tree Representation
+Example 2:
+    Input: tree = {2,1,3}, node value = 1
+    Output: 2
+    Explanation: 
+            2
+           / \
+          1   3
+        Binary Tree Representation
 
 Challenge
-	O(h), where h is the height of the BST.
+    O(h), where h is the height of the BST.
 
 Notice
-	It's guaranteed p is one node in the given tree. 
-	(You can directly compare the memory address to find p)
+    It's guaranteed p is one node in the given tree. 
+    (You can directly compare the memory address to find p)
 ***/
 
 /**
@@ -50,8 +51,8 @@ public class Solution {
         if (root == null || p == null) {
             return null;
         }
-        
-		TreeNode successor = null;
+
+        TreeNode successor = null;
         TreeNode current = root;
         
         while (current != null) {
@@ -63,7 +64,7 @@ public class Solution {
                 current = current.right;
             }
         }
-        
+
         return successor;
     }
 }
