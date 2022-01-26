db74ea126209e409dd76770c4f102bed2942844b
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-26 11:38:56 -0600

    Update [LintCode-152]Combinations.java

--- source_code/[LintCode-152]Combinations.java
+++ source_code/[LintCode-152]Combinations.java
@@ -31,13 +31,13 @@ public class Solution {
             return result;
         }
 
-        dfs(result, combination, n, k, 1);
+        dfs(n, k, 1, combination, result);
 
         return result;
     }
 	
     // helper method
-    private void dfs(List<List<Integer>> result, List<Integer> combination, int n, int k, int startIndex) {
+    private void dfs(int n, int k, int startIndex, List<Integer> combination, List<List<Integer>> result) {
         if (combination.size() == k) {
             result.add(new ArrayList<Integer>(combination));
             return;
@@ -45,7 +45,7 @@ public class Solution {
 		
         for (int i = startIndex; i <= n; i++) {
             combination.add(i);
-            dfs(result, combination, n, k, i + 1);
+            dfs(n, k, i + 1, combination, result);
             combination.remove(combination.size() - 1);
         }
     }
