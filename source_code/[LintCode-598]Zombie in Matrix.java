ee6e02977cc951939ef12a6c31fbcfa1293a5e26
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-26 22:04:44 -0600

    Update [LintCode-598]Zombie in Matrix.java

--- source_code/[LintCode-598]Zombie in Matrix.java
+++ source_code/[LintCode-598]Zombie in Matrix.java
@@ -7,110 +7,108 @@ but can not through wall.
 How long will it take to turn all people into zombies? 
 Return -1 if can not turn all people into zombies.
 
-Example
-	Example 1:
-		Input:
-			[[0,1,2,0,0],
-			 [1,0,0,2,1],
-			 [0,1,0,0,0]]
-		Output:
-			2
-	Example 2:
-		Input:
-			[[0,0,0],
-			 [0,0,0],
-			 [0,0,1]]
-		Output:
-			4
+Example 1:
+    Input:
+        [[0,1,2,0,0],
+         [1,0,0,2,1],
+         [0,1,0,0,0]]
+    Output:
+        2
+	
+Example 2:
+    Input:
+        [[0,0,0],
+         [0,0,0],
+         [0,0,1]]
+    Output:
+        4
 ***/
 //verion-1:BFS with helper class Point for holding all the x&y position information
 public class Solution {
-	// inner class
-	class Point {
-		// fields
-		public int x;
-		public int y;
-		// constructor
-		public Point(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
-	
-	//fields
-	private final int WALL = 2;
-	private final int ZOMBIE = 1;
-	private final int PEOPLE = 0;
-	private final int[] directionX = new int[] {1, 0, 0, -1};
-	private final int[] directionY = new int[] {0, 1, -1, 0};
-			
+    // inner class
+    class Point {
+        // fields
+        public int x;
+        public int y;
+        // constructor
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
+    //fields
+    private final int WALL = 2;
+    private final int ZOMBIE = 1;
+    private final int PEOPLE = 0;
+    private final int[] directionX = new int[] {1, 0, 0, -1};
+    private final int[] directionY = new int[] {0, 1, -1, 0};
 	
-	/**
+    /**
      * @param grid: a 2D integer grid
      * @return: an integer
      */
     public int zombie(int[][] grid) {
-		int result = -1;
-		// check corner case 
-		if (grid == null || grid.length == 0) {
-			return 0;
-		}
-		
-		int n = grid.length; // row size
-		int m = grid[0].length; // column size		
-		int days = 0;
-		int peopleCount = 0;
-		
-		Queue<Point> queue = new LinkedList<Point>();
+        int result = -1;
+        // check corner case 
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
 
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (grid[i][j] == PEOPLE) {
-					peopleCount++;
-				}
-				
-				if (grid[i][j] == ZOMBIE) {
-					Point zombie = new Point(i, j);
-					queue.offer(zombie);
-				}
-			}
-		}
-		
-		if (peopleCount == 0) {
-			return 0;
-		}
-		
-		while (!queue.isEmpty()) {
-			int size = queue.size();
-			days++;
-			for (int i = 0; i < size; i++) {
-				Point current = queue.poll();
-				
-				for (int index = 0; index < 4; index++) {
-					int nextX = current.x + directionX[index];
-					int nextY = current.y + directionY[index];
-					
-					if (nextX < 0 || nextX >= n ||
-							nextY < 0 || nextY >= m ||
-							grid[nextX][nextY] != PEOPLE) {
-						continue;
-					}
-					
-					grid[nextX][nextY] = ZOMBIE;
-					peopleCount--;
-					if (peopleCount == 0) {
-						return days;
-					}
-					
-					Point next = new Point(nextX, nextY);
-					queue.offer(next);				
-				}
-			}
-		}
-		
-		return result;
-	}
+        int n = grid.length; // row size
+        int m = grid[0].length; // column size		
+        int days = 0;
+        int peopleCount = 0;
+
+        Queue<Point> queue = new LinkedList<Point>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == PEOPLE) {
+                    peopleCount++;
+                }
+
+                if (grid[i][j] == ZOMBIE) {
+                    Point zombie = new Point(i, j);
+                    queue.offer(zombie);
+                }
+            }
+        }
+
+        if (peopleCount == 0) {
+            return 0;
+        }
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            days++;
+            for (int i = 0; i < size; i++) {
+                Point current = queue.poll();
+
+                for (int index = 0; index < 4; index++) {
+                    int nextX = current.x + directionX[index];
+                    int nextY = current.y + directionY[index];
+
+                    if (nextX < 0 || nextX >= n ||
+                            nextY < 0 || nextY >= m ||
+                            grid[nextX][nextY] != PEOPLE) {
+                        continue;
+                    }
+
+                    grid[nextX][nextY] = ZOMBIE;
+                    peopleCount--;
+                    if (peopleCount == 0) {
+                        return days;
+                    }
+
+                    Point next = new Point(nextX, nextY);
+                    queue.offer(next);				
+                }
+            }
+        }
+
+        return result;
+    }
 }
 
 //verion-2: use a pair of Queue to track all source positions(which are zombies) x and y values repectively, whenever offer&poll() they will act together at the same time
@@ -191,7 +189,7 @@ public class Solution {
                 }
             }
         }
-        
+
         return -1;
     }
 }
