1fddb058078d06d83b9a5d7cc1dc145a4593b609
Marco Chen <haisheng.chen4u@foxmail.com>
2022-01-29 12:13:38 -0600

    Update [LintCode-65]Median of two Sorted Arrays.java

--- source_code/[LintCode-65]Median of two Sorted Arrays.java
+++ source_code/[LintCode-65]Median of two Sorted Arrays.java
@@ -2,21 +2,20 @@
 * LintCode 65. Median of two Sorted Arrays
 There are two sorted arrays A and B of size m and n respectively. Find the median of the two sorted arrays.
 
-Example
-	Example 1
-		Input:
-			A = [1,2,3,4,5,6]
-			B = [2,3,4,5]
-		Output: 3.5
+Example 1
+    Input:
+        A = [1,2,3,4,5,6]
+        B = [2,3,4,5]
+    Output: 3.5
 
-	Example 2
-		Input:
-			A = [1,2,3]
-			B = [4,5]
-		Output: 3
+Example 2
+    Input:
+        A = [1,2,3]
+        B = [4,5]
+    Output: 3
 
 Challenge
-	The overall run time complexity should be O(log (m+n)).
+    The overall run time complexity should be O(log (m+n)).
 ***/
 public class Solution {
     /*
