be98f7945210c034e38fd5f4f64dabea52515e94
Marco Chen <haisheng.chen4u@foxmail.com>
2022-02-05 22:51:23 -0600

    Update [LintCode-17]Subsets.java

--- source_code/[LintCode-17]Subsets.java
+++ source_code/[LintCode-17]Subsets.java
@@ -32,6 +32,26 @@ Notice
     Elements in a subset must be in non-descending order.
     The solution set must not contain duplicate subsets.
 ***/
+
+/**
+* 这是一种组合类的搜索问题，
+* 因为这道题是return all possible subsets， 就是return所有可能的方案，一般来说，求所有具体方案的这类问题，通常你都可以朝着深度优先搜索（DFS）这个方向去分析。
+*
+* 一个集合的子集总个数:
+* 从数学推导分析来说： C(n, 0) + C(n, 1) + C(n, 2) + ... + C(n, n) = 2^n
+*
+* 从排列取舍的角度分析来说
+* 集合中，有n个元素，每个元素都有选与不选的2个选项，所以，每个位置都有2种状态，n个元素的组合选择，总共有2^n个子集选项。
+* 比如某集合为{1,2,3},从空集出发，作为根节点，分析如下：
+*                   [   ]          
+*               1?/       \
+*               [1]        [ ]
+*            2?/   \       /   \
+*          [1, 2]  [1]   [2]   [ ]
+*         3?/  \   ...................  
+*      [1,2,3] [1,2] [1,3] [1] [2,3] [2] [3] [] <----------注意这一层，就是所搜索得到的结果
+**/
+
 //version-1: DFS
 public class Solution {
     /**
@@ -62,10 +82,10 @@ public class Solution {
 
     // helper method
     private void search(int[] nums, int startIndex, List<Integer> subset, List<List<Integer>> result) {
-        result.add(new ArrayList<>(subset));
-        
+                
         // corner case
         if (startIndex == nums.length) {
+            result.add(new ArrayList<>(subset));
             return;
         }
 
