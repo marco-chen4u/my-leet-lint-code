80dd771944741938e7d8f4c4f3801a53efa58f20
Marco Chen <haisheng.chen4u@foxmail.com>
2022-02-13 22:42:52 -0600

    Update [LintCode-566]GFS Client.java

--- source_code/[LintCode-566]GFS Client.java
+++ source_code/[LintCode-566]GFS Client.java
@@ -6,7 +6,11 @@ Implement a simple client for GFS (Google File System, a distributed file system
 There are two private methods that already implemented in the base class:
     1.readChunk(filename, chunkIndex). Read a chunk from GFS.
     2.writeChunk(filename, chunkIndex, chunkData). Write a chunk to GFS.
-To simplify this question, we can assume that the chunk size is chunkSize bytes. (In a real world system, it is 64M). The GFS Client's job is splitting a file into multiple chunks (if need) and save to the remote GFS server. chunkSize will be given in the constructor. You need to call these two private methods to implement read & write methods.
+To simplify this question, we can assume that the chunk size is chunkSize bytes. 
+(In a real world system, it is 64M). The GFS Client's job is splitting a file into multiple chunks (if need) 
+and save to the remote GFS server. chunkSize will be given in the constructor. 
+
+You need to call these two private methods to implement read & write methods.
 
 Example 
     GFSClient(5)
