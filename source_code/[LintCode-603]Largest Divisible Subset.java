fc00a509124c86e3282645fab55c3ae2eb38dfb9
Marco Chen <haisheng.chen4u@foxmail.com>
2022-02-26 22:22:19 -0600

    Update [LintCode-603]Largest Divisible Subset.java

--- source_code/[LintCode-603]Largest Divisible Subset.java
+++ source_code/[LintCode-603]Largest Divisible Subset.java
@@ -1,6 +1,7 @@
 /***
 * LintCode 603. Largest Divisible Subset
-Given a set of distinct positive integers, find the largest subset such that every pair (Si, Sj) of elements in this subset satisfies: Si % Sj = 0 or Sj % Si = 0.
+Given a set of distinct positive integers, 
+find the largest subset such that every pair (Si, Sj) of elements in this subset satisfies: Si % Sj = 0 or Sj % Si = 0.
 
 Example 1:
     Input: nums =  [1,2,3], 
