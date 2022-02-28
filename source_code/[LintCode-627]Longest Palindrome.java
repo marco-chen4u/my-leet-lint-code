dd602822c497fdffcb9f376686c776fbadb921a7
Marco Chen <haisheng.chen4u@foxmail.com>
2022-02-27 19:54:09 -0600

    Update [LintCode-627]Longest Palindrome.java

--- source_code/[LintCode-627]Longest Palindrome.java
+++ source_code/[LintCode-627]Longest Palindrome.java
@@ -5,15 +5,15 @@ find the length of the longest palindromes that can be built with those letters.
 
 This is case sensitive, for example "Aa" is not considered a palindrome here.
 
-Example
-	Example 1:
-		Input : s = "abccccdd"
-		Output : 7
-		Explanation :
-			One longest palindrome that can be built is "dccaccd", whose length is `7`.
+
+Example 1:
+    Input : s = "abccccdd"
+    Output : 7
+    Explanation :
+        One longest palindrome that can be built is "dccaccd", whose length is `7`.
 
 Notice
-	Assume the length of given string will not exceed 1010.
+    Assume the length of given string will not exceed 1010.
 ***/
 
 //version-1
@@ -23,27 +23,27 @@ public class Solution {
      * @return: the length of the longest palindromes that can be built
      */
     public int longestPalindrome(String s) {
-		int result = 0;
-		if (s == null || s.length() == 0) {
-			return result;
-		}
-		
-		Map<Character, Integer> map = new HashMap<Character, Integer>();
-		for (char ch : s.toCharArray()) {
-			if (!map.containsKey(ch)) {
-				map.put(ch, 1);
-			}
-			else {
-				map.remove(ch);
-			}
-		}
-		
-		result = s.length() - map.size();//we got the number of characters that even int count
-		
-		result += (!map.isEmpty()) ? 1 : 0;
-		
-		return result;
-	}
+        int result = 0;
+        if (s == null || s.length() == 0) {
+            return result;
+        }
+
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        for (char ch : s.toCharArray()) {
+            if (!map.containsKey(ch)) {
+                map.put(ch, 1);
+            }
+            else {
+                map.remove(ch);
+            }
+        }
+
+        result = s.length() - map.size();//we got the number of characters that even int count
+
+        result += (!map.isEmpty()) ? 1 : 0;
+
+        return result;
+    }
 }
 
 //version-2: better to understand
@@ -53,33 +53,33 @@ public class Solution {
      * @return: the length of the longest palindromes that can be built
      */
     public int longestPalindrome(String s) {
-		int result = 0;
-		if (s == null || s.length() == 0) {
-			return result;
-		}
-		
-		Map<Character, Integer> map = new HashMap<Character, Integer>();
-		
-		for (char ch : s.toCharArray()) {
-			map.putIfAbsent(ch, 0);
-			map.put(ch, map.get(ch) + 1);
-		}
-		
-		boolean hasOdd = false;
-		for (int value : map.values()) {
-			if (value % 2 == 0) {
-				result += value;
-			}
-			else {
-				result += (value - 1);
-				hasOdd = true;
-			}
-		}
-		
-		result += (hasOdd) ? 1 : 0;
-		
-		return result;
-	}
+        int result = 0;
+        if (s == null || s.length() == 0) {
+            return result;
+        }
+
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+
+        for (char ch : s.toCharArray()) {
+            map.putIfAbsent(ch, 0);
+            map.put(ch, map.get(ch) + 1);
+        }
+
+        boolean hasOdd = false;
+        for (int value : map.values()) {
+            if (value % 2 == 0) {
+                result += value;
+            }
+            else {
+                result += (value - 1);
+                hasOdd = true;
+            }
+        }
+
+        result += (hasOdd) ? 1 : 0;
+
+        return result;
+    }
 }
 	
 //version-3: best solution
