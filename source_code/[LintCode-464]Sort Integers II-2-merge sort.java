a515a60b5efca23067bc02fb8285c5a9b508ebad
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-02 22:56:50 -0600

    Update [LintCode-464]Sort Integers II-2-merge sort.java

--- source_code/[LintCode-464]Sort Integers II-2-merge sort.java
+++ source_code/[LintCode-464]Sort Integers II-2-merge sort.java
@@ -2,19 +2,48 @@
 * LintCode 464. Sort Integers II
 Given an integer array, sort it in ascending order. Use quick sort, merge sort, heap sort or any O(nlogn) algorithm.
 
-Example
-	Example1:
-		Input: [3, 2, 1, 4, 5], 
-		Output: [1, 2, 3, 4, 5].
 
-	Example2:
-		Input: [2, 3, 1], 
-		Output: [1, 2, 3].
+Example1:
+    Input: [3, 2, 1, 4, 5], 
+    Output: [1, 2, 3, 4, 5].
+
+Example2:
+    Input: [2, 3, 1], 
+    Output: [1, 2, 3].
 ***/
 // version-2 merge sort
 public class Solution {
     
+    /**
+     * @param A: an integer array
+     * @return: nothing
+     */
+    public void sortIntegers2(int[] nums) {
+        // check corner case
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        
+        int[] temp = new int[nums.length];
+        mergeSort(nums, 0, nums.length - 1, temp);
+    }
+	
     // helper methods
+    private void mergeSort(int[] nums, int start, int end, int[] temp) {
+        if (start >= end) {
+            return;
+        }
+        
+        int mid = start + (end - start) / 2;
+        
+        // divide
+        mergeSort(nums, start, mid, temp);
+        mergeSort(nums, mid + 1, end, temp);
+        
+        // conquer
+        merge(nums, start, end, temp);
+    }
+
     private void merge(int[] nums, int start, int end, int[] temp) {
         int left= start;
         int mid = start + (end - start) / 2;
@@ -41,34 +70,5 @@ public class Solution {
         for (int i = start; i <= end; i++) {
             nums[i] = temp[i];
         }
-    }
-    
-    private void mergeSort(int[] nums, int start, int end, int[] temp) {
-        if (start >= end) {
-            return;
-        }
-        
-        int mid = start + (end - start) / 2;
-        
-        // divide
-        mergeSort(nums, start, mid, temp);
-        mergeSort(nums, mid + 1, end, temp);
-        
-        // conquer
-        merge(nums, start, end, temp);
-    }
-    
-    /**
-     * @param A: an integer array
-     * @return: nothing
-     */
-    public void sortIntegers2(int[] nums) {
-        // check corner case
-        if (nums == null || nums.length == 0) {
-            return;
-        }
-        
-        int[] temp = new int[nums.length];
-        mergeSort(nums, 0, nums.length - 1, temp);
     }
 }
