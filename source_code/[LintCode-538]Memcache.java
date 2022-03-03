4d95cdb104e087ccb67e7176a9de1e64991f3b4e
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-02 21:48:57 -0600

    Update [LintCode-538]Memcache.java

--- source_code/[LintCode-538]Memcache.java
+++ source_code/[LintCode-538]Memcache.java
@@ -12,39 +12,39 @@ Implement a memcache which support the following features:
         Return 2147483647 if key does not exist.
 It's guaranteed that the input is given with increasingcurtTime.
 
-Example
-    Example1
-        get(1, 0)
-        >> 2147483647
-        set(2, 1, 1, 2)
-        get(3, 1)
-        >> 1
-        get(4, 1)
-        >> 2147483647
-        incr(5, 1, 1)
-        >> 2147483647
-        set(6, 1, 3, 0)
-        incr(7, 1, 1)
-        >> 4
-        decr(8, 1, 1)
-        >> 3
-        get(9, 1)
-        >> 3
-        delete(10, 1)
-        get(11, 1)
-        >> 2147483647
-        incr(12, 1, 1)
-        >> 2147483647
-		
-Clarification
-Actually, a real memcache server will evict keys if memory is not sufficient, 
-and it also supports variety of value types like string and integer. 
-In our case, let's make it simple, we can assume that we have enough memory and all of the values are integers.
+Example1
+    get(1, 0)
+    >> 2147483647
+    set(2, 1, 1, 2)
+    get(3, 1)
+    >> 1
+    get(4, 1)
+    >> 2147483647
+    incr(5, 1, 1)
+    >> 2147483647
+    set(6, 1, 3, 0)
+    incr(7, 1, 1)
+    >> 4
+    decr(8, 1, 1)
+    >> 3
+    get(9, 1)
+    >> 3
+    delete(10, 1)
+    get(11, 1)
+    >> 2147483647
+    incr(12, 1, 1)
+    >> 2147483647
 
-Search "LRU" & "LFU" on google to get more information about how memcache evict data.
+Clarification
+    Actually, a real memcache server will evict keys if memory is not sufficient, 
+    and it also supports variety of value types like string and integer. 
+    In our case, let's make it simple, 
+    we can assume that we have enough memory and all of the values are integers.
+
+    Search "LRU" & "LFU" on google to get more information about how memcache evict data.
 
 Try the following problem to learn LRU cache:
-http://www.lintcode.com/problem/lru-cache
+    http://www.lintcode.com/problem/lru-cache
 ***/
 /**
  * Definition of Column:
