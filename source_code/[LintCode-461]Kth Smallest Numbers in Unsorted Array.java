7341d4e5b776f0dcc3278434e93c2dfe91f8379f
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-07 19:06:40 -0600

    Update [LintCode-461]Kth Smallest Numbers in Unsorted Array.java

--- source_code/[LintCode-461]Kth Smallest Numbers in Unsorted Array.java
+++ source_code/[LintCode-461]Kth Smallest Numbers in Unsorted Array.java
@@ -2,17 +2,16 @@
 * LintCode 461. Kth Smallest Numbers in Unsorted Array
 Find the kth smallest number in an unsorted integer array.
 
-Example
-	Example 1:
-		Input: [3, 4, 1, 2, 5], k = 3
-		Output: 3
+Example 1:
+    Input: [3, 4, 1, 2, 5], k = 3
+    Output: 3
 
-	Example 2:
-		Input: [1, 1, 1], k = 2
-		Output: 1
+Example 2:
+    Input: [1, 1, 1], k = 2
+    Output: 1
 
 Challenge
-	An O(nlogn) algorithm is acceptable, if you can do it in O(n), that would be great.
+    An O(nlogn) algorithm is acceptable, if you can do it in O(n), that would be great.
 ***/
 public class Solution {
     
