09890f212d366ffcfce26cfc1b17a5515a3c5673
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-08 13:55:30 -0600

    Update [LintCode-87]Remove Node in Binary Search Tree.java

--- source_code/[LintCode-87]Remove Node in Binary Search Tree.java
+++ source_code/[LintCode-87]Remove Node in Binary Search Tree.java
@@ -7,50 +7,50 @@ If there is no such a node with given value in the binary search tree, do nothin
 
 You should keep the tree still a binary search tree after removal.
 
-Example
-    Example 1
-        Input: 
-            Tree = {5,3,6,2,4}
-            k = 3
-        Output: {5,2,6,#,4} or {5,4,6,2}
-        Explanation:
-            Given binary search tree:
-                5
-               / \
-              3   6
-             / \
-            2   4
-            Remove 3, you can either return:
-                5
-               / \
-              2   6
-               \
-                4
-            or
-                5
-               / \
-              4   6
-             /
-            2
 
-    Example 2
-        Input: 
-            Tree = {5,3,6,2,4}
-            k = 4
-        Output: {5,3,6,2}
-        Explanation:
-            Given binary search tree:
-                5
-               / \
-              3   6
-             / \
-            2   4
-            Remove 4, you should return:
-                5
-               / \
-              3   6
-             /
-            2
+Example 1
+    Input: 
+        Tree = {5,3,6,2,4}
+        k = 3
+    Output: {5,2,6,#,4} or {5,4,6,2}
+    Explanation:
+        Given binary search tree:
+            5
+           / \
+          3   6
+         / \
+        2   4
+        Remove 3, you can either return:
+            5
+           / \
+          2   6
+           \
+            4
+        or
+            5
+           / \
+          4   6
+         /
+        2
+
+Example 2
+    Input: 
+        Tree = {5,3,6,2,4}
+        k = 4
+    Output: {5,3,6,2}
+    Explanation:
+        Given binary search tree:
+            5
+           / \
+          3   6
+         / \
+        2   4
+        Remove 4, you should return:
+            5
+           / \
+          3   6
+         /
+        2
 ***/
 
 /**
