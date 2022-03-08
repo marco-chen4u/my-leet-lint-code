9f24c6f3302ddb368cff860ea52b515fc5fa0a9b
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-07 23:14:39 -0600

    Update [LintCode-77]Longest Common Subsequence.java

--- source_code/[LintCode-77]Longest Common Subsequence.java
+++ source_code/[LintCode-77]Longest Common Subsequence.java
@@ -2,22 +2,22 @@
 * LintCode 77. Longest Common Subsequence
 Given two strings, find the longest common subsequence (LCS).
 Your code should return the length of LCS.
-Example
-	Example 1:
-		Input:  "ABCD" and "EDCA"
-		Output:  1	
-		Explanation:
-		LCS is 'A' or  'D' or 'C'
 
-	Example 2:
-		Input: "ABCD" and "EACB"
-		Output:  2	
-		Explanation: 
-		LCS is "AC"	
+Example 1:
+    Input:  "ABCD" and "EDCA"
+    Output:  1	
+    Explanation:
+        LCS is 'A' or  'D' or 'C'
+
+Example 2:
+    Input: "ABCD" and "EACB"
+    Output:  2	
+    Explanation: 
+        LCS is "AC"	
 Clarification
-	What's the definition of Longest Common Subsequence?
-		https://en.wikipedia.org/wiki/Longest_common_subsequence_problem
-		http://baike.baidu.com/view/2020307.htm
+    What's the definition of Longest Common Subsequence?
+        https://en.wikipedia.org/wiki/Longest_common_subsequence_problem
+        http://baike.baidu.com/view/2020307.htm
 ***/
 public class Solution {
     /**
@@ -26,44 +26,44 @@ public class Solution {
      * @return: The length of longest common subsequence of A and B
      */
     public int longestCommonSubsequence(String A, String B) {
-		// check corner cases
-		if (A == null || A.length() == 0 ||B == null || B.length() == 0) {
-			return 0;
-		}
-		
-		// state
-		int n = A.length();
-		int m = B.length();
-		int[][] dp = new int[n + 1][m + 1];		
-		// initialize
-		dp[0][0] = 0;
-		for (int i = 1; i <= n; i++) {
-			dp[i][0] = 0;
-		}
-		for (int j = 1; j <= m; j++) {
-			dp[0][j] = 0;
-		}		
-		for (int i = 1; i <= n; i++) {
-		    for (int j = 1; j <= m; j++) {
-		        dp[i][j] = 0;
-		    }
-		}
-		
-		// function
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= m; j++) {
-				if (A.charAt(i - 1) == B.charAt(j - 1)) {
-					dp[i][j] = dp[i - 1][j - 1] + 1;
-				}
-				
-				dp[i][j] = Math.max(dp[i][j],
-									Math.max(dp[i - 1][j], dp[i][j - 1]));
-			}
-		}
-		
-		// return
-		return dp[n][m];
-	}
+        // check corner cases
+        if (A == null || A.length() == 0 ||B == null || B.length() == 0) {
+            return 0;
+        }
+
+        // state
+        int n = A.length();
+        int m = B.length();
+        int[][] dp = new int[n + 1][m + 1];		
+        // initialize
+        dp[0][0] = 0;
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] = 0;
+        }		
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = 0;
+            }
+        }
+
+        // function
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (A.charAt(i - 1) == B.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+
+                dp[i][j] = Math.max(dp[i][j],
+                                    Math.max(dp[i - 1][j], dp[i][j - 1]));
+            }
+        }
+
+        // return
+        return dp[n][m];
+    }
 }
 
 //version-2 printer
@@ -114,7 +114,7 @@ public class Solution {
             }
         }
         
-		// printing out the longest common subsequence string
+        // printing out the longest common subsequence string
         int size = dp[n][m];
         char[] result = new char[size];
         int i = n;
@@ -136,12 +136,12 @@ public class Solution {
             }
         }
         
-        System.out.println("Longest Common Subsequence : " + String.valueOf(result));
+        //System.out.println("Longest Common Subsequence : " + String.valueOf(result));
         
         return dp[n][m];
     }
-	
-	// helper method
+
+    // helper method
     private boolean isEmpty(String str) {
         return str == null || str.length() == 0;
     }
