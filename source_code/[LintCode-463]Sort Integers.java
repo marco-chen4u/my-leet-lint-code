bc3296b8ba29cb628f53a6e1a7188fe0f5a3065d
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-08 20:25:35 -0600

    Update [LintCode-463]Sort Integers.java

--- source_code/[LintCode-463]Sort Integers.java
+++ source_code/[LintCode-463]Sort Integers.java
@@ -144,8 +144,9 @@ public class Solution {
         }
 
         int size = nums.length;
+	int lastPos = size - 1;
         for (int i = 0; i < size - 1; i++) {
-            for (int j = 0; j < size - i - 1; j++) {
+            for (int j = 0; j < lastPos - i; j++) {
                 if (nums[j] > nums[j + 1]) {
                     swap(nums, j, j + 1);
                 }
