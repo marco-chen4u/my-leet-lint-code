23a841cb05ebbd58674143e2174ac3be83637137
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-12 22:44:01 -0600

    Update LintCode-1179]Friend-Circles.java

--- source_code/LintCode-1179]Friend-Circles.java
+++ source_code/LintCode-1179]Friend-Circles.java
@@ -10,14 +10,18 @@ If M[i][j] = 1, then the ith and jth students are direct friends with each other
 And you have to output the total number of friend circles among all the students.
 
 Example 1:
-    Input: [[1,1,0],[1,1,0],[0,0,1]]
+    Input: [[1,1,0],
+            [1,1,0],
+            [0,0,1]]
     Output: 2
     Explanation:
         The 0th and 1st students are direct friends, so they are in a friend circle. 
         The 2nd student himself is in a friend circle. So return 2.
 
 Example 2:
-    Input: [[1,1,0],[1,1,1],[0,1,1]]
+    Input: [[1,1,0],
+            [1,1,1],
+            [0,1,1]]
     Output: 1
     Explanation:
         The 0th and 1st students are direct friends, the 1st and 2nd students are direct friends, 
