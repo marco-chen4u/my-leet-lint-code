bc38bf60d74210274435456cd382065617d2699c
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-13 10:45:11 -0500

    Update LintCode-1179]Friend-Circles.java

--- source_code/LintCode-1179]Friend-Circles.java
+++ source_code/LintCode-1179]Friend-Circles.java
@@ -27,6 +27,7 @@ Example 2:
         The 0th and 1st students are direct friends, the 1st and 2nd students are direct friends, 
         so the 0th and 2nd students are indirect friends. All of them are in the same friend circle, so return 1.
 **/
+//version-1: bfs
 public class Solution {
 
     private static final int FRIENDS = 1;
@@ -71,6 +72,70 @@ public class Solution {
 
                 visited[y] = true;
                 queue.offer(y);
+            }
+        }
+    }
+}
+
+//version-2: bfs(better understanding of the source code)
+public class Solution {
+
+    // constant
+    private static final int FRIENDS = 1;
+
+    // field
+    private int n; // row/cloumn size for matrix
+
+    /**
+     * @param matrix: a matrix
+     * @return: the total number of friend circles among all the students
+     */
+    public int findCircleNum(int[][] matrix) {
+        // corner corner case
+        if (matrix == null || matrix.length == 0 || 
+            matrix[0] == null || matrix[0].length == 0) {
+            return 0;
+        }
+
+        int count = 0;
+        n = matrix.length; // n * n matrix
+        boolean[] visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) {
+                continue;
+            }
+
+            bfs(matrix, visited, i);
+            count++;
+        }
+
+        return count;
+    }
+
+    // helper method
+    private void bfs(int[][] matrix, boolean[] visited, int row) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(row);
+        visited[row] = true;
+
+        while (!queue.isEmpty()) {
+            int x = queue.poll();// current row
+            for (int y = 0; y < n; y++) { // iterate each column for this current row
+                
+                /* since matrix[x, y] == FRIENDS,so matrix[y, x] == FRIENDS as well, 
+                so searching it column-wise next available one is likewise.*/
+                if (visited[y]) {
+                    continue;
+                }
+
+                if (matrix[x][y] != FRIENDS || matrix[y][x] != FRIENDS) {
+                    continue;
+                }
+
+                // if they are FRIENDS, queuing the this candidate for the next searching
+                queue.offer(y);
+                visited[y] = true;
             }
         }
     }
