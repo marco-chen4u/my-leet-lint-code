06119498a8e013ebf7d8e38ee2923b14f15df36f
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-14 22:43:04 -0500

    Update [LintCode-103]Linked List Cycle II.java

--- source_code/[LintCode-103]Linked List Cycle II.java
+++ source_code/[LintCode-103]Linked List Cycle II.java
@@ -34,18 +34,6 @@ Challenge
  * }
  */
 
-/**
- * Definition for ListNode
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-
 public class Solution {
     /**
      * @param head: The first node of linked list.
