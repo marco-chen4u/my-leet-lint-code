ff666fa2e50ef682478f4c620801c93be52ca89a
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-15 12:39:49 -0500

    Update [LintCode-548]Intersection-of-Two-Arrays-II.java

--- source_code/[LintCode-548]Intersection-of-Two-Arrays-II.java
+++ source_code/[LintCode-548]Intersection-of-Two-Arrays-II.java
@@ -70,3 +70,91 @@ public class Solution {
         return result;
     }
 }
+
+//version-2: sort and merge with two pointers
+public class Solution {
+    /**
+     * @param nums1: an integer array
+     * @param nums2: an integer array
+     * @return: an integer array
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (isEmpty(nums1)) {
+	       return nums1;
+	    }
+
+	    if (isEmpty(nums2)) {
+            return nums2;
+	    }
+
+	    quickSort(nums1, 0, nums1.length - 1);
+	    quickSort(nums2, 0, nums2.length - 1);
+
+        int i = 0;
+	    int j = 0;
+
+	    List<Integer> values = new ArrayList<>();
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+                continue;
+            }
+
+            if (nums1[i] > nums2[j]) {
+                j++;
+                continue;
+            }
+
+            values.add(nums1[i]);
+            i++;
+            j++;
+        }
+
+        int[] result = new int[values.size()];
+        int index = 0;
+        for (int value : values) {
+            result[index++] = value;
+        }
+        
+        return result;
+    }
+
+    // helper method
+    private boolean isEmpty(int[] nums) {
+	    return nums == null || nums.length == 0;
+    }	
+    private void quickSort(int[] nums, int start, int end) {
+	    if (start >= end) {
+		    return;
+	    }
+
+	    int left = start;
+	    int right = end;
+
+	    int mid = left + (right - left) / 2;
+	    int pivot = nums[mid];
+
+	    while (left <= right) {
+		    while (left <= right && nums[left] < pivot) {
+        	    left++;
+            }
+
+            while (left <= right && nums[right] > pivot) {
+                right--;
+            }
+
+            if (left <= right) {
+                int tmp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = tmp;
+
+                left++;
+                right--;
+            }
+        }
+
+        quickSort(nums, start, right);
+        quickSort(nums, left, end);
+    }
+}
