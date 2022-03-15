01ee49a90a8a3851019fe47d56c939a309f4d5e8
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-15 15:04:51 -0500

    Update [LintCode-165]Merge Two Sorted Lists.java

--- source_code/[LintCode-165]Merge Two Sorted Lists.java
+++ source_code/[LintCode-165]Merge Two Sorted Lists.java
@@ -43,22 +43,22 @@ public class Solution {
         }
         
         ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
+        ListNode current = dummy;
         
         while(l1 != null && l2 != null) {
             if (l1.val < l2.val) {
-                tail.next = l1;
+                current.next = l1;
                 l1 = l1.next;
             }
             else {
-                tail.next = l2;
+                current.next = l2;
                 l2 = l2.next;
             }
             
-            tail = tail.next;
+            current = current.next;
         }
         
-        tail.next = (l1 != null) ? l1 : l2;
+        current.next = (l1 != null) ? l1 : l2;
         
         return dummy.next;
     }
