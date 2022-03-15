4c7e47324fbd39c85c0653c077e9fc4529f81b02
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-15 14:52:36 -0500

    Update [LintCode-486]Merge K Sorted Arrays.java

--- source_code/[LintCode-486]Merge K Sorted Arrays.java
+++ source_code/[LintCode-486]Merge K Sorted Arrays.java
@@ -2,25 +2,26 @@
 * LintCode 486. Merge K Sorted Arrays
 Given k sorted integer arrays, merge them into one sorted array.
 
-Example
-	Example 1:
-		Input: 
-		  [
-			[1, 3, 5, 7],
-			[2, 4, 6],
-			[0, 8, 9, 10, 11]
-		  ]
-		Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-	Example 2:
-		Input:
-		  [
-			[1,2,3],
-			[1,2]
-		  ]
-		Output: [1,1,2,2,3]
+
+Example 1:
+    Input: 
+      [
+        [1, 3, 5, 7],
+        [2, 4, 6],
+        [0, 8, 9, 10, 11]
+      ]
+    Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+Example 2:
+    Input:
+      [
+        [1,2,3],
+        [1,2]
+      ]
+    Output: [1,1,2,2,3]
 
 Challenge
-	Do it in O(N log k).
+    Do it in O(N log k).
 
 *N is the total number of integers.
 *k is the number of arrays.
