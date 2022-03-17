8f35cb9786c4fe8a982af65f53879232aff3bf63
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-17 16:49:01 -0500

    Update [LintCode-503]Anagram (Map Reduce).java

--- source_code/[LintCode-503]Anagram (Map Reduce).java
+++ source_code/[LintCode-503]Anagram (Map Reduce).java
@@ -1,17 +1,18 @@
 /***
 * LintCode 503. Anagram (Map Reduce)
 Use Map Reduce to find anagrams in a given list of words.
-Example
-	Example 1:
-		Input: "lint lint lnit ln"
-		Output: 
-		  ["lint", "lint", "lnit"]
-		  ["ln"]
-	Example 2:
-		Input: "ab ba cab"
-		Output:
-		  ["ab", "ba"]
-		  ["cab"]
+
+Example 1:
+    Input: "lint lint lnit ln"
+    Output: 
+      ["lint", "lint", "lnit"]
+      ["ln"]
+
+Example 2:
+    Input: "ab ba cab"
+    Output:
+      ["ab", "ba"]
+      ["cab"]
 ***/
 /**
  * Definition of OutputCollector:
@@ -21,26 +22,26 @@ Example
  * }
  */
 public class Anagram {
-	// field
-	private static final String SEPERATOR = " ";
-	private static final String EMPTY = "";
-	
-	// helper method
-	private static boolean isEmpty(String str) {
-		return str == null || str.length() == 0;
-	}
-	
-	private static String getAnagramKey(String value) {
-		if (isEmpty(value)) {
-			return EMPTY;
-		}
-		
-		char[] charArray = value.toCharArray();
-		
-		Arrays.sort(charArray);
-		
-		return new String(charArray);
-	}
+    // field
+    private static final String SEPERATOR = " ";
+    private static final String EMPTY = "";
+
+    // helper method
+    private static boolean isEmpty(String str) {
+        return str == null || str.length() == 0;
+    }
+
+    private static String getAnagramKey(String value) {
+        if (isEmpty(value)) {
+            return EMPTY;
+        }
+
+        char[] charArray = value.toCharArray();
+
+        Arrays.sort(charArray);
+
+        return new String(charArray);
+    }
 
     public static class Map {
         public void map(String key, String value,
@@ -48,11 +49,11 @@ public class Anagram {
             // Write your code here
             // Output the results into output buffer.
             // Ps. output.collect(String key, String value);
-			String[] tokens = value.split(SEPERATOR);
-			for (String token : tokens) {
-				String tokenKey = getAnagramKey(token);
-				output.collect(tokenKey, token);
-			}
+            String[] tokens = value.split(SEPERATOR);
+            for (String token : tokens) {
+                String tokenKey = getAnagramKey(token);
+                output.collect(tokenKey, token);
+            }
         }
     }
 
@@ -62,13 +63,13 @@ public class Anagram {
             // Write your code here
             // Output the results into output buffer.
             // Ps. output.collect(String key, List<String> value);
-			List<String> result = new ArrayList<String>();
-			while (values.hasNext()) {
-				String value = values.next();
-				result.add(value);
-			}
-			
-			output.collect(key, result);
+            List<String> result = new ArrayList<String>();
+            while (values.hasNext()) {
+                String value = values.next();
+                result.add(value);
+            }
+
+            output.collect(key, result);
         }
     }
 }
