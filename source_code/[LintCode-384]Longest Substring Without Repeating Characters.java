65ba626e798768b7a02551dc99e2b4f270e07739
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-18 16:23:17 -0500

    Update [LintCode-384]Longest Substring Without Repeating Characters.java

--- source_code/[LintCode-384]Longest Substring Without Repeating Characters.java
+++ source_code/[LintCode-384]Longest Substring Without Repeating Characters.java
@@ -2,19 +2,18 @@
 * LintCode 384. Longest Substring Without Repeating Characters
 Given a string, find the length of the longest substring without repeating characters.
 
-Example
-	Example 1:
-		Input: "abcabcbb"
-		Output: 3
-		Explanation: The longest substring is "abc".
+Example 1:
+    Input: "abcabcbb"
+    Output: 3
+    Explanation: The longest substring is "abc".
 
-	Example 2:
-		Input: "bbbbb"
-		Output: 1
-		Explanation: The longest substring is "b".
+Example 2:
+    Input: "bbbbb"
+    Output: 1
+    Explanation: The longest substring is "b".
 
 Challenge
-	time complexity O(n)
+    time complexity O(n)
 ***/
 // version-1
 public class Solution {
@@ -24,30 +23,30 @@ public class Solution {
      */
     public int lengthOfLongestSubstring(String s) {
         int result = 0;
-		// check corner case
-		if (s == null || s.length() == 0) {
-			return result;
-		}
-		
-		int i = 0, j = 0;
-		Set<Character> set = new HashSet<Character>();
-		int size = s.length();
-		
-		for (; i < size; i++) {
-			while (j < size && !set.contains(s.charAt(j))) {
-				set.add(s.charAt(j));
-				j++;
-				
-				result = Math.max(result, j - i);
-			}
-			
-			if (j < size && set.contains(s.charAt(j))) {
-				set.remove(s.charAt(i));
-			}
-		}
-		
-		return result;
-	}
+        // check corner case
+        if (s == null || s.length() == 0) {
+            return result;
+        }
+
+        int i = 0, j = 0;
+        Set<Character> set = new HashSet<Character>();
+        int size = s.length();
+
+        for (; i < size; i++) {
+            while (j < size && !set.contains(s.charAt(j))) {
+                set.add(s.charAt(j));
+                j++;
+
+                result = Math.max(result, j - i);
+            }
+
+            if (j < size && set.contains(s.charAt(j))) {
+                set.remove(s.charAt(i));
+            }
+        }
+
+        return result;
+    }
 }
 
 //version-2
