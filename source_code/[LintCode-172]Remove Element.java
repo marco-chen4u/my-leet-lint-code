9f7d60b60ebb0d07cc0c6e7b4effd7bb49ffbfc8
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-19 16:21:40 -0500

    Update [LintCode-172]Remove Element.java

--- source_code/[LintCode-172]Remove Element.java
+++ source_code/[LintCode-172]Remove Element.java
@@ -3,19 +3,19 @@
 Given an array and a value, remove all occurrences of that value in place and return the new length.
 The order of elements can be changed, and the elements after the new length don't matter.
 
-Example
+
 Example 1:
-	Input: [], value = 0
-	Output: 0
+    Input: [], value = 0
+    Output: 0
 
 Example 2:
-	Input:  [0,4,4,0,0,2,4,4], value = 4
-	Output: 4	
-	Explanation: 
-	the array after remove is [0,0,0,2]
+    Input:  [0,4,4,0,0,2,4,4], value = 4
+    Output: 4	
+    Explanation: 
+        the array after remove is [0,0,0,2]
 
 ***/
-//version-1
+//version-1: two pointers
 public class Solution {
     /*
      * @param A: A list of integers
@@ -46,7 +46,7 @@ public class Solution {
     }
 }
 
-//version-2
+//version-2: two pointers
 public class Solution {
     /*
      * @param nums: A list of integers
