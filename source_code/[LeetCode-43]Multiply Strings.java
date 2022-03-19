ca12a44ead7b01f5899064f2220695216cdd8020
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-18 21:38:24 -0500

    Update [LeetCode-43]Multiply Strings.java

--- source_code/[LeetCode-43]Multiply Strings.java
+++ source_code/[LeetCode-43]Multiply Strings.java
@@ -1,17 +1,17 @@
 /***
 * LeetCode 43. Multiply Strings
-Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+Given two non-negative integers num1 and num2 represented as strings, 
+return the product of num1 and num2, also represented as a string.
 
-Example:
-    Example 1:
-        Input: num1 = "2", num2 = "3"
-        Output: "6"
+Example 1:
+    Input: num1 = "2", num2 = "3"
+    Output: "6"
 
-    Example 2:
-        Input: num1 = "123", num2 = "456"
-        Output: "56088"
+Example 2:
+    Input: num1 = "123", num2 = "456"
+    Output: "56088"
 
-Note:
+*Note:
     1.The length of both num1 and num2 is < 110.
     2.Both num1 and num2 contain only digits 0-9.
     3.Both num1 and num2 do not contain any leading zero, except the number 0 itself.
