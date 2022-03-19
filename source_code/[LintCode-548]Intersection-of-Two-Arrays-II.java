dba7c93fe2326b37ae179ae7e3b62bba06b2c84b
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-19 18:33:00 -0500

    Update [LintCode-548]Intersection-of-Two-Arrays-II.java

--- source_code/[LintCode-548]Intersection-of-Two-Arrays-II.java
+++ source_code/[LintCode-548]Intersection-of-Two-Arrays-II.java
@@ -6,21 +6,21 @@
     
 Example1
     Input: 
-    nums1 = [1, 2, 2, 1], nums2 = [2, 2]
+        nums1 = [1, 2, 2, 1], nums2 = [2, 2]
     Output: 
-    [2, 2]
+        [2, 2]
 
 Example2
     Input: 
-    nums1 = [1, 1, 2], nums2 = [1]
+        nums1 = [1, 1, 2], nums2 = [1]
     Output: 
-    [1]
+        [1]
 
 Challenge
     -What if the given array is already sorted? How would you optimize your algorithm?
     -What if nums1's size is small compared to num2's size? Which algorithm is better?
     -What if elements of nums2 are stored on disk, 
-    and the memory is limited such that you cannot load all elements into the memory at once?
+     and the memory is limited such that you cannot load all elements into the memory at once?
     
 Link: https://www.lintcode.com/problem/548/
 
