cc391c325d17fb8ae61fbcb57431c398ef6a08ce
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-19 20:31:29 -0500

    Update [LintCode-418]Integer to Roman.java

--- source_code/[LintCode-418]Integer to Roman.java
+++ source_code/[LintCode-418]Integer to Roman.java
@@ -96,4 +96,45 @@ public class Solution {
     }
 }
 
+//version-2: Map interface(with TreeMap implementation) + reverseOrder initialization
+public class Solution {
 
+    // constant
+    private static final Map<Integer, String> intToRomanMap = new TreeMap<>(Collections.reverseOrder());
+
+    static {
+        intToRomanMap.put(1000, "M");
+        intToRomanMap.put(900, "CM");
+        intToRomanMap.put(500, "D");
+        intToRomanMap.put(400, "CD");
+        intToRomanMap.put(100, "C");
+        intToRomanMap.put(90, "XC");
+        intToRomanMap.put(50, "L");
+        intToRomanMap.put(40, "XL");
+        intToRomanMap.put(10, "X");
+        intToRomanMap.put(9, "IX");
+        intToRomanMap.put(5, "V");
+        intToRomanMap.put(4, "IV");
+        intToRomanMap.put(1, "I");
+    }
+
+    /**
+     * @param n: The integer
+     * @return: Roman representation
+     */
+    public String intToRoman(int n) {
+        String result = "";
+
+        for (Map.Entry<Integer, String> entry : intToRomanMap.entrySet()) {
+            int key = entry.getKey();
+            String romanValue = entry.getValue();
+
+            while (n - key >= 0) {
+                n -= key;
+                result += romanValue;
+            }
+        }
+
+        return result;
+    }
+}
