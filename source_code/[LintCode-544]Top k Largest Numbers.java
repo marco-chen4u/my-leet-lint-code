26e54d25344c1adc348c838da9fe720d9ec704b9
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-20 14:11:43 -0500

    Update [LintCode-544]Top k Largest Numbers.java

--- source_code/[LintCode-544]Top k Largest Numbers.java
+++ source_code/[LintCode-544]Top k Largest Numbers.java
@@ -110,3 +110,84 @@ public class Solution {
         quickSort(nums, left, end, k);
     }   
 }
+
+//version-3: heapify
+public class Solution {
+    
+    // fields
+    private int heapSize; //heap size
+
+    /**
+     * @param nums: an integer array
+     * @param k: An integer
+     * @return: the top k largest numbers in array
+     */
+    public int[] topk(int[] nums, int k) {
+        // corner cases
+        if (nums == null || nums.length == 0 || k <= 0) {
+            return new int[0];
+        }
+
+        int n = nums.length;
+        k = Math.min(k, n);
+
+        heapify(nums);
+        
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = pop(nums);
+        }
+
+        return result;
+    }
+
+    // helper methods
+    private void heapify(int[] nums) {
+        heapSize = nums.length;
+
+        for (int i = heapSize / 2; i >= 0; i--) {
+            siftDown(nums,i);
+        }
+
+    }
+
+    private void siftDown(int[] nums, int index) {
+        while (index < heapSize) {
+            int left = index * 2 + 1;
+            int right = index * 2 + 2;
+
+            int max = index;
+
+            if (left < heapSize && nums[left] > nums[max]) {
+                max = left;
+            }
+
+            if (right < heapSize && nums[right] > nums[max]) {
+                max = right;
+            }
+
+            if (max == index) {
+                break;
+            }
+
+            int tmp = nums[index];
+            nums[index] = nums[max];
+            nums[max] = tmp;
+
+            index = max;
+        }
+    }
+
+    private int pop(int[] nums) {
+        int result = nums[0];
+
+        int lastPos = heapSize - 1;
+        nums[0] = nums[lastPos];
+
+        heapSize -= 1;
+
+        siftDown(nums, 0);
+
+        return result;
+    }
+}
