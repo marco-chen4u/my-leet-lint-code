fa2337cf0274e3d29b0d647212aa8125f7a4ae8c
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-19 20:05:33 -0500

    Update [LintCode-418]Integer to Roman.java

--- source_code/[LintCode-418]Integer to Roman.java
+++ source_code/[LintCode-418]Integer to Roman.java
@@ -2,6 +2,7 @@
 * LintCode 418. Integer to Roman
 Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
         Symbol       Value
+        -------------------
         I             1
         V             5
         X             10
@@ -9,6 +10,7 @@ Roman numerals are represented by seven different symbols: I, V, X, L, C, D and 
         C             100
         D             500
         M             1000
+        -------------------
 For example, two is written as II in Roman numeral, just two one's added together. 
 Twelve is written as, XII, which is simply X + II. 
 The number twenty seven is written as XXVII, which is XX + V + II.
