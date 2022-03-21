512b91e9034042c399dcea351bbed4359b72c8ea
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-20 21:38:35 -0500

    Update [LintCode-54]String to Integer (atoi).java

--- source_code/[LintCode-54]String to Integer (atoi).java
+++ source_code/[LintCode-54]String to Integer (atoi).java
@@ -4,24 +4,24 @@ Implement function atoi to convert a string to an integer.
 If no valid conversion could be performed, a zero value is returned.
 If the correct value is out of the range of representable values, 
 INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.
-Example
-    Example 1
-        Input: "10"
-        Output: 10
-    
-    Example 2
-        Input: "1"
-        Output: 1
-    
-    Example 3
-        Input: "123123123123123"
-        Output: 2147483647
-        Explanation: 123123123123123 > INT_MAX, so we return INT_MAX
-    
-    Example 4
-        Input: "1.0"
-        Output: 1
-        Explanation: We just need to change the first vaild number
+
+Example 1
+    Input: "10"
+    Output: 10
+
+Example 2
+    Input: "1"
+    Output: 1
+
+Example 3
+    Input: "123123123123123"
+    Output: 2147483647
+    Explanation: 123123123123123 > INT_MAX, so we return INT_MAX
+
+Example 4
+    Input: "1.0"
+    Output: 1
+    Explanation: We just need to change the first vaild number
 ***/
 public class Solution {
     public int atoi(String str) {
