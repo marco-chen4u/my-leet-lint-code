cd70a9b923a63ce5cb6ffb705e9e0bcafc4f530b
Marco Chen <haisheng.chen4u@foxmail.com>
2022-03-20 20:50:48 -0500

    Update [LintCode-408]Add Binary.java

--- source_code/[LintCode-408]Add Binary.java
+++ source_code/[LintCode-408]Add Binary.java
@@ -2,17 +2,17 @@
 * LintCode 408. Add Binary
 Given two binary strings, return their sum (also a binary string).
 
-Example
-	Example 1:
-		Input:
-		a = "0", b = "0"
-		Output:
-		"0"
-	Example 2:
-		Input:
-		a = "11", b = "1"
-		Output:
-		"100"
+Example 1:
+    Input:
+        a = "0", b = "0"
+    Output:
+        "0"
+
+Example 2:
+    Input:
+        a = "11", b = "1"
+    Output:
+        "100"
 ***/
 //version-1
 public class Solution {
@@ -91,5 +91,79 @@ public class Solution {
             ans = carry + ans;
         }
         return ans;
+    }
+}
+
+//version-3:
+public class Solution {
+    /**
+     * @param a: a number
+     * @param b: a number
+     * @return: the result
+     */
+    public String addBinary(String a, String b) {
+        // corner case
+        if (isEmpty(a)) {
+            return b;
+        }
+
+        if (isEmpty(b)) {
+            return a;
+        }
+
+        char[] chars1 = reverse(a.toCharArray());
+        char[] chars2 = reverse(b.toCharArray());
+        int size1 = chars1.length;
+        int size2 = chars2.length;
+
+        int size = Math.max(size1, size2);
+        char[] chars = new char[size];
+        int index = 0;
+        int i = 0;
+        int j = 0;
+
+        int carry = 0;
+        while (i < size1 || j < size2) {
+            int value1 = i < size1 ? chars1[i] - '0' : 0;
+            int value2 = j < size2 ? chars2[j] - '0' : 0;
+
+            int sum = value1 + value2 + carry;
+            chars[index++] = (char)(sum % 2 + '0');
+
+            carry = sum / 2;
+
+            i++;
+            j++;
+        }
+
+        String result = String.valueOf(reverse(chars));
+
+        if (carry != 0) {
+            result = "1" + result;
+        }
+
+        return result;
+
+    }
+
+    // helper methods
+    private boolean isEmpty(String str) {
+        return str == null || str.isEmpty();
+    }
+
+    private char[] reverse(char[] chars) {
+        int left = 0;
+        int right = chars.length - 1;
+
+        while (left < right) {
+            char tmp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = tmp;
+
+            left++;
+            right--;
+        }
+
+        return chars;
     }
 }
