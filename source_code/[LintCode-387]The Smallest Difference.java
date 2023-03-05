c029f05daf65229a3ff32ca07d0f04e9ce851c2f
Marco Chen <haisheng.chen4u@foxmail.com>
2023-03-04 21:46:28 -0600

    Update [LintCode-387]The Smallest Difference.java

--- source_code/[LintCode-387]The Smallest Difference.java
+++ source_code/[LintCode-387]The Smallest Difference.java
@@ -1,6 +1,10 @@
 /***
 * LintCode 387. The Smallest Difference
-Given two array of integers(the first array is array A, the second array is array B), now we are going to find a element in array A which is A[i], and another element in array B which is B[j], so that the difference between A[i] and B[j] (|A[i] - B[j]|) is as small as possible, return their smallest difference.
+Given two array of integers(the first array is array A, the second array is array B), 
+now we are going to find a element in array A which is A[i], and another element in array B which is B[j], 
+so that the difference between A[i] and B[j] (|A[i] - B[j]|) is as small as possible, 
+return their smallest difference.
+
 Example
     Example 1:
         Input: A = [3, 6, 7, 4], B = [2, 8, 9, 3]
