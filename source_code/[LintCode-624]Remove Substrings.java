592ccd1c895244db45f17759f5c45d19a20b52fd
Marco Chen <haisheng.chen4u@foxmail.com>
2023-03-08 21:27:32 -0600

    Update [LintCode-624]Remove Substrings.java

--- source_code/[LintCode-624]Remove Substrings.java
+++ source_code/[LintCode-624]Remove Substrings.java
@@ -5,22 +5,22 @@ You are supposed to remove every instance of those n substrings from s
 so that s is of the minimum length and output this minimum length.
 
 Example 1:
-        Input:
-            "ccdaabcdbb"
-            ["ab","cd"]
-        Output:
-            2
-        Explanation: 
-            ccdaabcdbb -> ccdacdbb -> cabb -> cb (length = 2)
+    Input:
+        "ccdaabcdbb"
+        ["ab","cd"]
+    Output:
+        2
+    Explanation: 
+        ccdaabcdbb -> ccdacdbb -> cabb -> cb (length = 2)
 
 Example 2:
-        Input:
-            "abcabd"
-            ["ab","abcd"]
-        Output:
-            0
-        Explanation: 
-            abcabd -> abcd -> "" (length = 0)
+    Input:
+        "abcabd"
+        ["ab","abcd"]
+    Output:
+        0
+    Explanation: 
+        abcabd -> abcd -> "" (length = 0)
 ***/
 public class Solution {
 
