6692e671a43a96c029dfa3758e91220d2487dbb9
Marco Chen <haisheng.chen4u@foxmail.com>
2023-03-09 14:53:49 -0600

    Update [LintCode-596]Minimum Subtree-1.java

--- source_code/[LintCode-596]Minimum Subtree-1.java
+++ source_code/[LintCode-596]Minimum Subtree-1.java
@@ -3,23 +3,23 @@
 Given a binary tree, find the subtree with minimum sum. Return the root of the subtree.
 
 Example
-	Example 1:
-		Input:
-				 1
-			   /   \
-			 -5     2
-			 / \   /  \
-			0   2 -4  -5 
-		Output:1
+    Example 1:
+        Input:
+                 1
+               /   \
+             -5     2
+             / \   /  \
+            0   2 -4  -5 
+        Output:1
 
-	Example 2:
-		Input:
-		   1
-		Output:1
+    Example 2:
+        Input:
+           1
+        Output:1
 
 Notice
-	LintCode will print the subtree which root is your return node.
-	It's guaranteed that there is only one subtree with minimum sum and the given binary tree is not an empty tree.
+    LintCode will print the subtree which root is your return node.
+    It's guaranteed that there is only one subtree with minimum sum and the given binary tree is not an empty tree.
 ***/
 /**
  * Definition of TreeNode:
@@ -42,27 +42,78 @@ public class Solution {
      */
     public TreeNode findSubtree(TreeNode root) {
         // check corner case
-		if (root == null) {
-			return null;
-		}
-		
-		findSubtreeHelper(root);
-		
-		return minSubtreeSum;
+        if (root == null) {
+            return null;
+        }
+
+        findSubtreeHelper(root);
+
+        return minSubtreeSum;
     }
-	
-	private int findSubtreeHelper(TreeNode node) {
-		if (node == null) {
-			return 0;
-		}
-		
-		int sum = node.val + findSubtreeHelper(node.left) + findSubtreeHelper(root.right);
-		
-		if (sum < minSubtreeSum) {
-			minSubtreeSum = sum;
-			minSumSubtree = node;
-		}
-		
-		return sum;
-	}
+
+    // helper methods
+    private int findSubtreeHelper(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int sum = node.val + findSubtreeHelper(node.left) + findSubtreeHelper(root.right);
+
+        if (sum < minSubtreeSum) {
+            minSubtreeSum = sum;
+            minSumSubtree = node;
+        }
+
+        return sum;
+    }
+}
+
+// version-2 : devide & conquer - introduce new class definition of the pair holding TreeNode and min value
+public class Solution {
+    // inner class
+    class ResultType {
+        // fields
+        TreeNode node;
+        int sum;
+        int minSum;
+        // construction
+        public ResultType(TreeNode node, int sum, int minSum) {
+            this.node = node;
+            this.sum = sum;
+            this.minSum = minSum;
+        }
+    }
+
+    /**
+     * @param root: the root of binary tree
+     * @return: the root of the minimum subtree
+     */
+    public TreeNode findSubtree(TreeNode root) {	
+        ResultType result = findSubtreeHelper(root);
+        return result.node;		
+    }
+
+    // helper method
+    private ResultType findSubtreeHelper(TreeNode node) {
+        if (node == null) {
+            return new ResultType(null, 0, Integer.MAX_VALUE);
+        }
+        ResultType leftResult = findSubtreeHelper(node.left);
+        ResultType rightResult = findSubtreeHelper(node.right);
+        ResultType result = new ResultType(node, 
+                                            node.val + leftResult.sum + rightResult.sum,
+                                            node.val + leftResult.sum, rightResult.sum);
+
+        if (result.minSum > leftResult.minSum) {
+            result.node = leftResult.node;
+            result.minSum = leftResult.minSum;
+        }
+
+        if (result.minSum > rightResult.minSum) {
+            result.node = rightResult.node;
+            result.minSum = rightResult.minSum;
+        }
+
+        return result;
+    }
 }
