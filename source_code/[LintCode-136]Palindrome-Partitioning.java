baf877fef03549ca3cdca1101ccc1b0c55825113
Marco Chen <haisheng.chen4u@foxmail.com>
2023-03-21 22:26:45 -0500

    Update and rename [LintCode-136]Palindrome Partitioning-1.java to [LintCode-136]Palindrome-Partitioning.java

--- source_code/[LintCode-136]Palindrome-Partitioning.java
+++ source_code/[LintCode-136]Palindrome-Partitioning.java
@@ -10,7 +10,77 @@ Example
           ["a","a","b"]
         ]
 ***/
-// version 1: DP
+//version-1: DFS-combination
+public class Solution {
+    /*
+     * @param s: A string
+     * @return: A list of lists of string
+     */
+    public List<List<String>> partition(String s) {
+        // write your code here
+        List<List<String>> result = new ArrayList<>();
+        if (s == null) {
+            return result;
+        }
+
+        List<String> combination = new ArrayList<String>();
+        if (s.isEmpty()) {
+            result.add(combination);
+            return result;
+        }
+
+        find(s, 0, combination, result);
+
+        return result;
+    }
+
+    // helper method
+    private void find(String str, int currentIndex, List<String> combination, List<List<String>> result) {
+        if (currentIndex == str.length()) {
+            result.add(new ArrayList<String>(combination));
+            return;
+        }
+
+        for (int i = currentIndex + 1; i <= str.length(); i++) {
+            String currentStr = str.substring(currentIndex, i);
+            if (!isPalindome(currentStr)) {
+                continue;
+            }
+
+            combination.add(currentStr);
+            find(str, i, combination, result);
+            combination.remove(combination.size() - 1);
+        }
+    }
+
+    private boolean isPalindome(String str) {
+        if (str == null || str.isEmpty()) {
+            return true;
+        }
+
+        if (str != null && str.length()== 1) {
+            return true;
+        }
+
+        // tow pointers
+        int i = 0;
+        int j = str.length() - 1;
+
+        while (i < j) {
+            
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
+
+// version-2: DP
 public class Solution {
     List<List<String>> results;
     boolean[][] isPalindrome;
@@ -29,11 +99,12 @@ public class Solution {
         
         getIsPalindrome(s);
         
-        helper(s, 0, new ArrayList<Integer>());
+        find(s, 0, new ArrayList<Integer>());
         
         return results;
     }
     
+    // helper method
     private void getIsPalindrome(String s) {
         // initialize
         int n = s.length();
@@ -55,7 +126,7 @@ public class Solution {
         }
     }
 
-    private void helper(String s,
+    private void find(String s,
                         int startIndex,
                         List<Integer> partition) {
         if (startIndex == s.length()) {
@@ -68,7 +139,7 @@ public class Solution {
                 continue;
             }
             partition.add(i);
-            helper(s, i + 1, partition);
+            find(s, i + 1, partition);
             partition.remove(partition.size() - 1);
         }
     }
