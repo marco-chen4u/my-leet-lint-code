92c7f3f3ecd05b6abac6caffaad37596aa2c3183
Marco Chen <haisheng.chen4u@foxmail.com>
2023-04-10 14:26:39 -0500

    Update [LintCode-554]Sort Integers (Map Reduce).java

--- source_code/[LintCode-554]Sort Integers (Map Reduce).java
+++ source_code/[LintCode-554]Sort Integers (Map Reduce).java
@@ -6,20 +6,19 @@ In the reducer, your can specify what the key / value is (this depends on how yo
 For the output of the reducer class, the key can be anything you want, the value should be ordered. 
 (the order is depending on when you output it)
 
-Example
 Example 1:
-	Input:
-		1: [14,7,9]
-		2: [10,1]
-		3: [2,5,6,3]
-		4: []
-	Output:
-		[1,2,3,5,6,7,9,10,14]
+    Input:
+        1: [14,7,9]
+        2: [10,1]
+        3: [2,5,6,3]
+        4: []
+    Output:
+        [1,2,3,5,6,7,9,10,14]
 Example 2:
-	Input:
-		1: [14,7]
-	Output:
-		[7,14]
+    Input:
+        1: [14,7]
+    Output:
+        [7,14]
 ***/
 /**
  * Definition of OutputCollector:
