6ade61605d17205c429f526cea0297b019705b6c
Marco Chen <haisheng.chen4u@foxmail.com>
2023-04-10 15:27:28 -0500

    Update [LintCode-537]N-Gram (Map Reduce).java

--- source_code/[LintCode-537]N-Gram (Map Reduce).java
+++ source_code/[LintCode-537]N-Gram (Map Reduce).java
@@ -4,29 +4,28 @@ Give a number of strings and the number N.
 Use the Map Reduce method to count all N-Grams and their occurrences. 
 The letter is granular.
 
-Example
-    Example 1:
-        Input: N = 3
-            doc_1: "abcabc"
-            doc_2: "abcabc"
-            doc_3: "bbcabc"
-        Output:
-            [
-              "abc": ５,
-              "bbc": 1, 
-              "bca": 3,
-              "cab": 3
-            ]
+Example 1:
+    Input: N = 3
+        doc_1: "abcabc"
+        doc_2: "abcabc"
+        doc_3: "bbcabc"
+    Output:
+        [
+          "abc": ５,
+          "bbc": 1, 
+          "bca": 3,
+          "cab": 3
+        ]
 
-    Example 2:
-        Input: N=3
-            doc_1: "abcabc"
-        Output:
-            [
-              "abc": 2, 
-              "bca": 1,
-              "cab": 1
-            ]
+Example 2:
+    Input: N=3
+        doc_1: "abcabc"
+    Output:
+        [
+          "abc": 2, 
+          "bca": 1,
+          "cab": 1
+        ]
 ***/
 /**
  * Definition of OutputCollector:
