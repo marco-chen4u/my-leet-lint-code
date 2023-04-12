1a6680636b5357acd2daecd8e806e9437e4daed7
Marco Chen <haisheng.chen4u@foxmail.com>
2023-04-12 13:06:14 -0500

    Create [LeetCode-1814]Count-Nice-Pairs-in-an-Array.java

---
+++ source_code/[LeetCode-1814]Count-Nice-Pairs-in-an-Array.java
@@ -0,0 +1,65 @@
+/***
+* LeetCode 1814. Count Nice Pairs in an Array
+You are given an array nums that consists of non-negative integers. 
+Let us define rev(x) as the reverse of the non-negative integer x. 
+For example, rev(123) = 321, and rev(120) = 21. A pair of indices (i, j) is nice if it satisfies all of the following conditions:
+
+0 <= i < j < nums.length
+nums[i] + rev(nums[j]) == nums[j] + rev(nums[i])
+Return the number of nice pairs of indices. Since that number can be too large, return it modulo 10^9 + 7.
+
+
+Example 1
+    Input: nums = [42,11,1,97]
+    Output: 2
+    Explanation: The two pairs are:
+     - (0,3) : 42 + rev(97) = 42 + 79 = 121, 97 + rev(42) = 97 + 24 = 121.
+     - (1,2) : 11 + rev(1) = 11 + 1 = 12, 1 + rev(11) = 1 + 11 = 12.
+     
+Example 2
+    Input: nums = [13,10,35,24,76]
+    Output: 4
+    
+Constraints:
+    1 <= nums.length <= 10^5
+    0 <= nums[i] <= 10^9
+    
+LeetCode link: https://leetcode.com/problems/count-nice-pairs-in-an-array/
+
+***/
+//version-1: Math + HashMap + counting
+class Solution {
+    private static int MOD = (int)(Math.pow(10, 9) + 7;
+
+    public int countNicePairs(int[] nums) {
+
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int num : nums) {
+            int diff = num - getReverse(num);
+            int repeatCount = map.getOrDefault(diff, 0);
+
+            count = (count + repeatCount) % MOD;
+
+            map.put(diff,repeatCount +1);            
+        }
+
+        return count;
+    }
+
+    // helper method
+    private int getReverse(int num) {
+        int result = 0;
+
+        while (num != 0) {
+            result = result * 10 + num % 10;
+            num /= 10;
+        }
+
+        return result;
+    }
+}
