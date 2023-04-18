68d621ef6294c19530b0a6649b3ec48e2cd3d732
Marco Chen <haisheng.chen4u@foxmail.com>
2023-04-18 18:03:58 -0500

    Update [LintCode-480]Binary Tree Paths.java

--- source_code/[LintCode-480]Binary Tree Paths.java
+++ source_code/[LintCode-480]Binary Tree Paths.java
@@ -1,23 +1,23 @@
 /***
 * LintCode 480. Binary Tree Paths
 Given a binary tree, return all root-to-leaf paths.
-Example
-	Example 1:
-		Input：{1,2,3,#,5}
-		Output：["1->2->5","1->3"]
-		Explanation：
-		   1
-		 /   \
-		2     3
-		 \
-		  5	  
-	Example 2:
-		Input：{1,2}
-		Output：["1->2"]
-		Explanation：
-		   1
-		 /   
-		2     
+
+Example 1:
+    Input：{1,2,3,#,5}
+    Output：["1->2->5","1->3"]
+    Explanation：
+           1
+         /   \
+        2     3
+         \
+          5	  
+Example 2:
+    Input：{1,2}
+    Output：["1->2"]
+    Explanation：
+           1
+         /   
+        2     
 ***/
 /*
  * Definition of TreeNode:
