ffb8eb5cf361c75aeabb208798981e3e5970d032
Marco Chen <haisheng.chen4u@foxmail.com>
2023-05-16 18:24:04 -0700

    Update [LintCode-65]Median of two Sorted Arrays.java

--- source_code/[LintCode-65]Median of two Sorted Arrays.java
+++ source_code/[LintCode-65]Median of two Sorted Arrays.java
@@ -16,7 +16,54 @@ Example 2
 
 Challenge
     The overall run time complexity should be O(log (m+n)).
+    
+LintCode link: https://www.lintcode.com/problem/65/
+LeetCode link: https://leetcode.com/problems/median-of-two-sorted-arrays/
 ***/
+//version-1: merge-sort(two pointers), works perfectly in leetcode but not lintcode
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int n = n1 + n2;
+
+        int[] nums = new int[n];
+        int index = 0;
+
+        // two pointers
+        int i = 0;
+        int j = 0;
+        while (i < n1 && j < n2) {
+            if(nums1[i] <= nums2[j]) {
+                nums[index++] = nums1[i++];
+            }
+            else {
+                nums[index++] = nums2[j++];
+            }
+        }
+
+        while (i < n1) {
+            nums[index++] = nums1[i++];
+        }
+
+        while (j < n2) {
+            nums[index++] = nums2[j++];
+        }
+
+        double result = 0.0;
+
+        if (n % 2 == 1) {
+            result = (float) nums[n / 2];
+        }
+        else {
+            result = (float)(nums[n / 2 - 1] + nums[ n / 2]) / 2;
+        }
+
+        return result;
+    }
+}
+
+//version-2: recursion
 public class Solution {
     /*
      * @param A: An integer array
