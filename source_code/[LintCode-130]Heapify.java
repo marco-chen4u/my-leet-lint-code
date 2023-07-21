e221c148058305cebf4156242b77e16c6b3d9ec6
Marco Chen <haisheng.chen4u@foxmail.com>
2023-07-20 17:20:06 -0700

    Update [LintCode-130]Heapify.java

--- source_code/[LintCode-130]Heapify.java
+++ source_code/[LintCode-130]Heapify.java
@@ -3,11 +3,11 @@
 Given an integer array, heapify it into a min-heap array.
 
 For a heap array A, A[0] is the root of heap, and for each A[i], A[i * 2 + 1] is the left child of A[i] and A[i * 2 + 2] is the right child of A[i].
-Example
+
 Example 1
-Input : [3,2,1,4,5]
-Output : [1,2,3,4,5]
-Explanation : return any one of the legitimate heap arrays
+    Input : [3,2,1,4,5]
+    Output : [1,2,3,4,5]
+    Explanation : return any one of the legitimate heap arrays
 
 Challenge
     O(n) time complexity
