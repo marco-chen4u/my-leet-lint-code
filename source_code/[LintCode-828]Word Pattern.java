d600dafb522e4111d50aefaebf6d592ca872fbf5
Marco Chen <haisheng.chen4u@foxmail.com>
2023-12-06 09:38:16 -0800

    Update and rename [LintCode-828]Word Pattern-1.java to [LintCode-828]Word Pattern.java

--- source_code/[LintCode-828]Word Pattern.java
+++ source_code/[LintCode-828]Word Pattern.java
@@ -29,7 +29,12 @@ Example4
         The pattern of str is abbc
 Notice
     You may assume pattern contains only lowercase letters, and str contains lowercase letters separated by a single space.
+
+Link: 
+    (1)LintCode: https://www.lintcode.com/problem/828/
+    (2)LeetCode: https://leetcode.com/problems/word-pattern/
 ***/
+//solution-1
 public class Solution {
     /**
      * @param pattern: a string, denote pattern string
@@ -73,6 +78,70 @@ public class Solution {
                     !patternMapping.containsKey(key) &&
                     !patternMapping.containsValue(value)) {
                 keyRecord.add(key);
+                patternMapping.put(key, value);
+
+                continue;
+            }
+
+            if (value.equals(patternMapping.get(key))) {
+                continue;
+            }
+            else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+//solution-2
+public class Solution {
+    /**
+     * @param pattern: a string, denote pattern string
+     * @param teststr: a string, denote matching string
+     * @return: an boolean, denote whether the pattern string and the matching string match or not
+     */
+    public boolean wordPattern(String pattern, String teststr) {
+        String SEPERATOR = " ";
+		
+        // check corner cases
+        if ((pattern == null && teststr == null) ||
+            (pattern.length() == 0 && teststr.length() == 0)) {
+            return true;
+        }
+
+        if ((pattern == null || pattern.length() == 0) && 
+            (teststr != null && teststr.length() > 0)) {
+            return false;
+        }
+
+        if ((pattern != null && pattern.length() > 0) 
+            && (teststr == null || teststr.length() == 0)) {
+            return false;
+        }
+
+        // initialize
+        int patternSize = pattern.length();
+
+        String[] testStrings = teststr.split(SEPERATOR);
+        if (patternSize != testStrings.length) {
+            return false;
+        }
+
+        Set<Character> keyRecord = new HashSet<Character>();
+        Set<String> valueRecord = new HashSet<String>();
+        Map<Character, String> patternMapping = new HashMap<Character, String>();
+
+        // calculate the mapping
+        for (int index = 0; index < patternSize; index++) {
+            char key = pattern.charAt(index);
+            String value = testStrings[index];
+
+            if (!keyRecord.contains(key) && !valueRecord.contains(value)) {
+                keyRecord.add(key);
+                valueRecord.add(value);
+
                 patternMapping.put(key, value);
 
                 continue;
