1a729e713de63a37b8e0df3f1cae660ad243b18b
Marco Chen <haisheng.chen4u@foxmail.com>
2023-12-06 09:18:59 -0800

    Update [LintCode-34]N-Queens II.java

--- source_code/[LintCode-34]N-Queens II.java
+++ source_code/[LintCode-34]N-Queens II.java
@@ -3,49 +3,62 @@
 Follow up for N-Queens problem.
 Now, instead outputting board configurations, return the total number of distinct solutions.
 
-Example
-	Example 1:
-		Input: n=1
-		Output: 1
-		Explanation:
-			1:
-			1
+Example 1:
+    Input: n=1
+    Output: 1
+    Explanation:
+        1:
+        1
 
-	Example 2:
-		Input: n=4
-		Output: 2
-		Explanation:
-			1:
-			0 0 1 0
-			1 0 0 0
-			0 0 0 1
-			0 1 0 0
-			2:
-			0 1 0 0 
-			0 0 0 1
-			1 0 0 0
-			0 0 1 0
+Example 2:
+    Input: n=4
+    Output: 2
+    Explanation:
+        1:
+        0 0 1 0
+        1 0 0 0
+        0 0 0 1
+        0 1 0 0
+        2:
+        0 1 0 0 
+        0 0 0 1
+        1 0 0 0
+        0 0 1 0
 ***/
 public class Solution {
     
     // helper methods
-    private boolean isValid(List<Integer> rows, int col) {
-        int row = rows.size();
-        for (int i = 0; i < rows.size(); i++) {
-            // direct attack
-            if (rows.get(i) == col) {
+    private boolean isValid(List<Integer> rows, int currentVal) {
+        int newIndex = rows.size();
+
+        int index = 0;
+        for (int columnVal : rows) {
+            // same column
+            if (currentVal == columnVal) {
                 return false;
             }
             
-            // slash attach[(x,y) <-> (x + 1, y - 1)]
-            if (i + rows.get(i) == row + col) {
+            // slash attach (left-top -> right-bottom)
+            /*
+               x + 1 = x'
+	                     => x - y = x' - y'
+	       y + 1 = y'
+            */
+            if (index - columnVal == currentVal - newIndex){
                 return false;
             }
             
-            // back-slash attack[(x,y) <-> (x + 1, y + 1)]
-            if (i - rows.get(i) == row - col) {
+            // back-slash attach (left-bottom -> right-top)
+            /*
+               x + 1 = x'
+	                     => x + y = x' + y'
+	       y - 1 = y'
+            */
+            if (index + columnVal == newIndex + currentVal){
                 return false;
             }
+
+            index += 1;
         }
         
         return true;
