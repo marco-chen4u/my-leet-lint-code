fb433d15a10616665e69ec5401242d7b6debe3f9
Marco Chen <haisheng.chen4u@foxmail.com>
2023-12-08 12:09:25 -0800

    Update [LinTcode-780]Remove Invalid Parentheses.java

--- source_code/[LinTcode-780]Remove Invalid Parentheses.java
+++ source_code/[LinTcode-780]Remove Invalid Parentheses.java
@@ -27,6 +27,7 @@ Link
     LintCode: https://www.lintcode.com/problem/780/
     LeetCode: https://leetcode.com/problems/remove-invalid-parentheses/
 ***/
+solution-1: dfs
 public class Solution {
     // field
     private final String EMPTY = "";// empty value string
@@ -95,37 +96,6 @@ public class Solution {
         
         return count == 0;
     }
-
-
-    
-/*  
-    private boolean isValidParenthese(String str) {
-        if (str == null || str.isEmpty()) {
-            return true;
-        }
-
-        char[] chars = str.toCharArray();
-        Stack<Character> stack = new Stack<>();
-
-        for (char ch : str.toCharArray()) {
-            if (isLeftBracket(ch)) {
-                stack.push(ch);
-                continue;
-            }
-
-            if (isRightBracket(ch) && !stack.isEmpty() && isPair(stack.peek(), ch)) {
-                stack.pop();
-                continue;
-            }
-        }
-
-        return stack.isEmpty();
-    }
-    
-    private boolean isPair(char left, char right) {
-        return isLeftBracket(left) && isRightBracket(right);
-    }
-*/
     
     private void dfs(List<String> result, 
                         String str, int startIndex, 
@@ -161,4 +131,112 @@ public class Solution {
         }
     }
    
+}
+
+// solution-2: dfs
+class Solution {
+    private final String EMPTY_STR = "";
+    private final char DEFAULT_CHAR = '$';
+
+    public List<String> removeInvalidParentheses(String s) {
+        List<String> results = new ArrayList<>();
+
+        if (s == null || s.isEmpty()) {
+            results.add(EMPTY_STR);
+            return results;
+        }
+
+        int leftCount = 0;
+        int rightCount = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (isLeft(ch)) {
+                leftCount += 1;
+                continue;
+            }
+
+            if (isRight(ch)) {
+                if (leftCount > 0) {
+                    leftCount -= 1;
+                }
+                else {
+                    rightCount += 1;
+                }
+            }
+        }
+
+        find(s, 0, leftCount, rightCount, results);
+
+        return results;
+    }
+
+    private void find(String str, int currentIndex, int leftCount, int rightCount, List<String> results) {
+        if (leftCount == 0 && rightCount == 0) {
+            if (isValid(str)) {
+                results.add(str);
+            }
+
+            return;
+        }
+
+        for (int i = currentIndex; i < str.length(); i++) {
+            char current = str.charAt(i);
+            char pre = i > 0 ? str.charAt(i - 1) : DEFAULT_CHAR;
+
+            // [1] check if more left brackets
+            if (leftCount > 0 && 
+                isLeft(current) && 
+                (pre != current)) {
+                
+                String newStr = str.substring(0, i) + str.substring(i + 1);// remove the current character
+                find(newStr, i, leftCount - 1, rightCount, results);
+            }
+
+            // [2] check if more right brackets
+            if (rightCount > 0 &&
+                isRight(current) && 
+                (pre != current)) {
+                
+                String newStr = str.substring(0, i) + str.substring(i + 1);
+                find(newStr, i, leftCount, rightCount - 1, results);
+            }
+
+        }
+    }
+
+    private boolean isLeft(char ch) {
+        return '(' == ch;
+    }
+
+    private boolean isRight(char ch) {
+        return ')' == ch;
+    }
+
+    private boolean isPair(char left, char right) {
+        return isLeft(left) && isRight(right);
+    }
+
+    private boolean isValid(String str) {
+        if (str == null || str.isEmpty()) {
+            return true;
+        }
+
+        char[] chars = str.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+            if (isLeft(ch)) {
+                stack.push(ch);
+                continue;
+            }
+
+            if (isRight(ch) && !stack.isEmpty() && isPair(stack.peek(), ch)) {
+                stack.pop();
+                continue;
+            }
+        }
+
+        return stack.isEmpty();
+
+    }
 }
