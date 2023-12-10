c28b9a1eaf46570692f636f8aeffb3018a2c9b19
Marco Chen <haisheng.chen4u@foxmail.com>
2023-12-10 10:44:50 -0800

    Update [LintCode-52]Next Permutation.java

--- source_code/[LintCode-52]Next Permutation.java
+++ source_code/[LintCode-52]Next Permutation.java
@@ -76,3 +76,76 @@ public class Solution {
         nums[j] = tmp;
     }
 }
+
+// solution-2: inplace + sort[quick-sort]  note: quick-sort's time complexity is O(nlogn)
+/*
+ find the index to replace and the cadidate which is just a little bit larger value than this to-replace item, then do the swap, and sort the rest part after the index to replace
+*/
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int size = nums.length;
+
+        int lastIndex = size - 1;
+        int indexToReplace = lastIndex - 1;
+
+        while (indexToReplace >= 0) {
+            if (nums[indexToReplace] < nums[indexToReplace + 1]) break;
+            indexToReplace--;
+        }
+
+        //System.out.println("indexToReplace = " + indexToReplace);
+        if (indexToReplace == -1) {
+            quickSort(nums, 0, lastIndex);
+            return;
+        }
+
+        int largerIndex = lastIndex;
+        while (largerIndex > indexToReplace && nums[largerIndex] <= nums[indexToReplace]) {
+            largerIndex--;
+        }
+        //System.out.println("largerIndex = " + largerIndex);
+
+        swap(nums, indexToReplace, largerIndex);
+
+        quickSort(nums, indexToReplace + 1, lastIndex);
+    }
+
+    // helper methods
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void quickSort(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int left = start;
+        int right = end;
+        int mid = start + (end - start) / 2;
+        int pivot = nums[mid];
+
+        while (left <= right) {
+            while (left <= right && nums[left] < pivot) {
+                left++;
+            }
+
+            while (left <= right && nums[right] > pivot) {
+                right--;
+            }
+
+            if (left <= right) {
+                swap(nums, left, right);
+
+                left++;
+                right--;
+            }
+        }
+
+        quickSort(nums, start, right);
+        quickSort(nums, left, end);
+    }
+}
