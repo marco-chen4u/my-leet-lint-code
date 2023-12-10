b72a5f7ddae996731cb9ad599f03b6e7789b7bd8
Marco Chen <haisheng.chen4u@foxmail.com>
2023-12-09 17:36:32 -0800

    Update [LintCode-52]Next Permutation.java

--- source_code/[LintCode-52]Next Permutation.java
+++ source_code/[LintCode-52]Next Permutation.java
@@ -3,21 +3,21 @@
 Given a list of integers, which denote a permutation.
 Find the next permutation in ascending order.
 
-Example
-	Example 1:
-		Input:[1]
-		Output:[1]
 
-	Example 2:
-		Input:[1,3,2,3]
-		Output:[1,3,3,2]
+Example 1
+    Input:[1]
+    Output:[1]
 
-	Example 3:
-		Input:[4,3,2,1]
-		Output:[1,2,3,4]
+Example 2
+    Input:[1,3,2,3]
+    Output:[1,3,3,2]
+
+Example 3
+    Input:[4,3,2,1]
+    Output:[1,2,3,4]
 
 Notice
-	The list may contains duplicate integers.
+    The list may contains duplicate integers.
 ***/
 public class Solution {
     /**
