8a3ece1a21a3f28eb5f89631dc9392d32b2f42d1
Marco Chen <haisheng.chen4u@foxmail.com>
2023-12-09 17:26:30 -0800

    Update [LintCode-190]Next Permutation II.java

--- source_code/[LintCode-190]Next Permutation II.java
+++ source_code/[LintCode-190]Next Permutation II.java
@@ -4,23 +4,25 @@ Implement next permutation, which rearranges numbers into the lexicographically 
 
 If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
 
-Example
-	Example 1:
-		Input:1,2,3
-		Output:1,3,2
-	Example 2:
-		Input:3,2,1
-		Output:1,2,3
-	Example 3:
-		Input:1,1,5
-		Output:1,5,1
-		Challenge
-			The replacement must be in-place, do not allocate extra memory.
+Example 1
+    Input:1,2,3
+    Output:1,3,2
+
+Example 2
+    Input:3,2,1
+    Output:1,2,3
+
+Example 3
+    Input:1,1,5
+    Output:1,5,1
+
+Challenge
+    The replacement must be in-place, do not allocate extra memory.
 ***/
 public class Solution {
     // field
     private final int DEFAULT_VALUE = -1;
-    
+
     /**
      * @param nums: An array of integers
      * @return: nothing
@@ -30,35 +32,35 @@ public class Solution {
         if (nums == null || nums.length == 0) {
             return;
         }
-        
+
         int size= nums.length;
-        
+
         int indexToReplace = DEFAULT_VALUE;
         int pivot = DEFAULT_VALUE;
         for (int i = size - 2; i >= 0; i--) {
             if (nums[i] < nums[i + 1]) {
                 indexToReplace = i;
                 pivot = nums[indexToReplace];
-                
+
                 break;
             }
         }
-        
+
         if (pivot == DEFAULT_VALUE) {// the last index of permutation
             Arrays.sort(nums);
             return;
         }
-        
+
         int i = indexToReplace + 1;
         while (i < size && nums[i] > pivot) {
-			i++;
-		}
-		
-		swap(nums, indexToReplace, i - 1);
-        
+            i++;
+        }
+
+        swap(nums, indexToReplace, i - 1);
+
         int startPos = indexToReplace + 1;
         int endPos = size;
-        
+
         Arrays.sort(nums, startPos, endPos);
     }
     
