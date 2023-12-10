23312ab87cfde2ab9ef9986a66f3f588cb676b77
Marco Chen <haisheng.chen4u@foxmail.com>
2023-12-10 11:50:39 -0800

    Update [LintCode-15]Permutations.java

--- source_code/[LintCode-15]Permutations.java
+++ source_code/[LintCode-15]Permutations.java
@@ -1,25 +1,25 @@
 /*** LintCode 15. Permutations
  * Given a list of numbers, return all possible permutations.
 
-Example
-    Example 1:
-        Input: [1]
-        Output:
-            [
-              [1]
-            ]
-            
-    Example 2:
-        Input: [1,2,3]
-        Output:
-            [
-              [1,2,3],
-              [1,3,2],
-              [2,1,3],
-              [2,3,1],
-              [3,1,2],
-              [3,2,1]
-            ]
+
+Example 1:
+Input: [1]
+Output:
+    [
+      [1]
+    ]
+    
+Example 2:
+Input: [1,2,3]
+Output:
+    [
+      [1,2,3],
+      [1,3,2],
+      [2,1,3],
+      [2,3,1],
+      [3,1,2],
+      [3,2,1]
+    ]
 
 Challenge
     Do it without recursion.
