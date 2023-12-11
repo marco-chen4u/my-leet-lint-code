1cae3bd2e019e6ffa6c8b69a974fd08d9b8dd7ba
Marco Chen <haisheng.chen4u@foxmail.com>
2023-12-10 17:12:13 -0800

    Update [LintCode-634]Word Squares.java

--- source_code/[LintCode-634]Word Squares.java
+++ source_code/[LintCode-634]Word Squares.java
@@ -6,29 +6,35 @@ A sequence of words forms a valid word square if the kth row and column read the
 
 For example, the word sequence ["ball","area","lead","lady"] forms a word square because each word reads the same both horizontally and vertically.
 
-b a l l
-a r e a
-l e a d
-l a d y
+    [
+      ['b', 'a', 'l', 'l'],
+      ['a', 'r', 'e', 'a'],
+      ['r', 'e', 'a', 'd'],
+      ['l', 'a', 'd', 'y']
+    ]
 
-Example
-	Example 1:
-		Input:
-		["area","lead","wall","lady","ball"]
-		Output:
-		[["wall","area","lead","lady"],["ball","area","lead","lady"]]
-		Explanation:
-			The output consists of two word squares. The order of output does not matter (just the order of words in each word square matters).
-	Example 2:
-		Input:
-		["abat","baba","atan","atal"]
-		Output:
-		 [["baba","abat","baba","atan"],["baba","abat","baba","atal"]]
+Example 1
+    Input:
+        ["area","lead","wall","lady","ball"]
+    Output:
+        [["wall","area","lead","lady"],["ball","area","lead","lady"]]
+    Explanation:
+        The output consists of two word squares. The order of output does not matter (just the order of words in each word square matters).
+
+Example 2
+    Input:
+        ["abat","baba","atan","atal"]
+    Output:
+        [["baba","abat","baba","atan"],["baba","abat","baba","atal"]]
 Notice
-	There are at least 1 and at most 1000 words.
-	All words will have the exact same length.
-	Word length is at least 1 and at most 5.
-	Each word contains only lowercase English alphabet a-z.
+    There are at least 1 and at most 1000 words.
+    All words will have the exact same length.
+    Word length is at least 1 and at most 5.
+    Each word contains only lowercase English alphabet a-z.
+
+Link
+    LintCode: https://www.lintcode.com/problem/634/
+    LeetCode: https://leetcode.com/problems/word-squares/
 ***/
 //version-1(bad solution)
 public class Solution {
