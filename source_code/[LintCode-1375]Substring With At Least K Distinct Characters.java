829da76f095284cb6dc9c3c5a3464971d991d198
Marco Chen <haisheng.chen4u@foxmail.com>
2023-12-20 08:19:28 -0800

    Update [LintCode-1375]Substring With At Least K Distinct Characters.java

--- source_code/[LintCode-1375]Substring With At Least K Distinct Characters.java
+++ source_code/[LintCode-1375]Substring With At Least K Distinct Characters.java
@@ -1,24 +1,30 @@
 /***
 * LintCode 1375. Substring With At Least K Distinct Characters
 Given a string s, with only lowercase characters,  Return the number of substrings that at least k distinct characters.
-Example
-    Example 1:
-        Input: S = "abcabcabca", k = 4
-        Output: 0
-        Explanation: There are only three distinct characters in the string.
-    Example 2:
-        Input: S = "abcabcabcabc", k = 3
-        Output: 55
-        Explanation: Any substring whose length is not smaller than 3 contains a, b, c.
-            For example, there are 10 substrings whose length are 3, "abc", "bca", "cab" ... "abc"
-            There are 9 substrings whose length are 4, "abca", "bcab", "cabc" ... "cabc"
-            ...
-            There is 1 substring whose length is 12, "abcabcabcabc"
-            So the answer is 1 + 2 + ... + 10 = 55.
+
+Example 1
+    Input: S = "abcabcabca", k = 4
+    Output: 0
+    Explanation: There are only three distinct characters in the string.
+
+Example 2
+    Input: S = "abcabcabcabc", k = 3
+    Output: 55
+    Explanation: Any substring whose length is not smaller than 3 contains a, b, c.
+        For example, there are 10 substrings whose length are 3, "abc", "bca", "cab" ... "abc"
+        There are 9 substrings whose length are 4, "abca", "bcab", "cabc" ... "cabc"
+        ...
+        There is 1 substring whose length is 12, "abcabcabcabc"
+        So the answer is 1 + 2 + ... + 10 = 55.
+
 Notice
     1.10 ≤ length(S) ≤ 1,000,000
     2.1 ≤ k ≤ 26
+
+Link
+    https://www.lintcode.com/problem/1375/
 ***/
+//version-1: two-pointer, sliding-window, time-complexity: O(n)
 public class Solution {
     /**
     * @param s : input string
@@ -58,6 +64,50 @@ public class Solution {
             letters[preWindowIndex]--;
 
             left++;//slow pointer moving farward
+        }
+
+        return result;
+    }
+}
+
+//version-2: same as version-1
+public class Solution {
+    /**
+     * @param s: a string
+     * @param k: an integer
+     * @return: the number of substrings there are that contain at least k distinct characters
+     */
+    public long kDistinctCharacters(String s, int k) {
+        // Write your code here
+        if (s == null || s.isEmpty() || k < 1) {
+            return 0;
+        }
+
+        long result = 0;
+
+        int n = s.length();
+        int[] letter_counter = new int[26];
+        int count = 0; // distinct char count
+
+        int i, j;
+
+        for (i = 0, j = 0; i < n; i++) {
+
+            while (j < n && count < k) {
+                int current = s.charAt(j) - 'a';
+                letter_counter[current] += 1;
+                count += letter_counter[current] == 1 ? 1 : 0;
+
+                j += 1;
+            }
+
+            result += (count == k) ? n - j + 1 : 0;
+
+            int pre = s.charAt(i) - 'a';
+            letter_counter[pre] -= 1;
+
+            count -= (letter_counter[pre] == 0) ? 1 : 0;
+
         }
 
         return result;
