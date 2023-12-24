acbb5ed24152c2c402daace56d68c0eb9b85954c
Marco Chen <haisheng.chen4u@foxmail.com>
2023-12-23 22:39:44 -0600

    Update [LintCode-122]Largest Rectangle in Histogram.java

--- source_code/[LintCode-122]Largest Rectangle in Histogram.java
+++ source_code/[LintCode-122]Largest Rectangle in Histogram.java
@@ -2,17 +2,22 @@
 * LintCode 122. Largest Rectangle in Histogram
 Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
 
-Example
-    Example 1:
-        Input：[2,1,5,6,2,3]
-        Output：10
-        Explanation：
-            The third and fourth rectangular truncated rectangle has an area of 2*5=10.
-    Example 2:
-        Input：[1,1]
-        Output：2
-        Explanation：
-            The first and second rectangular truncated rectangle has an area of 2*1=2.
+
+Example 1
+    Input：[2,1,5,6,2,3]
+    Output：10
+    Explanation：
+        The third and fourth rectangular truncated rectangle has an area of 2*5=10.
+
+Example 2
+    Input：[1,1]
+    Output：2
+    Explanation：
+        The first and second rectangular truncated rectangle has an area of 2*1=2.
+
+Link
+    LintCode: https://www.lintcode.com/problem/122/
+    LeetCode: https://leetcode.com/problems/largest-rectangle-in-histogram/description/
 ***/
 public class Solution {
     /**
