cde50978389c5ea90305f23bccb9212bdaf0c4f4
Marco Chen <haisheng.chen4u@foxmail.com>
2024-01-18 14:46:56 -0600

    Update [LintCode-364]Trapping Rain Water II.java

--- source_code/[LintCode-364]Trapping Rain Water II.java
+++ source_code/[LintCode-364]Trapping Rain Water II.java
@@ -27,7 +27,7 @@ Link:
     LintCode: https://www.lintcode.com/problem/364/
     LeetCode : https://leetcode.com/problems/trapping-rain-water-ii/
 ***/
-//version-1: minHeap, time complexity is log(2*(m + n)) -> log(m + n)
+//version-1: minHeap, time complexity is O(m * n * log(2*(m + n)) -> O(m * n * log(m + n))
 class Element {
     // fields
     int x;
@@ -120,3 +120,79 @@ public class Solution {
         return result;
     }
 }
+
+//version-2: minHeap, time-complexity: O(m * n * log(m + n))
+class Solution {
+
+    private static final int[] DIRECTION_X = new int[] {1, 0, 0, -1};
+    private static final int[] DIRECTION_Y = new int[] {0, 1, -1, 0};
+
+    public int trapRainWater(int[][] heightMap) {
+        int m = heightMap.length; // row size
+        int n = heightMap[0].length; // column size
+
+        Queue<Element> minHeap = new PriorityQueue<>();
+
+        boolean[][] visited = new boolean[m][n];
+
+        // for the couter to get the lowest bucket pillar
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || i == m - 1 || j == n - 1) {
+                    minHeap.offer(new Element(i, j, heightMap[i][j]));
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        int result = 0;
+
+        while (!minHeap.isEmpty()) {
+            Element current = minHeap.poll();
+            int currentX = current.x;
+            int currentY = current.y;
+            int currentHeight = current.height;
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = currentX + DIRECTION_X[i];
+                int nextY = currentY + DIRECTION_Y[i];
+
+                if (nextX < 0 || nextX >= m || nextY < 0 || nextY >= n) {
+                    continue;
+                }
+
+                if (visited[nextX][nextY]) {
+                    continue;
+                }
+
+                int nextHeight = heightMap[nextX][nextY];
+
+                visited[nextX][nextY] = true;
+                minHeap.offer(new Element(nextX, nextY, Math.max(currentHeight, nextHeight)));
+                result += Math.max(0, currentHeight - nextHeight);
+            }
+        }
+
+        return result;
+    }
+}
+
+// helper class
+class Element implements Comparable<Element> {
+    int x;
+    int y;
+    int height;
+
+    public Element(int x, int y, int height) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+    }
+
+    @Override
+    public int compareTo(Element other) {
+        return this.height - other.height;
+    }
+}
+
+
