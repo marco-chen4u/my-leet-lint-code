8347841f9646857fff74f7d09241cf8060a2f810
Marco Chen <haisheng.chen4u@foxmail.com>
2024-03-08 12:49:29 -0600

    Update [LintCode-427]Generate Parentheses.java

--- source_code/[LintCode-427]Generate Parentheses.java
+++ source_code/[LintCode-427]Generate Parentheses.java
@@ -14,6 +14,7 @@ Link:
     LintCode: https://www.lintcode.com/problem/427
     LeetCode: https://leetcode.com/problems/generate-parentheses/
 ***/
+//version-1: stringbuilder and dfs
 public class Solution {
     private final String LEFT_PARENTHESE = "(";
     private final String RIGHT_PARENTHESE = ")";
@@ -57,6 +58,44 @@ public class Solution {
             parentheses.append(RIGHT_PARENTHESE);
             findValidPair(result, parentheses, left, right + 1);
             parentheses.setLength(parentheses.length() - 1);
+        }
+    }
+}
+
+//version-2: better and faster, dfs
+class Solution {
+
+    private static int n; // total length;
+    private static final char OPEN_BRACKET = '(';
+    private static final char CLOSE_BRACKET = ')';
+
+    public List<String> generateParenthesis(int n) {
+        if (n <= 0) {
+            return Collections.emptyList();
+        }
+
+        this.n = n;
+
+        String str = "";
+        List<String> results = new LinkedList<>();
+        findPair(0, 0, str, results);
+
+        return results;
+    }
+
+    // helper method
+    private void findPair(int left, int right, String str, List<String> results) {
+        if (left == n && right == n) {
+            results.add(str);
+            return;
+        }
+
+        if (left < n) {
+            findPair(left + 1, right, str + OPEN_BRACKET, results);
+        }
+
+        if (right < n && right < left) {
+            findPair(left, right + 1, str + CLOSE_BRACKET, results);
         }
     }
 }
